20e5b255b4a49452af21c6baa63fe00530b56d12
Minor G <minorg@server.fake>
2019-12-29 20:28:50 -0500

    configuration, change /graphql to /api/graphql to simplify forwarding

--- packages/lib/webpack.config.devServer.js
+++ packages/lib/webpack.config.devServer.js
@@ -11,7 +11,7 @@ module.exports = function (contentBase) {
       open: false,
       port: 9001,
       proxy: {
-        "/graphql": {
+        "/api": {
           "target": {
             "host": "localhost",
             "protocol": 'http:',
