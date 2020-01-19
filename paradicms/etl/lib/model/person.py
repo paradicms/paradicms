05f631855e666c725540f0e552b9419624e98d73
Minor G <minorg@server.fake>
2020-01-19 16:15:19 -0500

    CONTACT namespace to get sortName

--- paradicms/etl/lib/model/person.py
+++ paradicms/etl/lib/model/person.py
@@ -4,6 +4,7 @@ from rdflib.namespace import FOAF
 
 from ._model import _Model
 from ._name_property_mixin import _NamePropertyMixin
+from ..namespace import CONTACT
 
 
 class Person(_Model, _NamePropertyMixin):
@@ -22,6 +23,14 @@ class Person(_Model, _NamePropertyMixin):
     @given_name.setter
     def given_name(self, value: str):
         self._set_single_value(FOAF.givenName, value)
+
+    @property
+    def sort_name(self) -> Optional[str]:
+        return self._get_single_value(CONTACT.sortName, str)
+
+    @sort_name.setter
+    def sort_name(self, value: str):
+        self._set_single_value(CONTACT.sortName, value)
 
     def validate(self):
         _NamePropertyMixin.validate(self)
--- paradicms/etl/lib/namespace.py
+++ paradicms/etl/lib/namespace.py
@@ -1,6 +1,7 @@
 from rdflib import Namespace
 
 CMS = Namespace("http://www.paradicms.org/ns#")
+CONTACT = Namespace("http://www.w3.org/2000/10/swap/pim/contact#")
 DCMITYPE = Namespace("http://purl.org/dc/dcmitype/")
 EXIF = Namespace("http://www.w3.org/2003/12/exif/ns#")
 PROV = Namespace("http://www.w3.org/ns/prov#")
