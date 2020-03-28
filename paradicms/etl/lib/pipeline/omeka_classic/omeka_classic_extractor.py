69b416bac8e9c08835df3c683b8bc3e76b81c9b2
Minor G <minorg@server.fake>
2020-03-28 15:52:19 -0400

    convert OmekaClassicExtractor

--- paradicms/etl/lib/pipeline/omeka_classic/omeka_classic_extractor.py
+++ paradicms/etl/lib/pipeline/omeka_classic/omeka_classic_extractor.py
@@ -1,8 +1,10 @@
 import json
 
-from paradicms.etl.lib.pipeline._extractor import _Extractor
-from paradicms.etl.lib.pipeline._pipeline_storage import _PipelineStorage
+from pathvalidate import sanitize_filename
 from yomeka.client.omeka_rest_api_client import OmekaRestApiClient
+
+from paradicms.etl.lib.pipeline._extractor import _Extractor
+from paradicms.etl.lib.pipeline.pipeline_storage import PipelineStorage
 
 
 class OmekaClassicExtractor(_Extractor):
@@ -11,20 +13,32 @@ class OmekaClassicExtractor(_Extractor):
         self.__client = OmekaRestApiClient(api_key=api_key, endpoint_url=endpoint_url)
         self.__endpoint_url = endpoint_url
 
-    def extract(self, *, force: bool, storage: _PipelineStorage):
-        collections_key = self.__endpoint_url + " collections.json"
-        files_key = self.__endpoint_url + " files.json"
-        items_key = self.__endpoint_url + " items.json"
+    def extract(self, *, force: bool, storage: PipelineStorage):
+        collections_file_path = storage.extracted_data_dir_path / (sanitize_filename(self.__endpoint_url + " collections") + ".json")
+        files_file_path = storage.extracted_data_dir_path / (sanitize_filename(self.__endpoint_url + " files") + ".json")
+        items_file_path = storage.extracted_data_dir_path / (sanitize_filename(self.__endpoint_url + " items") + ".json")
 
-        if not storage.head(collections_key) or force:
-            storage.put(collections_key, json.dumps(
-                [json.loads(collection.json) for collection in self.__client.get_all_collections()]))
-            storage.put(files_key,
-                        json.dumps([json.loads(file_.json) for file_ in self.__client.get_all_files()]))
-            storage.put(items_key, json.dumps([json.loads(item.json) for item in self.__client.get_all_items()]))
+        if not collections_file_path.exists or force:
+            collections = tuple(json.loads(collection.json) for collection in self.__client.get_all_collections())
+            files = tuple(json.loads(file_.json) for file_ in self.__client.get_all_files())
+            items = tuple(json.loads(item.json) for item in self.__client.get_all_items())
+
+            with open(collections_file_path, "w+") as collections_file:
+                json.dump(collections, collections_file)
+            with open(files_file_path, "w+") as files_file:
+                json.dump(files, files_file)
+            with open(items_file_path, "w+") as items_file:
+                json.dump(items, items_file)
+        else:
+            with open(collections_file_path) as collections_file:
+                collections = json.load(collections_file)
+            with open(files_file_path) as files_file:
+                files = json.load(files_file)
+            with open(items_file_path) as items_file:
+                items = json.load(items_file)
 
         return {
-            "collections": json.load(storage.get(collections_key)),
-            "files": json.load(storage.get(files_key)),
-            "items": json.load(storage.get(items_key))
+            "collections": collections,
+            "files": files,
+            "items": items
         }
