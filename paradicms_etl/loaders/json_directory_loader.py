d81919c929b2eebb559bfb3c08b651fc9a0ce0b3
Minor G <minorg@server.fake>
2020-07-24 20:05:10 -0400

    hack to accomodate Gatsby

--- paradicms_etl/loaders/json_directory_loader.py
+++ paradicms_etl/loaders/json_directory_loader.py
@@ -14,26 +14,39 @@ from paradicms_etl.pipeline_storage import PipelineStorage
 
 
 class JsonDirectoryLoader(_Loader):
-    def __init__(self, *, directory_path: Path, clean: bool = False, **kwds):
+    def __init__(self, *, root_directory_path: Path, clean: bool = False, gatsby_js: bool = False, **kwds):
         _FileLoader.__init__(self, **kwds)
         self.__clean = clean
-        self.__directory_path = directory_path
+        self.__gatsby_js = gatsby_js
+        self.__root_directory_path = root_directory_path
 
     def load(self, *, force: bool, models: Generator[_Model, None, None], storage: PipelineStorage):
-        directory_path = self.__directory_path
-        if directory_path is None:
-            directory_path = storage.transformed_data_dir_path / sanitize_filename(self._pipeline_id)
-        if self.__clean and directory_path.is_dir():
-            rmtree(directory_path)
-        directory_path.mkdir(parents=False, exist_ok=True)
+        root_directory_path = self.__root_directory_path
+        if root_directory_path is None:
+            root_directory_path = storage.transformed_data_dir_path / sanitize_filename(self._pipeline_id)
+        if self.__clean and root_directory_path.is_dir():
+            rmtree(root_directory_path)
+        root_directory_path.mkdir(exist_ok=True)
 
-        json_objects_by_class_name = {}
+        json_objects = {}
         for model in models:
-            json_objects_by_class_name.setdefault(model.__class__.__name__, []).append(model.to_dict())
+            assert model.uri is not None
+            class_json_objects = json_objects.setdefault(model.__class__.__name__, {})
+            assert str(model.uri) not in class_json_objects
+            class_json_objects[str(model.uri)] = JsonFileLoader.remove_nulls(model.to_dict())
 
-        json_objects_by_class_name = JsonFileLoader.remove_nulls(json_objects_by_class_name)
+        for class_name, json_objects in json_objects.items():
+            if self.__gatsby_js and len(json_objects) == 1:
+                # The gatsby-json-transformer has a problem picking up a directory with a single .json file in it with a top-level object (instead of an array)
+                # Work around this by writing a file with a top-level array instead.
+                file_path = root_directory_path / (stringcase.camelcase(class_name) + ".json")
+                with open(file_path, "w+") as file_:
+                    json.dump(tuple(json_objects.values()), file_)
+                continue
 
-        for class_name, json_objects in json_objects_by_class_name.items():
-            file_path = directory_path / (stringcase.camelcase(class_name) + ".json")
-            with open(file_path, "w+") as file_:
-                json.dump(json_objects, file_)
+            class_directory_path = root_directory_path / stringcase.camelcase(class_name)
+            class_directory_path.mkdir(exist_ok=True)
+            for uri, json_object in json_objects.items():
+                file_path = class_directory_path / (sanitize_filename(uri) + ".json")
+                with open(file_path, "w+") as file_:
+                    json.dump(json_object, file_)
