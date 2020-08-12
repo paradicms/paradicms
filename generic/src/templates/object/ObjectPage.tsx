b3bf10f1781ef27dccd02d382c6458b354c74bd0
Minor G <minorg@server.fake>
2020-08-12 19:42:40 -0400

    first cut of ObjectPage

---
+++ generic/src/templates/object/ObjectPage.tsx
@@ -0,0 +1,59 @@
+import {graphql} from "gatsby";
+import * as React from "react";
+import {Layout} from "~/components/layout/Layout";
+import {Card, CardContent, CardHeader, Grid} from "@material-ui/core";
+import {Institution} from "~/models/institution/Institution";
+import {CollectionPageQuery} from "~/graphql/types";
+import {Object} from "~/models/object/Object";
+import {RightsTable} from "~/components/rights/RightsTable";
+
+const ObjectPage: React.FunctionComponent<{
+  data: CollectionPageQuery;
+  pageContext: {
+    object: Object;
+    institution: Institution;
+  };
+}> = ({
+  data: {
+    allImageJson: {nodes: objectImages},
+    allPropertyDefinitionJson: {nodes: propertyDefinitions},
+  },
+  pageContext: {institution, object},
+}) => {
+  const rights = object.rights ?? institution.rights ?? undefined;
+
+  return (
+    <Layout
+      breadcrumbs={{institution, object}}
+      documentTitle={"Object - " + object.title}
+    >
+      <Grid container direction="column" spacing={2}>
+        {rights ? (
+          <Card>
+            <CardHeader>Rights</CardHeader>
+            <CardContent>
+              <RightsTable rights={rights} />
+            </CardContent>
+          </Card>
+        ) : null}
+      </Grid>
+    </Layout>
+  );
+};
+
+export default ObjectPage;
+
+export const query = graphql`
+  query ObjectPage($objectUri: String!) {
+    allImageJson(filter: {objectUri: {eq: $objectUri}}) {
+      nodes {
+        ...ImageFragment
+      }
+    }
+    allPropertyDefinitionJson {
+      nodes {
+        ...PropertyDefinitionFragment
+      }
+    }
+  }
+`;
