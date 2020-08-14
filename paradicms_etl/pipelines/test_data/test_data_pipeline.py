d20959ecaac886d614de2b4624793ce4a679844c
Minor G <minorg@server.fake>
2020-08-14 19:55:27 -0400

    missing comma

--- paradicms_etl/pipelines/test_data/test_data_pipeline.py
+++ paradicms_etl/pipelines/test_data/test_data_pipeline.py
@@ -25,7 +25,7 @@ class TestDataPipeline(_Pipeline):
                         pipeline_id=self.__ID,
                         root_directory_path=root_dir_path / "gui" / "generic" / "src" / "data" / "test",
                         strategy=JsonDirectoryLoader.Strategy.FILE_PER_MODEL_TYPE
-                    )
+                    ),
                 ),
                 pipeline_id=self.__ID
             ),
