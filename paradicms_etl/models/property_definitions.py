2a1e5b43a46bfb8e1822ea31beb4e161e71644ca
Minor G <minorg@server.fake>
2020-08-16 10:17:39 -0400

    PropertyDefinitions: add a few DCTERMS properties, separate VRA and DCTERMS properties

--- paradicms_etl/models/property_definitions.py
+++ paradicms_etl/models/property_definitions.py
@@ -7,24 +7,38 @@ from paradicms_etl.namespace import VRA
 
 
 class PropertyDefinitions:
+    # Dublin Core Terms
     ALTERNATIVE_TITLE = PropertyDefinition(key="alternative_title", label_plural="Alternative titles", label_singular="Alternative title", uri=DCTERMS.alternative)
+    CONTRIBUTOR = PropertyDefinition(faceted=True, key="contributor", label_plural="Contributors", label_singular="Contributor", uri=DCTERMS.contributor)
+    COVERAGE = PropertyDefinition(faceted=True, key="coverage", label_plural="Coverages", label_singular="Coverage", uri=DCTERMS.coverage)
     CREATOR = PropertyDefinition(faceted=True, key="creator", label_plural="Creators", label_singular="Creator", uri=DCTERMS.creator)
-    CULTURAL_CONTEXT = PropertyDefinition(key="cultural_context", label_plural="Cultural contexts", label_singular="Cultural context", uri=VRA.culturalContext)
     DATE = PropertyDefinition(key="date", label_plural="Dates", label_singular="Date", uri=DCTERMS.date)
+    DATE_CREATED = PropertyDefinition(key="date_created", label_plural="Dates created", label_singular="Date created", uri=DCTERMS.dateCreated)
+    DATE_SUBMITTED = PropertyDefinition(key="date_submitted", label_plural="Dates submitted", label_singular="Date submitted", uri=DCTERMS.dateSubmitted)
     DESCRIPTION = PropertyDefinition(key="description", label_plural="Descriptions", label_singular="Description", uri=DCTERMS.description)
     EXTENT = PropertyDefinition(faceted=True, key="extent", label_plural="Extents", label_singular="Extent", uri=DCTERMS.extent)
+    FORMAT = PropertyDefinition(key="format", label_plural="Formats", label_singular="Format", uri=DCTERMS["format"])
     IDENTIFIER = PropertyDefinition(key="identifier", label_plural="Identifiers", label_singular="Identifier", uri=DCTERMS.identifier)
+    IS_REFERENCED_BY = PropertyDefinition(key="is_referenced_by", label_plural="Referenced by", label_singular="Referenced by", uri=DCTERMS.isReferencedBy)
     LANGUAGE = PropertyDefinition(faceted=True, key="language", label_plural="Languages", label_singular="Language", uri=DCTERMS.language)
-    MATERIAL = PropertyDefinition(faceted=True, key="material", label_plural="Materials", label_singular="Material", uri=VRA.material)
+    LICENSE = PropertyDefinition(key="license", label_plural="Licenses", label_singular="License", uri=DCTERMS.license)
     MEDIUM = PropertyDefinition(faceted=True, key="medium", label_plural="Media", label_singular="Medium", uri=DCTERMS.medium)
     PROVENANCE = PropertyDefinition(key="provenance", label_plural="Provenances", label_singular="Provenance", uri=DCTERMS.provenance)
     PUBLISHER = PropertyDefinition(faceted=True, key="publisher", label_plural="Publishers", label_singular="Publisher", uri=DCTERMS.publisher)
+    REFERENCES = PropertyDefinition(key="references", label_plural="References", label_singular="References", uri=DCTERMS.references)
+    RELATION = PropertyDefinition(key="relation", label_plural="Relations", label_singular="Relation", uri=DCTERMS.relation)
+    RIGHTS = PropertyDefinition(key="rights", label_plural="Rights", label_singular="Rights", uri=DCTERMS.rights)
+    RIGHTS_HOLDER = PropertyDefinition(key="rights_holder", label_plural="Rights holders", label_singular="Rights holder", uri=DCTERMS.rightsHolder)
     SOURCE = PropertyDefinition(key="source", label_plural="Sources", label_singular="Source", uri=DCTERMS.source)
     SPATIAL = PropertyDefinition(faceted = True, key="spatial", label_plural="Spatial coverages", label_singular="Spatial coverage", uri=DCTERMS.spatial)
     SUBJECT = PropertyDefinition(faceted=True, key="subject", label_plural="Subjects", label_singular="Subject", uri=DCTERMS.subject)
-    TECHNIQUE = PropertyDefinition(faceted=True, key="technique", label_plural="Techniques", label_singular="Technique", uri=VRA.hasTechnique)
     TEMPORAL = PropertyDefinition(faceted=True, key="temporal", label_plural="Temporal coverages", label_singular="Temporal coverage", uri=DCTERMS.temporal)
     TYPE = PropertyDefinition(faceted=True, key="type", label_plural="Types", label_singular="Type", uri=DCTERMS.type)
+
+    # VRA Core
+    CULTURAL_CONTEXT = PropertyDefinition(key="cultural_context", label_plural="Cultural contexts", label_singular="Cultural context", uri=VRA.culturalContext)
+    MATERIAL = PropertyDefinition(faceted=True, key="material", label_plural="Materials", label_singular="Material", uri=VRA.material)
+    TECHNIQUE = PropertyDefinition(faceted=True, key="technique", label_plural="Techniques", label_singular="Technique", uri=VRA.hasTechnique)
 
     @classmethod
     def as_tuple(cls) -> Tuple[PropertyDefinition, ...]:
