f217bb9453fa86be8fac22b4349f781bc553aa07
Minor G <minorg@server.fake>
2020-08-17 21:18:12 -0400

    don't need PropertyKey and PropertyValue typedefs in py or ts

--- generic/src/models/PropertyDefinition.ts
+++ generic/src/models/PropertyDefinition.ts
@@ -1,6 +1,6 @@
-import {PropertyDefinitionJson} from "~/graphql/types";
-
-export type PropertyDefinition = Pick<
-  PropertyDefinitionJson,
-  "faceted" | "key" | "labelPlural" | "labelSingular"
->;
+export interface PropertyDefinition {
+  readonly faceted?: boolean | null;
+  readonly key: string;
+  readonly labelPlural: string;
+  readonly labelSingular: string;
+}
--- generic/src/models/PropertyValue.ts
+++
@@ -1,1 +0,0 @@
-export type PropertyValue = string;
