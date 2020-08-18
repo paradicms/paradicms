f5f8fd66dcd9351555899b40cfdf1097d7981209
Minor G <minorg@server.fake>
2020-08-17 21:19:22 -0400

    institution rights is required

--- generic/src/models/Institution.ts
+++ generic/src/models/Institution.ts
@@ -2,6 +2,6 @@ import {Rights} from "~/models/Rights";
 
 export interface Institution {
   readonly name: string;
-  readonly rights?: Rights | null;
+  readonly rights: Rights;
   readonly uri: string;
 }
