6899fb728ead677ac5491b82d085ad5a10ffb1c2
Minor G <minorg@server.fake>
2020-09-18 19:40:45 -0400

    AirtableExtractor: allow query parameters

--- paradicms_etl/extractors/airtable_extractor.py
+++ paradicms_etl/extractors/airtable_extractor.py
@@ -1,5 +1,5 @@
 import json
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Union
 from urllib.parse import quote
 from urllib.request import urlopen
 
@@ -9,30 +9,56 @@ from paradicms_etl._extractor import _Extractor
 
 
 class AirtableExtractor(_Extractor):
-    def __init__(self, *, api_key: str, base_id: str, tables: List[str], **kwds):
+    def __init__(
+        self,
+        *,
+        api_key: str,
+        base_id: str,
+        tables: Union[List[str], Tuple[str], Dict[str, Dict[str, str]]],
+        **kwds,
+    ):
+        """
+        :param api_key: Airtable API key
+        :param base_id: Airtable base identifier
+        :param tables: a list or a union of table [names] or a dict where the keys are table names and the values are API query parameters
+        """
         _Extractor.__init__(self, **kwds)
         self.__api_key = api_key
         self.__base_id = base_id
-        self.__tables = tables
+        if isinstance(tables, (list, tuple)):
+            self.__tables = {table: {} for table in tables}
+        else:
+            assert isinstance(tables, dict)
+            self.__tables = tables
 
     def extract(self, *, force: bool):
         records_by_table = {}
-        for table in self.__tables:
+        for table, query_parameters in self.__tables.items():
             records_by_table[table] = self.__extract_table_records(
-                force=force, table=table
+                force=force, table=table, query_parameters=query_parameters
             )
         return {"records_by_table": records_by_table}
 
-    def __extract_table_records(self, *, force: bool, table: str) -> Tuple[Dict, ...]:
+    def __extract_table_records(
+        self, *, force: bool, table: str, query_parameters: Dict[str, str]
+    ) -> Tuple[Dict, ...]:
         records = []
         offset = None
         while True:
+            request_query_parameters = query_parameters.copy()
+            request_query_parameters["api_key"] = self.__api_key
+            if offset is not None:
+                request_query_parameters["offset"] = offset
+
             url = (
                 self.table_url(base_id=self.__base_id, table=table)
-                + f"?api_key={quote(self.__api_key)}"
+                + "?"
+                + "&".join(
+                    f"{key}={quote(value)}"
+                    for key, value in request_query_parameters.items()
+                )
             )
-            if offset is not None:
-                url += "&offset=" + quote(offset)
+
             file_path = self._extracted_data_dir_path / (
                 sanitize_filename(url) + ".json"
             )
