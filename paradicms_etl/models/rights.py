dd9941ef13f994c4fdfb1adaaa3c4eb57e7f6412
Minor G <minorg@server.fake>
2020-09-20 19:37:19 -0400

    add creator to Rights

--- paradicms_etl/models/rights.py
+++ paradicms_etl/models/rights.py
@@ -13,6 +13,7 @@ from paradicms_etl.models.rights_value import RightsValue
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass(init=False)
 class Rights:
+    creator: Optional[RightsValue] = None
     holder: Optional[RightsValue] = None
     license: Optional[RightsValue] = None
     statement: Optional[RightsValue] = None
@@ -20,16 +21,25 @@ class Rights:
     def __init__(
         self,
         *,
+        creator: Union[None, RightsValue, str, URIRef] = None,
         holder: Union[None, RightsValue, str, URIRef] = None,
         license: Union[None, RightsValue, str, URIRef] = None,
         statement: Union[None, RightsValue, str, URIRef] = None
     ):
+        self.creator = RightsValue.from_value(creator)
         self.holder = RightsValue.from_value(holder)
         self.license = RightsValue.from_value(license)
         self.statement = RightsValue.from_value(statement)
 
     @classmethod
     def from_properties(cls, properties: Tuple[Property, ...]):
+        creator = RightsValue.from_properties(
+            properties=tuple(
+                property
+                for properties in properties
+                if property.uri == PropertyDefinitions.CREATOR.uri
+            )
+        )
         holder = RightsValue.from_properties(
             properties=tuple(
                 property
@@ -52,12 +62,19 @@ class Rights:
             )
         )
 
-        if not holder and not license and not statement:
+        if not creator and not holder and not license and not statement:
             return None
 
-        return Rights(holder=holder, license=license, statement=statement)
+        return Rights(
+            creator=creator, holder=holder, license=license, statement=statement
+        )
 
     def to_rdf(self, *, add_to_resource: Resource) -> None:
+        if self.creator is not None:
+            self.creator.to_rdf(
+                add_to_resource=add_to_resource,
+                property_uri=PropertyDefinitions.CREATOR.uri,
+            )
         if self.holder is not None:
             self.holder.to_rdf(
                 add_to_resource=add_to_resource,
