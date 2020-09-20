0eeaee0886a5aaecfe238fb5ba06299aa8e49aa1
Minor G <minorg@server.fake>
2020-09-20 11:38:46 -0400

    _CompositePipeline

---
+++ paradicms_etl/pipelines/_composite_pipeline.py
@@ -0,0 +1,72 @@
+from typing import Dict, List, Optional, Tuple
+
+from configargparse import ArgParser
+
+from paradicms_etl._loader import _Loader
+from paradicms_etl._pipeline import _Pipeline
+from paradicms_etl.extractors.nop_extractor import NopExtractor
+from paradicms_etl.transformers.nop_transformer import NopTransformer
+
+
+class _CompositePipeline(_Pipeline):
+    """
+    A base class for pipelines that compose 2+ other pipelines and share a loader.
+    """
+
+    def __init__(
+        self,
+        *,
+        id: str,
+        loader: _Loader,
+        pipelines: Tuple[_Pipeline, ...],
+        exclude_pipeline_id: Optional[List[str]] = None,
+        include_pipeline_id: Optional[List[str]] = None,
+        **kwds,
+    ):
+        _Pipeline.__init__(
+            self,
+            extractor=NopExtractor(pipeline_id=id, **kwds),
+            id=id,
+            loader=loader,
+            transformer=NopTransformer(pipeline_id=id, **kwds),
+            **kwds,
+        )
+
+        exclude_pipeline_ids = tuple(exclude_pipeline_id) if exclude_pipeline_id else ()
+        include_pipeline_ids = tuple(include_pipeline_id) if include_pipeline_id else ()
+
+        filtered_pipeline_ids = [pipeline.id for pipeline in pipelines]
+        for exclude_pipeline_id in exclude_pipeline_ids:
+            if exclude_pipeline_id not in filtered_pipeline_ids:
+                raise ValueError(f"unknown pipeline: {exclude_pipeline_id}")
+        for include_pipeline_id in include_pipeline_ids:
+            if include_pipeline_id not in filtered_pipeline_ids:
+                raise ValueError(f"unknown pipeline: {include_pipeline_id}")
+
+        for exclude_pipeline_id in exclude_pipeline_ids:
+            filtered_pipeline_ids.remove(exclude_pipeline_id)
+        if include_pipeline_ids:
+            filtered_pipeline_ids = [
+                pipeline_id
+                for pipeline_id in filtered_pipeline_ids
+                if pipeline_id in include_pipeline_ids
+            ]
+
+        self.__pipelines = tuple(
+            pipeline for pipeline in pipelines if pipeline.id in filtered_pipeline_ids
+        )
+
+    @classmethod
+    def add_arguments(cls, arg_parser: ArgParser) -> None:
+        _Pipeline.add_arguments(arg_parser)
+        arg_parser.add_argument("--exclude-pipeline-id", action="append")
+        arg_parser.add_argument("--include-pipeline-id", action="append")
+
+    def extract_transform_load(self, **kwds):
+        for pipeline in self.__pipelines:
+            self.loader.load(pipeline.extract_transform(**kwds))
+        self.loader.flush()
+
+    @property
+    def _pipelines(self) -> Tuple[_Pipeline, ...]:
+        return self.__pipelines
