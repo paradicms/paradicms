e37980a54ffd1e5e3da6be3409263c05d290a22f
Minor G <minorg@server.fake>
2020-09-19 20:01:43 -0400

    fix NopLoader

--- paradicms_etl/loaders/nop_loader.py
+++ paradicms_etl/loaders/nop_loader.py
@@ -2,5 +2,7 @@ from paradicms_etl._loader import _Loader
 
 
 class NopLoader(_Loader):
-    def load(self, *args, **kwds):
-        pass
+    def load(self, *, models):
+        # Iterate over models to exhaust the generator
+        for _ in models:
+            pass
