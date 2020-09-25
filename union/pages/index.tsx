3d9852dd8706fd037eb8977edd06423f0bbdabe7
Minor G <minorg@server.fake>
2020-09-25 19:18:39 -0400

    integrate InstitutionsGallery

--- union/pages/index.tsx
+++ union/pages/index.tsx
@@ -1,34 +1,42 @@
 import * as React from "react";
 import {GetStaticProps} from "next";
-import {Institution} from "@paradicms/models";
+import {Image, Images, Institution, Institutions} from "@paradicms/models";
 import {Hrefs} from "lib/Hrefs";
 import {Data} from "lib/Data";
-import {List, ListItem, ListItemText} from "@material-ui/core";
 import {Layout} from "components/Layout";
 import {Link} from "@paradicms/next";
+import {InstitutionsGallery} from "@paradicms/material-ui";
 
 const IndexPage: React.FunctionComponent<{
+  images: readonly Image[];
   institutions: readonly Institution[];
-}> = ({institutions}) => (
-  <Layout documentTitle="Institutions">
-    <List>
-      {institutions.map(institution => (
-        <ListItem key={institution.uri}>
-          <ListItemText>
-            <Link {...Hrefs.institution(institution.uri).home}>
-              {institution.name}
-            </Link>
-          </ListItemText>
-        </ListItem>
-      ))}
-    </List>
-  </Layout>
-);
+}> = ({images, institutions}) => {
+  const joinedInstitutions = Institutions.join({
+    institutions,
+    imagesByDepictsUri: Images.indexByDepictsUri(images),
+  });
+
+  return (
+    <Layout documentTitle="Institutions">
+      <InstitutionsGallery
+        institutions={joinedInstitutions}
+        renderInstitutionLink={(institution, children) => (
+          <Link
+            {...Hrefs.institution(institution.uri).home}
+            data-cy={"institution-link-" + institution.uri}
+          >
+            {children}
+          </Link>
+        )}
+      />
+    </Layout>
+  );
+};
 
 export default IndexPage;
 
 export const getStaticProps: GetStaticProps = async context => {
   return {
-    props: {institutions: Data.institutions},
+    props: {images: Data.images, institutions: Data.institutions},
   };
 };
