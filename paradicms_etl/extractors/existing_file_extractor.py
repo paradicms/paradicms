4fb0c4143aa3eedebeb2059c6f7d94c35aff30cf
Minor G <minorg@server.fake>
2020-09-30 18:37:59 -0400

    existing_csv_file_extractor -> existing_file_extractor

---
+++ paradicms_etl/extractors/existing_file_extractor.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from typing import Optional
+
+from paradicms_etl._extractor import _Extractor
+
+
+class ExistingFileExtractor(_Extractor):
+    def __init__(
+        self,
+        *,
+        file_name: Optional[str] = None,
+        file_path: Optional[Path] = None,
+        **kwds
+    ):
+        _Extractor.__init__(self, **kwds)
+        if file_path is not None:
+            self.__file_path = file_path
+        elif file_name is not None:
+            self.__file_path = self._extracted_data_dir_path / file_name
+        else:
+            raise ValueError("must specify file_name or file_path")
+
+    def extract(self, **kwds):
+        if not self.__file_path.is_file():
+            raise ValueError(str(self.__file_path) + " does not exist")
+        return {"file_path": self.__file_path}
