f292edbc7d902f7263c399bb95c284de3f2cf3d9
Minor G <minorg@server.fake>
2020-10-04 11:01:57 -0400

    gui_builder_test

--- paradicms_etl/loaders/gui/gui_builder.py
+++ paradicms_etl/loaders/gui/gui_builder.py
@@ -4,11 +4,11 @@ import subprocess
 import sys
 from datetime import datetime
 from pathlib import Path
-from typing import Union
+from typing import Optional, Union
 
 
 class GuiBuilder:
-    def __init__(self, *, data_dir_path: Path, gui: Union[str, Path]):
+    def __init__(self, *, gui: Union[str, Path]):
         """
         :param data_dir_path: path to the GUI's data directory, which will be passed to the build process as DATA_DIRECTORY_PATH
         :param gui: name of a gui (in gui/ of this repository) or path to a gui
@@ -16,30 +16,30 @@ class GuiBuilder:
 
         self.__logger = logging.getLogger(self.__class__.__name__)
 
-        self.__data_dir_path = data_dir_path
-
         if isinstance(gui, Path):
             gui_dir_path = gui
         elif os.path.isdir(gui):
             gui_dir_path = Path(self.__gui)
         else:
-            gui_dir_path = Path(__file__).parent.parent.parent.parent / "gui" / gui
+            gui_dir_path = (
+                Path(__file__).parent.parent.parent.parent.parent / "gui" / gui
+            )
         if not gui_dir_path.is_dir():
             raise ValueError(f"{gui_dir_path} does not exist")
 
         self.__gui_dir_path = gui_dir_path
 
-    def build(self) -> Path:
+    def build(self, data_dir_path: Path) -> Path:
         """
         Build the GUI
-        :return: directory path the built site
+        :return: directory path to the built site
         """
 
         self.__logger.info("building GUI")
-        self.__run_npm_script("build")
+        self.__run_npm_script("build", data_dir_path=data_dir_path)
         self.__logger.info("built GUI")
         self.__logger.info("exporting GUI build")
-        self.__run_npm_script("export")
+        self.__run_npm_script("export", data_dir_path=data_dir_path)
         self.__logger.info("exported GUI build")
 
         gui_out_dir_path = self.__gui_dir_path / "out"
@@ -51,16 +51,27 @@ class GuiBuilder:
 
         return gui_out_dir_path
 
-    def __clean_gui(self, gui_dir_path: Path):
-        self.__run_npm_script("clean", gui_dir_path=gui_dir_path)
+    def clean(self):
+        self.__run_npm_script("clean")
 
-    def __run_npm_script(self, script):
+    @property
+    def gui_dir_path(self) -> Path:
+        return self.__gui_dir_path
+
+    def install(self):
+        self.__run_npm_script("install")
+
+    def __run_npm_script(self, script, data_dir_path: Optional[Path] = None):
         subprocess_env = os.environ.copy()
-        subprocess_env["DATA_DIRECTORY_PATH"] = str(self.__data_dir_path)
-        self.__logger.info("using DATA_DIRECTORY_PATH = %s", self.__data_dir_path)
+        if data_dir_path is not None:
+            subprocess_env["DATA_DIRECTORY_PATH"] = str(data_dir_path)
+            self.__logger.info("using DATA_DIRECTORY_PATH = %s", data_dir_path)
         subprocess_env["EDITOR"] = ""
 
-        args = ["npm", "run", script]
+        args = ["npm"]
+        if script != "install":
+            args.append("run")
+        args.append(script)
         self.__logger.info("running %s", args)
         subprocess_ret = subprocess.call(
             args, cwd=str(self.__gui_dir_path), env=subprocess_env, shell=True,
---
+++ test/paradicms_etl_test/loaders/gui/gui_builder_test.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from typing import Tuple
+
+from paradicms_etl._model import _Model
+from paradicms_etl.loaders.gui.gui_builder import GuiBuilder
+from paradicms_etl.loaders.gui.gui_data_loader import GuiDataLoader
+
+GUI = "union"
+
+
+# def test_clean():
+#     GuiBuilder(gui=GUI).clean()
+
+
+def test_build(test_data_models: Tuple[_Model, ...], tmp_path):
+    gui_builder = GuiBuilder(gui=GUI)
+
+    if not (gui_builder.gui_dir_path / "node_modules").is_dir():
+        gui_builder.install()
+
+    gui_builder.clean()
+    assert not (gui_builder.gui_dir_path / "out").exists()
+
+    gui_data_loader = GuiDataLoader(
+        loaded_data_dir_path=Path(tmp_path), pipeline_id="test"
+    )
+    gui_data_loader.load(models=test_data_models)
+    gui_data_loader.flush()
+
+    gui_out_dir_path = gui_builder.build(data_dir_path=Path(tmp_path))
+    assert gui_out_dir_path == (gui_builder.gui_dir_path / "out")
+    assert gui_out_dir_path.is_dir()
+
+
+# def test_install():
+#     GuiBuilder(gui=GUI).install()
