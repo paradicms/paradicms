781e1a1c8af3e854a0c2c2b87e6576b80e22e61b
Minor G <minorg@server.fake>
2020-10-19 09:23:23 -0400

    collapse union components into Layout

--- union/components/BreadcrumbsProps.ts
+++
@@ -1,7 +0,0 @@
-import {Collection, Institution, Object} from "@paradicms/models";
-
-export interface BreadcrumbsProps {
-  collection?: Collection;
-  institution?: Institution;
-  object?: Object;
-}
--- union/components/Layout.tsx
+++ union/components/Layout.tsx
@@ -1,20 +1,34 @@
 import * as React from "react";
 import {
+  AppBar,
+  Breadcrumbs,
   Card,
   CardContent,
   CardHeader,
   Grid,
   makeStyles,
+  Toolbar,
+  Typography,
 } from "@material-ui/core";
 import Helmet from "react-helmet";
-import {Breadcrumbs} from "components/Breadcrumbs";
-import {BreadcrumbsProps} from "components/BreadcrumbsProps";
-import {Footer} from "@paradicms/material-ui";
-import {Navbar} from "components/Navbar";
+import {Footer, NavbarSearchForm} from "@paradicms/material-ui";
+import {Link} from "@paradicms/material-ui-next";
+import {Hrefs} from "lib/Hrefs";
+import {Collection, Institution, Object} from "@paradicms/models";
+import {useState} from "react";
 
 const useStyles = makeStyles(theme => ({
+  brand: {
+    marginRight: theme.spacing(4),
+  },
   breadcrumbs: {
     marginLeft: theme.spacing(4),
+  },
+  navLink: {
+    color: "white",
+    fontSize: "larger",
+    marginRight: theme.spacing(4),
+    textDecoration: "none",
   },
   card: {
     marginLeft: theme.spacing(4),
@@ -23,13 +37,107 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export const Layout: React.FunctionComponent<React.PropsWithChildren<{
-  breadcrumbs?: BreadcrumbsProps;
+  breadcrumbs?: {
+    collection?: Collection;
+    institution?: Institution;
+    object?: Object;
+  };
   cardTitle?: React.ReactNode;
   className?: string;
   documentTitle?: string;
   onSearch?: (text: string) => void;
-}>> = ({breadcrumbs, cardTitle, children, documentTitle, onSearch}) => {
+}>> = ({
+  breadcrumbs,
+  cardTitle,
+  children,
+  documentTitle,
+  onSearch: onSearchUserDefined,
+}) => {
   const classes = useStyles();
+
+  const breadcrumbNodes: React.ReactNode[] = [];
+  if (breadcrumbs) {
+    breadcrumbNodes.push(
+      <Link key="home" {...Hrefs.home}>
+        Home
+      </Link>
+    );
+    const {collection, institution, object} = breadcrumbs;
+    if (institution) {
+      breadcrumbNodes.push(
+        <Link key="institutions" {...Hrefs.home}>
+          Institutions
+        </Link>
+      );
+      breadcrumbNodes.push(
+        <Link
+          key={`institution-${institution.uri}`}
+          {...Hrefs.institution(institution.uri).home}
+        >
+          {institution.name}
+        </Link>
+      );
+
+      if (collection) {
+        breadcrumbNodes.push(
+          <Link key="collections" {...Hrefs.institution(institution.uri).home}>
+            Collections
+          </Link>
+        );
+        breadcrumbNodes.push(
+          <Link
+            key={`collection-${collection.uri}`}
+            {...Hrefs.institution(institution.uri).collection(collection.uri)
+              .home}
+          >
+            {collection.title}
+          </Link>
+        );
+      }
+
+      if (object) {
+        if (collection) {
+          breadcrumbNodes.push(
+            <Link
+              key="objects"
+              {...Hrefs.institution(institution.uri)
+                .collection(collection.uri)
+                .objects()}
+            >
+              Objects
+            </Link>
+          );
+        } else {
+          breadcrumbNodes.push(<span key="objects">Objects</span>);
+        }
+        breadcrumbNodes.push(
+          <Link
+            key={`object-${object.uri}`}
+            {...Hrefs.institution(institution.uri).object(object.uri)}
+          >
+            {object.title}
+          </Link>
+        );
+      }
+    }
+  }
+
+  const [redirectToSearchText, setRedirectToSearchText] = useState<
+    string | null
+  >(null);
+
+  // @ts-ignore
+  let onSearch: (text: string) => void;
+  if (onSearchUserDefined) {
+    onSearch = onSearchUserDefined;
+  } else {
+    onSearch = setRedirectToSearchText;
+    if (redirectToSearchText) {
+      window.location.href = Hrefs.search({
+        text: redirectToSearchText,
+      }).href.toString();
+    }
+  }
 
   return (
     <>
@@ -48,15 +156,23 @@ export const Layout: React.FunctionComponent<React.PropsWithChildren<{
       />
       <Grid data-cy="frame" container direction="column" spacing={2}>
         <Grid item>
-          <Navbar onSearch={onSearch} />
+          <AppBar data-cy="navbar" position="static">
+            <Toolbar>
+              <Typography variant="h6" className={classes.brand}>
+                Paradicms
+              </Typography>
+              <Link className={classes.navLink} {...Hrefs.home}>
+                Home
+              </Link>
+              <NavbarSearchForm onSearch={onSearch} />
+            </Toolbar>
+          </AppBar>
         </Grid>
         {breadcrumbs ? (
-          <Grid item data-cy="Fixtures-row">
-            <Breadcrumbs
-              className={classes.breadcrumbs}
-              data-cy="Fixtures"
-              {...breadcrumbs}
-            />
+          <Grid item>
+            <Breadcrumbs className={classes.breadcrumbs} data-cy="breadcrumbs">
+              {breadcrumbNodes}
+            </Breadcrumbs>
           </Grid>
         ) : null}
         <Grid item>
