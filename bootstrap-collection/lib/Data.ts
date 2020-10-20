21d7d0442a6a01b1876d718a5e91f5c642b486d1
Minor G <minorg@server.fake>
2020-10-20 19:04:23 -0400

    Data for bootstrap-collection

--- bootstrap-collection/lib/Data.ts
+++ bootstrap-collection/lib/Data.ts
@@ -1,34 +1,56 @@
 import fs from "fs";
 import * as path from "path";
-import {Image, Object, PropertyDefinition} from "@paradicms/models";
+import {Collection, Image, Object, PropertyDefinition} from "@paradicms/models";
 
 export class Data {
-  private static findDataDirectory(): string {
+  constructor() {
     let dataDirectoryPath: string | undefined = process.env.DATA_DIRECTORY_PATH;
     if (!dataDirectoryPath) {
       throw new EvalError("must specify a data directory path");
     }
     console.info("using data directory ", dataDirectoryPath);
-    return dataDirectoryPath;
+
+    const collections = Data.getModels<Collection>(
+      dataDirectoryPath,
+      "collection"
+    );
+    if (collections.length === 0) {
+      throw new EvalError("no collections");
+    }
+    this.collection = collections[0];
+
+    this.objects = Data.getModels<Object>(
+      dataDirectoryPath,
+      "object"
+    ).filter(object =>
+      object.collectionUris.some(
+        collectionUri => collectionUri === this.collection.uri
+      )
+    );
+
+    const objectUris = new Set<string>(this.objects.map(object => object.uri));
+    this.images = Data.getModels<Image>(
+      dataDirectoryPath,
+      "image"
+    ).filter(image => objectUris.has(image.depictsUri));
+
+    this.propertyDefinitions = Data.getModels<PropertyDefinition>(
+      dataDirectoryPath,
+      "propertyDefinition"
+    );
   }
 
-  private static readonly dataDirectoryPath = Data.findDataDirectory();
+  readonly collection: Collection;
+  readonly images: readonly Image[];
+  readonly objects: readonly Object[];
+  readonly propertyDefinitions: readonly PropertyDefinition[];
 
-  static get images(): readonly Image[] {
-    return Data.getModels<Image>("image");
-  }
-
-  private static getModels<ModelT>(fileBaseName: string): readonly ModelT[] {
-    const filePath = path.join(Data.dataDirectoryPath, fileBaseName + ".json");
+  private static getModels<ModelT>(
+    dataDirectoryPath: string,
+    fileBaseName: string
+  ): readonly ModelT[] {
+    const filePath = path.join(dataDirectoryPath, fileBaseName + ".json");
     const fileContents = fs.readFileSync(filePath, "utf8");
     return JSON.parse(fileContents);
-  }
-
-  static get objects(): readonly Object[] {
-    return Data.getModels<Object>("object");
-  }
-
-  static get propertyDefinitions(): readonly PropertyDefinition[] {
-    return Data.getModels<PropertyDefinition>("propertyDefinition");
   }
 }
