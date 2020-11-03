ff47f97a1fda58af96fcb7bd56a70447d4113e7c
Minor G <minorg@server.fake>
2020-11-03 18:13:45 -0500

    first sketch of a GuiMetadata model

---
+++ paradicms_etl/models/gui_metadata.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from dataclasses_json import LetterCase, dataclass_json
+
+from paradicms_etl._model import _Model
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass(frozen=True)
+class GuiMetadata(_Model):
+    document_description: Optional[str] = None
+    document_title: Optional[str] = None
+    navbar_title: Optional[str] = None
