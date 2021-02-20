ee560b97786210a2e4d498a2d4c02f10e57ad6ce
Minor G <minorg@server.fake>
2021-02-20 18:19:27 -0500

    PropertyDefinition.to_rdf, regenerate test data

--- paradicms_etl/models/property_definition.py
+++ paradicms_etl/models/property_definition.py
@@ -2,8 +2,11 @@ from dataclasses import dataclass
 from typing import Optional
 
 from dataclasses_json import LetterCase, dataclass_json
+from rdflib import Graph, Literal, RDF, RDFS
+from rdflib.resource import Resource
 
 from paradicms_etl.models._named_model import _NamedModel
+from paradicms_etl.namespace import CMS
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
@@ -12,3 +15,15 @@ class PropertyDefinition(_NamedModel):
     label: str
     faceted: Optional[bool] = None
     full_text_searchable: Optional[bool] = None
+
+    def to_rdf(self, *, graph: Graph, property_definitions) -> Resource:
+        resource = _NamedModel.to_rdf(
+            self, graph=graph, property_definitions=property_definitions
+        )
+        resource.add(RDF.type, CMS[self.__class__.__name__])
+        resource.add(RDFS.label, Literal(self.label))
+        if self.faceted:
+            resource.add(CMS.faceted, Literal(self.faceted))
+        if self.full_text_searchable:
+            resource.add(CMS.fullTextSearchable, Literal(self.full_text_searchable))
+        return resource
