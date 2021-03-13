4ad111e5191bf40c02e240c513f6ae20a806d4d2
Minor G <minorg@server.fake>
2021-03-13 10:30:36 -0500

    remove old readModels

--- material-ui-union/lib/Data.ts
+++ material-ui-union/lib/Data.ts
@@ -1,19 +1,8 @@
 import fs from "fs";
-import * as path from "path";
 import {graph, IndexedFormula, parse} from "rdflib";
 import {RdfData} from "@paradicms/rdf";
 
 export class Data extends RdfData {
-  private static findDataDirectory(): string {
-    let dataDirectoryPath: string | undefined = process.env.DATA_DIRECTORY_PATH;
-    if (!dataDirectoryPath) {
-      throw new EvalError("must specify a data directory path");
-    }
-    // console.info("using data directory ", dataDirectoryPath);
-    return dataDirectoryPath;
-  }
-
-  private static readonly dataDirectoryPath = Data.findDataDirectory();
   // private static instanceCount = 0;
   static readonly instance = new Data();
 
@@ -36,13 +25,5 @@ export class Data extends RdfData {
       "text/turtle"
     );
     return store;
-  }
-
-  protected readModels<ModelT>(fileBaseName: string): readonly ModelT[] {
-    const filePath = path.join(Data.dataDirectoryPath, fileBaseName + ".json");
-    const fileContents = fs.readFileSync(filePath, "utf8");
-    const models: ModelT[] = JSON.parse(fileContents);
-    // console.info("read", models.length, "models from", filePath);
-    return models;
   }
 }
