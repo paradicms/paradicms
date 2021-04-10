6e334bfbe7d575662a54d1040df185aaa5afa250
Minor G <minorg@server.fake>
2021-04-10 14:53:07 -0400

    change OmekaSExtractor to use urllib.request

--- paradicms_etl/extractors/omeka_s_extractor.py
+++ paradicms_etl/extractors/omeka_s_extractor.py
@@ -1,7 +1,7 @@
 import json
 from typing import Dict
+from urllib.request import urlopen
 
-import requests
 from pathvalidate import sanitize_filename
 
 from paradicms_etl._extractor import _Extractor
@@ -12,26 +12,30 @@ class OmekaSExtractor(_Extractor):
         _Extractor.__init__(self)
         self.__endpoint_url = endpoint_url.rstrip("/")
 
-    def __extract_all_pages(self, session: requests.Session, url: str):
+    def __extract_all_pages(self, url: str):
         resources = []
         while True:
             self._logger.info("getting %s", url)
-            response = session.get(url)
-            page_resources = response.json()
+            with urlopen(url) as response:
+                page_resources = json.loads(response.read())
+                link_header = response.headers.get("Link")
             assert isinstance(page_resources, list)
-            self._logger.info("retrieved %d resources from %s", len(page_resources), url)
+            self._logger.info(
+                "retrieved %d resources from %s", len(page_resources), url
+            )
             resources.extend(page_resources)
-            link_header = response.headers.get("Link")
             next_url = None
             if link_header:
                 for link_header in link_header.split(","):
-                    link_header_parts = [part.strip() for part in link_header.rsplit(";", 1)]
+                    link_header_parts = [
+                        part.strip() for part in link_header.rsplit(";", 1)
+                    ]
                     if len(link_header_parts) != 2:
                         continue
                     link_url, link_rel = link_header_parts
-                    if not link_rel.startswith("rel=\"") or not link_rel.endswith("\""):
+                    if not link_rel.startswith('rel="') or not link_rel.endswith('"'):
                         continue
-                    link_rel = link_rel[len("rel=\""):-1].lower()
+                    link_rel = link_rel[len('rel="') : -1].lower()
                     if not link_url.startswith("<") or not link_url.endswith(">"):
                         continue
                     link_url = link_url[1:-1]
@@ -44,8 +48,6 @@ class OmekaSExtractor(_Extractor):
             url = next_url
 
     def extract(self, *, force: bool):
-        session = requests.Session()
-
         api_context = None  # Retrieve lazily
         api_context_url = self.__endpoint_url + "-context"
 
@@ -67,19 +69,19 @@ class OmekaSExtractor(_Extractor):
                     resources = json.load(file_)
 
             if resources is None:
-                resources = self.__extract_all_pages(session=session, url=url)
+                resources = self.__extract_all_pages(url=url)
                 for resource in resources:
                     # Replace the @context reference with the resolved context
                     assert resource["@context"] == api_context_url
                     if api_context is None:
-                        api_context = self.__get_api_context(session=session, url=api_context_url)
+                        api_context = self.__get_api_context(url=api_context_url)
                     resource["@context"] = api_context
                 with open(file_path, "w+") as file_:
                     json.dump(resources, file_)
             results[resources_name] = resources
         return results
 
-    def __get_api_context(self, *, session: requests.Session, url: str) -> Dict[str, object]:
-        response = session.get(url)
-        response_json = response.json()
+    def __get_api_context(self, *, url: str) -> Dict[str, object]:
+        with urlopen(url) as response:
+            response_json = json.loads(response.read())
         return response_json["@context"]
