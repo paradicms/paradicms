80f84bc9f3484947818c28b607dc32b62b7b2551
Minor G <minorg@server.fake>
2021-04-13 20:12:06 -0400

    delete is_uri

--- test/paradicms_etl_test/utils/is_uri_test.py
+++
@@ -1,8 +0,0 @@
-from paradicms_etl.utils.is_uri import is_uri
-
-
-def test_is_uri():
-    assert is_uri("http://example.com")
-    assert is_uri("https://example.com")
-    assert not is_uri("some other string")
-    assert not is_uri(" http://example.com")
