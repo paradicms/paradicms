c2033fe6f71188c9458ecc0709c0e9a4103f3586
Minor G <minorg@server.fake>
2021-05-08 22:21:29 -0400

    adapting unit test

--- test/paradicms_etl_test/transformers/markdown_directory_transformer_test.py
+++ test/paradicms_etl_test/transformers/markdown_directory_transformer_test.py
@@ -1,6 +1,10 @@
+from rdflib import DCTERMS, FOAF, Literal
+
 from paradicms_etl.models._named_model import _NamedModel
 from paradicms_etl.models.object import Object
+from paradicms_etl.models.opaque_named_model import OpaqueNamedModel
 from paradicms_etl.models.person import Person
+from paradicms_etl.namespace import CMS
 from paradicms_etl.transformers.markdown_directory_transformer import (
     MarkdownDirectoryTransformer,
 )
@@ -23,12 +27,26 @@ def test_transform():
     for model in models:
         assert isinstance(model, _NamedModel), type(model)
 
-    objects = [model for model in models if isinstance(model, Object)]
+    objects = [
+        model
+        for model in models
+        if isinstance(model, OpaqueNamedModel) and model.type == CMS.Object
+    ]
     assert len(objects) == 1
     object_ = objects[0]
-    assert object_.title == "Test object"
+    assert any(
+        property_.uri == DCTERMS.title and property_.value == Literal("Test object")
+        for property_ in object_.properties
+    )
 
-    people = [model for model in models if isinstance(model, Person)]
+    people = [
+        model
+        for model in models
+        if isinstance(model, OpaqueNamedModel) and model.type == CMS.Person
+    ]
     assert len(people) == 1
     person = people[0]
-    assert person.name == "Test person"
+    assert any(
+        property_.uri == FOAF.name and property_.value == Literal("Test person")
+        for property_ in person.properties
+    )
