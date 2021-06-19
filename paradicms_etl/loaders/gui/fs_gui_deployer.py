7f569ccf3481db62cfd1ff6764720c3dd6aa25c5
Minor G <minorg@server.fake>
2021-06-19 09:44:31 -0400

    FsGuiDeployer: overwrite should not clean

--- paradicms_etl/loaders/gui/fs_gui_deployer.py
+++ paradicms_etl/loaders/gui/fs_gui_deployer.py
@@ -14,20 +14,27 @@ class FsGuiDeployer(_GuiDeployer):
         If a previous deployment is at that path, the previous deployment is archived
         in a timestamped subdirectory of gui_deploy_dir_path.
     Else:
-        Delete the GUI output directory and recreate it with the new contents.
+        Overwrite the contents of the GUI output directory.
     """
 
     def __init__(
-        self, *, archive: bool = False, copy: bool = False, gui_deploy_dir_path: Path
+        self,
+        *,
+        archive: bool = False,
+        clean: bool = False,
+        copy: bool = False,
+        gui_deploy_dir_path: Path,
     ):
         """
         :param archive: archive old deployments to the same gui_deploy_dir_path rather than overwriting
+        :param clean: if archive is False, delete the existing gui_deploy_dir_path before populating it
         :param copy: copy gui_dir_path to gui_deploy_dir_path rather than renaming; used if a rename would cross mounts ("Invalid cross-device link")
         :param gui_deploy_dir_path: directory to "deploy" the GUI to
         """
 
         _GuiDeployer.__init__(self)
         self.__archive = archive
+        self.__clean = clean
         self.__copy = copy
         self.__gui_deploy_dir_path = gui_deploy_dir_path
 
@@ -50,18 +57,17 @@ class FsGuiDeployer(_GuiDeployer):
             else:
                 self.__gui_deploy_dir_path.mkdir(exist_ok=True)
         else:
-            if self.__gui_deploy_dir_path.is_dir():
-                self._logger.info("deleting existing %s", self.__gui_deploy_dir_path)
-                rmtree(self.__gui_deploy_dir_path)
             current_gui_deploy_dir_path = self.__gui_deploy_dir_path
 
-        assert not current_gui_deploy_dir_path.exists(), current_gui_deploy_dir_path
+        if self.__clean and current_gui_deploy_dir_path.is_dir():
+            self._logger.info("deleting existing %s", current_gui_deploy_dir_path)
+            rmtree(current_gui_deploy_dir_path)
 
         if self.__copy:
             self._logger.info(
                 "copying %s to %s", gui_out_dir_path, current_gui_deploy_dir_path
             )
-            copytree(gui_out_dir_path, current_gui_deploy_dir_path)
+            copytree(gui_out_dir_path, current_gui_deploy_dir_path, dirs_exist_ok=True)
         else:
             self._logger.info(
                 "renaming %s to %s", gui_out_dir_path, current_gui_deploy_dir_path
