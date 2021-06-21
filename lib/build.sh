a11bc6e34769eca36d08c7a0e5b2c92a0225af6b
Minor G <minorg@server.fake>
2021-06-21 19:33:43 -0400

    build.sh parallelized

--- lib/build.sh
+++ lib/build.sh
@@ -1,32 +1,69 @@
 #!/bin/bash
 
-set -e
+set -eu
 
 cd "$(dirname "$0")"
 
-cd base
-npm run build
+# Run parallel commands and fail if any of them fails.
+pids=()
 
+# No dependencies
+cd base
+npm run build &
+pids+=($!)
+
+# No dependencies
+cd ../next
+npm run build &
+pids+=($!)
+
+for pid in "${pids[@]}"; do
+  wait "$pid"
+done
+
+
+# Depends on base
 cd ../models
 npm run build
 
+
+# Depends on models
 cd ../model-utils
 npm run build
 
-cd ../rdf
-npm run build
 
-cd ../bootstrap
-npm run build
-
+pids=()
+# Depend on models and model-utils
 cd ../lunr
-npm run build
+npm run build &
+pids+=($!)
 
+# Depend on models and model-utils
+cd ../rdf
+npm run build &
+pids+=($!)
+
+for pid in "${pids[@]}"; do
+  wait "$pid"
+done
+
+
+pids=()
+# Depend on models, model-utils, and lunr
+cd ../bootstrap
+npm run build &
+pids+=($!)
+
+# Depend on models, model-utils, and lunr
 cd ../material-ui
-npm run build
+npm run build &
+pids+=($!)
 
-cd ../next
-npm run build
+for pid in "${pids[@]}"; do
+  wait "$pid"
+done
 
+
+# Depends on material-ui and next
 cd ../material-ui-next
 npm run build
