fdb5aba68e51e3b0dab45788ccce5f51018974d7
Minor G <minorg@server.fake>
2021-06-27 08:48:06 -0400

    test markdown_directory loading, output data

---
+++ test/paradicms_etl_test/pipelines/markdown_directory_pipeline_test.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+from paradicms_etl._pipeline import _Pipeline
+from paradicms_etl.extractors.markdown_directory_extractor import (
+    MarkdownDirectoryExtractor,
+)
+from paradicms_etl.loaders.rdf_file_loader import RdfFileLoader
+from paradicms_etl.transformers.markdown_directory_transformer import (
+    MarkdownDirectoryTransformer,
+)
+
+
+class MarkdownDirectoryPipeline(_Pipeline):
+    ID = "markdown_directory"
+
+    def __init__(self, data_dir_path: Path):
+        _Pipeline.__init__(
+            self,
+            extractor=MarkdownDirectoryExtractor(
+                data_dir_path=data_dir_path, pipeline_id=self.ID
+            ),
+            id=self.ID,
+            transformer=MarkdownDirectoryTransformer(pipeline_id=self.ID),
+            loader=RdfFileLoader(data_dir_path=data_dir_path, pipeline_id=self.ID),
+        )
+
+
+def test_extract_transform_load():
+    data_dir_path = Path(__file__).absolute().parent.parent.parent.parent / "data"
+    data_ttl_file_path = (
+        data_dir_path
+        / MarkdownDirectoryPipeline.ID
+        / "loaded"
+        / "markdown_directory.ttl"
+    )
+    data_ttl_file_path.unlink(missing_ok=True)
+
+    MarkdownDirectoryPipeline(data_dir_path=data_dir_path).extract_transform_load(
+        force_extract=False
+    )
+
+    assert data_ttl_file_path.is_file(), data_ttl_file_path
