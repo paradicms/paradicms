c1be4e9a1c15b76b2d0c20910beadef25ac71b74
Minor G <minorg@server.fake>
2021-07-03 17:10:05 -0400

    test for the last change

--- test/paradicms_etl_test/loaders/gui/gui_images_loader_test.py
+++ test/paradicms_etl_test/loaders/gui/gui_images_loader_test.py
@@ -14,3 +14,7 @@ def test_load(test_data_original_images: Tuple[Image, ...], tmp_path):
     )
     loaded_images = tuple(loader.load(models=test_data_original_images))
     assert len(loaded_images) == len(test_data_original_images) * 3
+    assert all(isinstance(loaded_image, Image) for loaded_image in loaded_images)
+    assert len(set(loaded_image.uri for loaded_image in loaded_images)) == len(
+        loaded_images
+    )
