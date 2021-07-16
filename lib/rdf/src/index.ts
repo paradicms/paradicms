60b96a163515cec37c1482f5dcc24b6a9d434f46
Minor G <minorg@server.fake>
2021-07-16 19:36:50 -0400

    export everything

--- lib/rdf/src/index.ts
+++ lib/rdf/src/index.ts
@@ -1,2 +1,16 @@
-export * from "./vocabularies";
+export * from "./CollectionRdfReader";
+export * from "./GuiMetadataRdfReader";
+export * from "./ImageRdfReader";
+export * from "./InstitutionRdfReader";
+export * from "./LicenseRdfReader";
+export * from "./LiteralWrapper";
+// Skip ModelNode
+export * from "./ModelRdfReader";
+export * from "./Namespace";
+export * from "./ObjectRdfReader";
+export * from "./PropertyDefinitionRdfReader";
 export * from "./RdfData";
+export * from "./RdfReaderException";
+export * from "./RightsRdfReader";
+export * from "./RightsStatementRdfReader";
+export * from "./vocabularies";
