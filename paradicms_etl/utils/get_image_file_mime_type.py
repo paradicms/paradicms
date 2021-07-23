42b8d9bbe848c52995bfa81d55afc0d5493a5b19
Minor G <minorg@server.fake>
2021-07-22 20:48:12 -0400

    initial move of files

--- paradicms_etl/utils/get_image_file_mime_type.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from PIL import Image
-
-
-def get_image_file_mime_type(image_file_path: Path):
-    with Image.open(str(image_file_path)) as image:
-        image_format = image.format
-    return "image/" + image_format.lower()
