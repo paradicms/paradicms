dcb88340fd73a2ec34f84c97b28bd21203843216
Minor G <minorg@server.fake>
2021-07-23 22:06:08 -0400

    fix test

--- lib/rdf/test/parseTestData.ts
+++ lib/rdf/test/parseTestData.ts
@@ -3,7 +3,7 @@ import {Parser, Store} from "n3";
 
 export const parseTestData = (): Store => {
   const ttl = fs
-    .readFileSync(process.env.DATA_TTL_FILE_PATH).toString();
+    .readFileSync(process.env.DATA_TTL_FILE_PATH!).toString();
   const parser = new Parser({format: "text/turtle"});
   const store = new Store();
   store.addQuads(parser.parse(ttl));
