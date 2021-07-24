74099dcd5e3387a6b7b6983c3b1518e206bb6bfe
Minor G <minorg@server.fake>
2021-07-24 17:48:41 -0400

    new *Facet models

---
+++ lib/models/src/Facet.ts
@@ -0,0 +1,6 @@
+import {FacetType} from "./FacetType";
+
+export interface Facet {
+  readonly label: string;
+  readonly type: FacetType;
+}
---
+++ lib/models/src/defaultConfiguration.ts
@@ -0,0 +1,7 @@
+import {Configuration} from "./Configuration";
+
+export const defaultConfiguration: Configuration = {
+  bootstrapStylesheetHref: "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css",
+  documentTitle: null,
+  navbarTitle: null,
+}
