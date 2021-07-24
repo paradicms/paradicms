46955da1c54665215041665d3973b40aae937276
Minor G <minorg@server.fake>
2021-07-23 21:55:20 -0400

    fix parseTestData

--- lib/rdf/test/parseTestData.ts
+++ lib/rdf/test/parseTestData.ts
@@ -1,24 +1,9 @@
 import * as fs from "fs";
-import * as path from "path";
 import {Parser, Store} from "n3";
 
 export const parseTestData = (): Store => {
   const ttl = fs
-    .readFileSync(
-      path.join(
-        __dirname,
-        "..",
-        "..",
-        "..",
-        "..",
-        "etl",
-        "data",
-        "test_data",
-        "loaded",
-        "data.ttl",
-      ),
-    )
-    .toString();
+    .readFileSync(process.env.DATA_TTL_FILE_PATH).toString();
   const parser = new Parser({format: "text/turtle"});
   const store = new Store();
   store.addQuads(parser.parse(ttl));
