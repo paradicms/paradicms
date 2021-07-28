a971f9f96ed22653a3e5d4c99b5cc18c18462a24
Minor G <minorg@server.fake>
2021-07-28 19:53:04 -0400

    DatasetRdfReader test

---
+++ lib/rdf/test/DatasetRdfReader.test.ts
@@ -0,0 +1,26 @@
+import {describe} from "mocha";
+import {parseTestData} from "./parseTestData";
+import {DatasetRdfReader} from "../src/DatasetRdfReader";
+import {Store} from "n3";
+import {expect} from "chai";
+
+describe("Dataset RDF reader", () => {
+  let store: Store;
+
+  before(function(this: any) {
+    // @ts-ignore
+    this.timeout(10000);
+    store = parseTestData();
+  });
+
+  it("should read a dataset from the store", () => {
+    const model = DatasetRdfReader.read(store);
+    expect(model.collections).to.not.be.empty;
+    expect(model.images).to.not.be.empty;
+    expect(model.institutions).to.not.be.empty;
+    expect(model.licenses).to.not.be.empty;
+    expect(model.objects).to.not.be.empty;
+    expect(model.propertyDefinitions).to.not.be.empty;
+    expect(model.rightsStatements).to.not.be.empty;
+  });
+});
