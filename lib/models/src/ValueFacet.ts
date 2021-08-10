af60b6dfe37b70ee59469a56644c905fb21dabd5
Minor G <minorg@server.fake>
2021-08-10 19:51:00 -0400

    documentation for ValueFacet and ValueFilter

--- lib/models/src/ValueFacet.ts
+++ lib/models/src/ValueFacet.ts
@@ -2,7 +2,21 @@ import {ValueFacetValue} from "./ValueFacetValue";
 import {Facet} from "./Facet";
 import {PrimitiveType} from "./PrimitiveType";
 
+/**
+ * Facet that creates a histogram of distinct values (model, property, value).
+ *
+ * Examples on Object include institution, collections, or subject terms.
+ *
+ * The naming follows that of Lucene/ElasticSearch (value and range facets).
+ */
 export interface ValueFacet<T extends PrimitiveType> extends Facet {
+  /**
+   * Count of models that do not have a value for the property.
+   */
   readonly unknownCount: number;
+
+  /**
+   * Histogram of distinct values.
+   */
   readonly values: readonly ValueFacetValue<T>[];
 }
--- lib/models/src/ValueFilter.ts
+++ lib/models/src/ValueFilter.ts
@@ -1,8 +1,40 @@
 import {Filter} from "./Filter";
 import {PrimitiveType} from "./PrimitiveType";
 
+/**
+ * Filter that excludes/includes specific values where (model, filtered property, value).
+ *
+ * This filter is a companion to ValueFacet, which describes the universe of possible values for a given property.
+ *
+ * If none of exclude* or include* is set, then all models are implicitly included (i.e., the filter is a no-op).
+ */
 export interface ValueFilter<T extends PrimitiveType> extends Filter {
-  readonly excludeUnknown?: boolean; // undefined = false / include unknown
+  /**
+   * If true, exclude all models that have the filtered property.
+   *
+   * This is shorthand for excludeValues with all values from the universe.
+   */
+  readonly excludeKnown?: boolean;
+
+  /**
+   * If true, exclude all models that do not have the filtered property.
+   * If false or undefined, include those models.
+   */
+  readonly excludeUnknown?: boolean;
+
+  /**
+   * Exclude models that have the filtered property with the given value.
+   *
+   * Any model that does not have the filtered property with the given value is implicitly included.
+   *
+   * excludeValues has precedence over includeValues.
+   */
   readonly excludeValues?: readonly T[];
+
+  /**
+   * Include models that have the filtered property with the given value.
+   *
+   * Any model that does not have the filtered property with the given value is implicitly excluded.
+   */
   readonly includeValues?: readonly T[];
 }
