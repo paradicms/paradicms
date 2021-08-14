6adbc98b234a2b0d7c7c315a6a57c2cf2119b8ec
Minor G <minorg@server.fake>
2021-08-14 11:47:19 -0400

    PropertyDefinition: remove faceted and full_text_searchable

--- paradicms_etl/models/dublin_core_property_definitions.py
+++ paradicms_etl/models/dublin_core_property_definitions.py
@@ -8,34 +8,26 @@ class DublinCorePropertyDefinitions(_ModelSingletons):
     _MODEL_CLASS = PropertyDefinition
 
     ABSTRACT = PropertyDefinition(
-        full_text_searchable=True,
         label="Abstract",
         uri=DCTERMS.abstract,
     )
     ALTERNATIVE_TITLE = PropertyDefinition(
-        full_text_searchable=True,
         label="Alternative title",
         uri=DCTERMS.alternative,
     )
     BIBLIOGRAPHIC_CITATION = PropertyDefinition(
-        full_text_searchable=True,
         label="Bibliographic citation",
         uri=DCTERMS.bibliographicCitation,
     )
     CONTRIBUTOR = PropertyDefinition(
-        full_text_searchable=True,
         label="Contributor",
         uri=DCTERMS.contributor,
     )
     COVERAGE = PropertyDefinition(
-        faceted=True,
-        full_text_searchable=True,
         label="Coverage",
         uri=DCTERMS.coverage,
     )
     CREATOR = PropertyDefinition(
-        faceted=True,
-        full_text_searchable=True,
         label="Creator",
         uri=DCTERMS.creator,
     )
@@ -49,23 +41,19 @@ class DublinCorePropertyDefinitions(_ModelSingletons):
         uri=DCTERMS.dateSubmitted,
     )
     DESCRIPTION = PropertyDefinition(
-        full_text_searchable=True,
         label="Description",
         uri=DCTERMS.description,
     )
     EXTENT = PropertyDefinition(
-        full_text_searchable=True,
         label="Extent",
         uri=DCTERMS.extent,
     )
     FORMAT = PropertyDefinition(
         label="Format",
-        full_text_searchable=True,
         uri=DCTERMS["format"],
     )
     IDENTIFIER = PropertyDefinition(
         label="Identifier",
-        full_text_searchable=True,
         uri=DCTERMS.identifier,
     )
     IS_REFERENCED_BY = PropertyDefinition(
@@ -73,7 +61,6 @@ class DublinCorePropertyDefinitions(_ModelSingletons):
         uri=DCTERMS.isReferencedBy,
     )
     LANGUAGE = PropertyDefinition(
-        faceted=True,
         label="Language",
         uri=DCTERMS.language,
     )
@@ -82,19 +69,14 @@ class DublinCorePropertyDefinitions(_ModelSingletons):
         uri=DCTERMS.license,
     )
     MEDIUM = PropertyDefinition(
-        faceted=True,
-        full_text_searchable=True,
         label="Medium",
         uri=DCTERMS.medium,
     )
     PROVENANCE = PropertyDefinition(
-        full_text_searchable=True,
         label="Provenance",
         uri=DCTERMS.provenance,
     )
     PUBLISHER = PropertyDefinition(
-        faceted=True,
-        full_text_searchable=True,
         label="Publisher",
         uri=DCTERMS.publisher,
     )
@@ -112,36 +94,26 @@ class DublinCorePropertyDefinitions(_ModelSingletons):
         uri=DCTERMS.rightsHolder,
     )
     SOURCE = PropertyDefinition(
-        full_text_searchable=True,
         label="Source",
         uri=DCTERMS.source,
     )
     SPATIAL = PropertyDefinition(
-        faceted=True,
-        full_text_searchable=True,
         label="Spatial coverage",
         uri=DCTERMS.spatial,
     )
     SUBJECT = PropertyDefinition(
-        faceted=True,
-        full_text_searchable=True,
         label="Subject",
         uri=DCTERMS.subject,
     )
     TEMPORAL = PropertyDefinition(
-        faceted=True,
-        full_text_searchable=True,
         label="Temporal coverage",
         uri=DCTERMS.temporal,
     )
     TITLE = PropertyDefinition(
         label="Title",
-        full_text_searchable=True,
         uri=DCTERMS.title,
     )
     TYPE = PropertyDefinition(
-        faceted=True,
-        full_text_searchable=True,
         label="Type",
         uri=DCTERMS.type,
     )
