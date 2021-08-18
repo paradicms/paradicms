5f4b8d391be221d7afef5a4e4be47994bc6a0e85
Minor G <minorg@server.fake>
2021-08-17 20:27:48 -0400

    JoinedPropertyDefinition test

---
+++ lib/models/test/JoinedPropertyDefinition.test.ts
@@ -0,0 +1,27 @@
+import {expect} from "chai";
+import {testDataset} from "./testDataset";
+import {JoinedDataset} from "../src/JoinedDataset";
+import {IndexedDataset} from "../src";
+
+describe("JoinedPropertyDefinition", () => {
+  const propertyDefinition = new IndexedDataset(
+    testDataset
+  ).propertyDefinitionByUri(
+    testDataset.propertyValueDefinitions[0].propertyUri
+  )!;
+  const sut = JoinedDataset.fromDataset(testDataset).propertyDefinitionByUri(
+    propertyDefinition.uri
+  )!;
+
+  it("should get the label", () => {
+    expect(sut.label!).to.not.be.empty;
+  });
+
+  it("should get the URI", () => {
+    expect(sut.uri).to.eq(propertyDefinition.uri);
+  });
+
+  it("should get associated values", () => {
+    expect(sut.values).to.not.be.empty;
+  });
+});
