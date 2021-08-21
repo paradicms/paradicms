b41820f3886730a0a87c7007f5f4e7c98a92a87b
Minor G <minorg@server.fake>
2021-08-21 13:41:42 -0400

    PropertyValueDefinition can have 1+ property URIs

--- paradicms_etl/models/property_value_definition.py
+++ paradicms_etl/models/property_value_definition.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Tuple
 
 from rdflib import Graph
 from rdflib.namespace import RDF, RDFS
@@ -11,15 +11,20 @@ from paradicms_etl.models._named_model import _NamedModel
 
 @dataclass(frozen=True)
 class PropertyValueDefinition(_NamedModel):
-    property_uri: URIRef  # Child -> parent reference
+    property_uris: Tuple[URIRef, ...]  # Child -> parent references
     value: Node  # A property value
     label: Optional[str] = None
+
+    def __post_init__(self):
+        if not self.property_uris:
+            raise ValueError("must specify at least one property URI")
 
     def to_rdf(self, *, graph: Graph) -> Resource:
         resource = _NamedModel.to_rdf(self, graph=graph)
         if self.label is not None:
             resource.add(RDFS.label, Literal(self.label))
-        # Not kosher use of rdf:predicate, but I couldn't find a better fit
-        resource.add(RDF.predicate, self.property_uri)
+        for property_uri in self.property_uris:
+            # Not kosher use of rdf:predicate, but I couldn't find a better fit
+            resource.add(RDF.predicate, property_uri)
         resource.add(RDF.value, self.value)
         return resource
