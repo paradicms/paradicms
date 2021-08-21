3880ac2731aa2715d39eb24bfb868c53f66c9b8a
Minor G <minorg@server.fake>
2021-08-21 16:17:50 -0400

    only need to specify filters

--- lib/models/src/ObjectSearchConfiguration.ts
+++ lib/models/src/ObjectSearchConfiguration.ts
@@ -1,8 +1,6 @@
-import {Facet} from "./Facet";
 import {Filter} from "./Filter";
 
 export interface ObjectSearchConfiguration {
-  readonly facets: readonly Facet[];
   readonly filters: readonly Filter[];
-  readonly fullTextSearchablePropertyUris: readonly string[];
+  readonly searchablePropertyUris: readonly string[];
 }
--- lib/models/src/defaultConfiguration.ts
+++ lib/models/src/defaultConfiguration.ts
@@ -1,13 +1,9 @@
 import {Configuration} from "./Configuration";
-import {StringPropertyValueFacet} from "StringPropertyValueFacet";
-import {Facet} from "./Facet";
-import {Filter} from "./Filter";
-import {StringPropertyValueFilter} from "./StringPropertyValueFilter";
 import {PropertyDefinition} from "./PropertyDefinition";
 
 const DCTERMS_NS = "http://purl.org/dc/terms/";
 
-const facetedObjectPropertyDefinitions: readonly PropertyDefinition[] = [
+const filterablePropertyDefinitions: readonly PropertyDefinition[] = [
   {
     label: "Medium",
     uri: DCTERMS_NS + "medium",
@@ -23,32 +19,12 @@ export const defaultConfiguration: Configuration = {
     "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css",
   documentTitle: null,
   navbarTitle: null,
-  objectSearch: (() => {
-    const facets: Facet[] = [];
-    const filters: Filter[] = [];
-    for (const propertyDefinition of facetedObjectPropertyDefinitions) {
-      const facet: StringPropertyValueFacet = {
-        propertyUri: propertyDefinition.uri,
-        type: "StringPropertyValue",
-        unknownCount: 0,
-        values: [],
-      };
-      facets.push(facet);
-
-      const filter: StringPropertyValueFilter = {
-        label: propertyDefinition.label,
-        propertyUri: propertyDefinition.uri,
-        type: "StringPropertyValue",
-      };
-      filters.push(filter);
-    }
-    return {
-      facets,
-      filters,
-      fullTextSearchablePropertyUris: [
-        DCTERMS_NS + "description",
-        DCTERMS_NS + "title",
-      ],
-    };
-  })(),
+  objectSearch: {
+    filters: filterablePropertyDefinitions.map(propertyDefinition => ({
+      label: propertyDefinition.label,
+      propertyUri: propertyDefinition.uri,
+      type: "StringPropertyValue",
+    })),
+    searchablePropertyUris: [DCTERMS_NS + "description", DCTERMS_NS + "title"],
+  },
 };
