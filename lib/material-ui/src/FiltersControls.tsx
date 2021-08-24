82f4be6ffbe09250e2a2b2eda4724a5f4e8de839
Minor G <minorg@server.fake>
2021-08-23 20:27:35 -0400

    fix material-ui regressions

--- lib/material-ui/src/FiltersControls.tsx
+++ lib/material-ui/src/FiltersControls.tsx
@@ -6,16 +6,20 @@ import {
   Grid,
 } from "@material-ui/core";
 import {
-  Facet,
+  CollectionValueFilter,
   Filter,
-  StringPropertyValueFacet,
+  InstitutionValueFilter,
+  JoinedCollectionValueFacet,
+  JoinedFacet,
+  JoinedInstitutionValueFacet,
+  JoinedStringPropertyValueFacet,
   StringPropertyValueFilter,
 } from "@paradicms/models";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import {ValueFilterTable} from "@paradicms/react-search";
+import {createFilterControl, ValueFilterTable} from "@paradicms/react-search";
 
 export const FiltersControls: React.FunctionComponent<{
-  facets: readonly Facet[];
+  facets: readonly JoinedFacet[];
   filters: readonly Filter[];
   onChange: (filters: readonly Filter[]) => void;
 }> = ({facets, filters, onChange}) => {
@@ -28,41 +32,52 @@ export const FiltersControls: React.FunctionComponent<{
           onChange(filtersCopy);
         };
 
-        switch (filter.type) {
-          case "StringPropertyValue": {
-            const concreteFilter: StringPropertyValueFilter = filter as StringPropertyValueFilter;
-            const facet: StringPropertyValueFacet | undefined = facets.find(
-              facet =>
-                facet.type === "StringPropertyValue" &&
-                (facet as StringPropertyValueFacet).propertyUri ===
-                  concreteFilter.propertyUri
-            ) as StringPropertyValueFacet | undefined;
-            if (!facet) {
-              return;
-            }
-            return (
-              <Grid
-                className="facet"
-                // data-cy={propertyFacet.definition.uri + "-facet"}
-                item
-                key={filterI}
-              >
-                <Accordion TransitionProps={{unmountOnExit: true}}>
-                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    {filter.label}
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    <ValueFilterTable
-                      facet={facet}
-                      filter={concreteFilter}
-                      onChange={onChangeFilter}
-                    />
-                  </AccordionDetails>
-                </Accordion>
-              </Grid>
-            );
-          }
-        }
+        return createFilterControl({
+          facets,
+          factory: {
+            createCollectionValueFilterControl(
+              facet: JoinedCollectionValueFacet,
+              filter: CollectionValueFilter
+            ): React.ReactNode {
+              throw new EvalError("not implemented");
+            },
+
+            createInstitutionValueFilterControl(
+              facet: JoinedInstitutionValueFacet,
+              filter: InstitutionValueFilter
+            ): React.ReactNode {
+              throw new EvalError("not implemented");
+            },
+
+            createStringPropertyValueFilterControl(
+              facet: JoinedStringPropertyValueFacet,
+              filter: StringPropertyValueFilter
+            ): React.ReactNode {
+              return (
+                <Grid
+                  className="facet"
+                  // data-cy={propertyFacet.definition.uri + "-facet"}
+                  item
+                  key={filterI}
+                >
+                  <Accordion TransitionProps={{unmountOnExit: true}}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                      {filter.label}
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <ValueFilterTable
+                        facet={facet}
+                        filter={filter}
+                        onChange={onChangeFilter}
+                      />
+                    </AccordionDetails>
+                  </Accordion>
+                </Grid>
+              );
+            },
+          },
+          filter,
+        });
       })}
     </Grid>
   );
--- lib/models/src/JoinedFacet.ts
+++ lib/models/src/JoinedFacet.ts
@@ -1,7 +1,8 @@
 import {JoinedDataset} from "./JoinedDataset";
 import {FacetType} from "./FacetType";
+import {Facet} from "./Facet";
 
-export abstract class JoinedFacet {
+export abstract class JoinedFacet implements Facet {
   protected constructor(protected readonly joinedDataset: JoinedDataset) {}
 
   abstract readonly type: FacetType;
