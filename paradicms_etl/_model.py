05b5c9c58c313089df2c80cdec80c4689117b0b1
Minor G <minorg@server.fake>
2021-10-02 13:20:49 -0400

    _Model: rework accessors

--- paradicms_etl/_model.py
+++ paradicms_etl/_model.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple, Union
+from typing import Generator, Optional, Tuple, Union
 
 from rdflib import BNode, ConjunctiveGraph, Graph, Literal, RDF, URIRef
 from rdflib.resource import Resource
@@ -39,76 +39,63 @@ class _Model:
     def from_rdf(cls, resource: Resource):
         return cls(cls._copy_resource(resource))
 
+    def __literal_values(
+        self, p: Union[URIRef, Tuple[URIRef, ...]], expected_type=None
+    ) -> Generator[object, None, None]:
+        for value in self.__values(p):
+            if not isinstance(value, Literal):
+                continue
+            python_value = value.toPython()
+            if expected_type is not None and not isinstance(
+                python_value, expected_type
+            ):
+                raise TypeError(f"expected {p} to have a {expected_type} value")
+            yield python_value
+
     def _optional_bool_value(
         self, p: Union[URIRef, Tuple[URIRef, ...]]
-    ) -> Optional[str]:
-        return self._optional_literal_value(p, bool)
-
-    def _optional_literal_value(
-        self, p: Union[URIRef, Tuple[URIRef, ...]], expected_type=None
-    ) -> Optional[object]:
-        if isinstance(p, URIRef):
-            return self.__optional_literal_value(p, expected_type=expected_type)
-        else:
-            for p in p:
-                value = self.__optional_literal_value(p, expected_type=expected_type)
-                if value is not None:
-                    return value
-            return None
-
-    def __optional_literal_value(
-        self, p: URIRef, expected_type=None
-    ) -> Optional[object]:
-        value = self._resource.value(p)
-        if value is None:
-            return None
-        if not isinstance(value, Literal):
-            raise TypeError(f"expected {p} to have a literal value")
-        python_value = value.toPython()
-        if expected_type is not None and not isinstance(python_value, expected_type):
-            raise TypeError(f"expected {p} to have a {expected_type} value")
-        return python_value
+    ) -> Optional[bool]:
+        return self.__optional_value(self.__literal_values(p, bool))
 
     def _optional_str_value(
         self, p: Union[URIRef, Tuple[URIRef, ...]]
     ) -> Optional[str]:
-        return self._optional_literal_value(p, str)
+        return self.__optional_value(self.__literal_values(p, str))
 
     def _optional_uri_value(
         self, p: Union[URIRef, Tuple[URIRef, ...]]
     ) -> Optional[URIRef]:
-        if isinstance(p, URIRef):
-            return self.__optional_uri_value(p)
-        else:
-            for p in p:
-                value = self.__optional_uri_value(p)
-                if value is not None:
-                    return value
-            return None
+        return self.__optional_value(self.__uri_values(p))
 
-    def __optional_uri_value(self, p: URIRef) -> Optional[URIRef]:
-        value = self._resource.value(p)
-        if value is None:
-            return None
-        if not isinstance(value, Resource) or not isinstance(value.identifier, URIRef):
-            raise TypeError(f"expected {p} to have a URI value")
-        return value.identifier
+    @staticmethod
+    def __optional_value(values: Generator[object, None, None]) -> Optional[object]:
+        for value in values:
+            return value
+        return None
 
     def _required_bool_value(self, p: Union[URIRef, Tuple[URIRef, ...]]) -> bool:
-        return self.__required_value(p, self._optional_bool_value(p))
+        return self.__required_value(self.__literal_values(p, bool))
 
     def _required_str_value(self, p: Union[URIRef, Tuple[URIRef, ...]]) -> str:
-        return self.__required_value(p, self._optional_str_value(p))
+        return self.__required_value(self.__literal_values(p, str))
 
     def _required_uri_value(self, p: Union[URIRef, Tuple[URIRef, ...]]) -> URIRef:
-        return self.__required_value(p, self._optional_uri_value(p))
+        return self.__required_value(self.__uri_values(p))
 
-    def __required_value(
-        self, p: Union[URIRef, Tuple[URIRef, ...]], optional_value: Optional[object]
-    ) -> object:
-        if optional_value is None:
-            raise KeyError(f"missing {p}")
-        return optional_value
+    @staticmethod
+    def __required_value(values: Generator[object, None, None]) -> object:
+        for value in values:
+            return value
+        raise KeyError
+
+    @staticmethod
+    def __required_values(
+        values: Generator[object, None, None],
+    ) -> Tuple[object, ...]:
+        values_tuple = tuple(values)
+        if values_tuple:
+            return values_tuple
+        raise KeyError
 
     @property
     def _resource(self) -> Resource:
@@ -122,3 +109,19 @@ class _Model:
         else:
             graph += self._resource.graph
             return graph.resource(self._resource.identifier)
+
+    def __uri_values(
+        self, p: Union[URIRef, Tuple[URIRef, ...]]
+    ) -> Generator[URIRef, None, None]:
+        for value in self.__values(p):
+            if isinstance(value, Resource) and isinstance(value.identifier, URIRef):
+                yield value
+
+    def __values(
+        self, p: Union[URIRef, Tuple[URIRef, ...]]
+    ) -> Generator[object, None, None]:
+        if isinstance(p, URIRef):
+            yield from self.__resource.objects(p)
+        else:
+            for p in p:
+                yield from self.__resource.objects(p)
