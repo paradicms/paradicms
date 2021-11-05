b7b985b562cdcf7cdb7a1cd5a8b7292fedc209cb
Minor G <minorg@server.fake>
2021-11-05 08:10:05 -0400

    more tests

--- test/paradicms_etl_test/models/text_test.py
+++ test/paradicms_etl_test/models/text_test.py
@@ -1,3 +1,5 @@
+from rdflib import Graph
+
 from paradicms_etl.models.rights import Rights
 from paradicms_etl.models.text import Text
 
@@ -10,6 +12,15 @@ def test_rights():
     Text.from_fields(
         value="Test", rights=Rights(creator="Test creator")
     ).rights.creator == "Test creator"
+
+
+def test_to_rdf():
+    expected = Text.from_fields(value="Test", rights=Rights(creator="Test creator"))
+    graph = Graph()
+    expected.to_rdf(graph=graph)
+    actual = Text.from_rdf(graph.resource(tuple(graph.subjects())[0]))
+    assert actual.value == expected.value
+    assert actual.rights.creator == expected.rights.creator
 
 
 def test_value():
