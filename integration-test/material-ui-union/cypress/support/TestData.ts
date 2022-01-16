42ae4a78fb66d310b265e0ca3cf6c6d9a32d2fba
Minor G <minorg@server.fake>
2022-01-16 14:25:25 -0500

    delete integration-test/material-ui-union

--- integration-test/material-ui-union/cypress/support/TestData.ts
+++
@@ -1,9 +0,0 @@
-import {RdfData} from "@paradicms/rdf";
-
-export class TestData extends RdfData {
-  static get fixture(): Cypress.Chainable<TestData> {
-    return cy.fixture("data.ttl").then(ttl => {
-      return new TestData(RdfData.parseTurtle(ttl));
-    });
-  }
-}
