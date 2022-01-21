2e9e8073291bf1b5df2911602204bdede7559539
Minor G <minorg@server.fake>
2022-01-20 20:05:18 -0500

    DateTimeDescription: accept different literal datatypes

--- lib/models/src/DateTimeDescription.ts
+++ lib/models/src/DateTimeDescription.ts
@@ -1,26 +1,30 @@
-import {Model} from "./Model";
-import {TIME, XSD} from "./vocabularies";
-import {Literal, NamedNode} from "n3";
+import { Model } from "./Model";
+import { TIME, XSD } from "./vocabularies";
+import { Literal, NamedNode } from "n3";
 import * as dayjs from "dayjs";
 
 export class DateTimeDescription extends Model {
   get day(): number | null {
-    const literal = this.propertyObjects(TIME.day).find(
-      term =>
-        term.termType === "Literal" && term.datatype.value === XSD.gDay.value
-    ) as Literal | undefined;
-    if (!literal) {
-      return null;
+    for (const term of this.propertyObjects(TIME.day)) {
+      if (term.termType !== "Literal") {
+        continue;
+      }
+      const literal: Literal = term;
+      if (literal.datatype.value === XSD.gDay.value) {
+        // https://www.w3.org/TR/xmlschema11-2/#gDay
+        const match = literal.value.match(
+          /---(0[1-9]|[12][0-9]|3[01])(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))?/
+        );
+        if (!match) {
+          throw new RangeError("expected time:day to match xsd:gDay regex");
+        }
+        // "---09" -> ["---09", "09", undefined, undefined, undefined, undefined, 0, "---09", undefined]
+        return parseInt(match[1]);
+      } else if (literal.datatype.value === XSD.integer.value) {
+        return parseInt(literal.value);
+      }
     }
-    // https://www.w3.org/TR/xmlschema11-2/#gDay
-    const match = literal.value.match(
-      /---(0[1-9]|[12][0-9]|3[01])(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))?/
-    );
-    if (!match) {
-      throw new RangeError("expected time:day to match xsd:gDay regex");
-    }
-    // "---09" -> ["---09", "09", undefined, undefined, undefined, undefined, 0, "---09", undefined]
-    return parseInt(match[1]);
+    return null;
   }
 
   get hour(): number | null {
@@ -32,52 +36,54 @@ export class DateTimeDescription extends Model {
   }
 
   get month(): number | null {
-    const literal = this.propertyObjects(TIME.month).find(
-      term =>
-        term.termType === "Literal" && term.datatype.value === XSD.gMonth.value
-    ) as Literal | undefined;
-    if (!literal) {
-      return null;
+    for (const term of this.propertyObjects(TIME.month)) {
+      if (term.termType !== "Literal") {
+        continue;
+      }
+      const literal: Literal = term;
+      if (literal.datatype.value === XSD.gMonth.value) {
+        // https://www.w3.org/TR/xmlschema11-2/#gMonth
+        const match = literal.value.match(
+          /--(0[1-9]|1[0-2])(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))?/
+        );
+        if (!match) {
+          throw new RangeError("expected time:month to match xsd:gMonth regex");
+        }
+        // "--01" -> ["--01", "01", undefined, undefined, undefined, undefined, 0, "--01", undefined]
+        return parseInt(match[1]);
+      } else if (literal.datatype.value === XSD.integer.value) {
+        return parseInt(literal.value);
+      }
     }
-    // https://www.w3.org/TR/xmlschema11-2/#gMonth
-    const match = literal.value.match(
-      /--(0[1-9]|1[0-2])(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))?/
-    );
-    if (!match) {
-      throw new RangeError("expected time:month to match xsd:gMonth regex");
-    }
-    // "--01" -> ["--01", "01", undefined, undefined, undefined, undefined, 0, "--01", undefined]
-    return parseInt(match[1]);
+    return null;
   }
 
   private optionalInt(property: NamedNode): number | null {
-    const literal = this.propertyObjects(property).find(
-      term =>
-        term.termType === "Literal" && term.datatype.value === XSD.integer.value
-    ) as Literal | undefined;
-    if (!literal) {
-      return null;
+    for (const term of this.propertyObjects(property)) {
+      if (term.termType !== "Literal") {
+        continue;
+      }
+      const literal: Literal = term;
+      if (literal.datatype.value === XSD.integer.value) {
+        return parseInt(literal.value);
+      }
     }
-    return parseInt(literal.value);
+    return null;
   }
 
   get second(): number | null {
-    const literal = this.propertyObjects(TIME.second).find(
-      term =>
-        term.termType === "Literal" &&
-        (term.datatype.value === XSD.decimal.value ||
-          term.datatype.value === XSD.integer.value)
-    ) as Literal | undefined;
-    if (!literal) {
-      return null;
+    for (const term of this.propertyObjects(TIME.second)) {
+      if (term.termType !== "Literal") {
+        continue;
+      }
+      const literal: Literal = term;
+      if (literal.datatype.value === XSD.decimal.value) {
+        return parseFloat(literal.value);
+      } else if (literal.datatype.value === XSD.integer.value) {
+        return parseInt(literal.value);
+      }
     }
-    if (literal.datatype.value === XSD.decimal.value) {
-      return parseFloat(literal.value);
-    } else if (literal.datatype.value === XSD.integer.value) {
-      return parseInt(literal.value);
-    } else {
-      throw new EvalError();
-    }
+    return null;
   }
 
   toString() {
@@ -132,21 +138,25 @@ export class DateTimeDescription extends Model {
   }
 
   get year(): number | null {
-    const literal = this.propertyObjects(TIME.year).find(
-      term =>
-        term.termType === "Literal" && term.datatype.value === XSD.gYear.value
-    ) as Literal | undefined;
-    if (!literal) {
-      return null;
+    for (const term of this.propertyObjects(TIME.year)) {
+      if (term.termType !== "Literal") {
+        continue;
+      }
+      const literal: Literal = term;
+      if (literal.datatype.value === XSD.gYear.value) {
+        // https://www.w3.org/TR/xmlschema11-2/#gYear
+        const match = literal.value.match(
+          /-?([1-9][0-9]{3,}|0[0-9]{3})(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))?/
+        );
+        if (!match) {
+          throw new RangeError("expected time:year to match xsd:gYear regex");
+        }
+        // "2022" -> ["2022", "2022", undefined, undefined, undefined, undefined, 0, "2022", undefined]
+        return parseInt(match[1]);
+      } else if (literal.datatype.value === XSD.integer.value) {
+        return parseInt(literal.value);
+      }
     }
-    // https://www.w3.org/TR/xmlschema11-2/#gYear
-    const match = literal.value.match(
-      /-?([1-9][0-9]{3,}|0[0-9]{3})(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))?/
-    );
-    if (!match) {
-      throw new RangeError("expected time:year to match xsd:gYear regex");
-    }
-    // "2022" -> ["2022", "2022", undefined, undefined, undefined, undefined, 0, "2022", undefined]
-    return parseInt(match[1]);
+    return null;
   }
 }
