761de4871b448e8ade8dfd598fb83765f61ed7f7
Minor G <minorg@server.fake>
2022-04-27 19:04:57 -0400

    fix LogoutPage to do logout properly

--- app/worksheet/src/ts/pages/LogoutPage.tsx
+++ app/worksheet/src/ts/pages/LogoutPage.tsx
@@ -1,7 +1,15 @@
 import React = require("react");
-import {Navigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {Hrefs} from "~/Hrefs";
+import {useCurrentUserService} from "~/hooks/useCurrentUserService";
+import {useEffect} from "react";
 
-export const LogoutPage: React.FunctionComponent = () => (
-  <Navigate to={Hrefs.logoutCallback} />
-);
+export const LogoutPage: React.FunctionComponent = () => {
+  const currentUserService = useCurrentUserService();
+  const navigate = useNavigate();
+  useEffect(() => {
+    currentUserService.deleteCurrentUser();
+    navigate(Hrefs.logoutCallback);
+  }, [currentUserService, navigate]);
+  return null;
+};
