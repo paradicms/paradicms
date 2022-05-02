f5f5f04aa83064520aba5586fbbc983376c62c25
Minor G <minorg@server.fake>
2022-05-01 20:33:58 -0400

    WorksheetFeatureSet: accept an abstract

--- paradicms_etl/models/worksheet_feature_set.py
+++ paradicms_etl/models/worksheet_feature_set.py
@@ -1,7 +1,10 @@
+from typing import Union
+
 from rdflib import URIRef, RDF
 from rdflib.namespace import DCTERMS
 
 from paradicms_etl.models.resource_backed_named_model import ResourceBackedNamedModel
+from paradicms_etl.models.text import Text
 from paradicms_etl.namespaces import WORKSHEET
 from paradicms_etl.utils.resource_builder import ResourceBuilder
 
@@ -12,10 +15,13 @@ class WorksheetFeatureSet(ResourceBackedNamedModel):
         self.title
 
     @classmethod
-    def from_fields(cls, *, title: str, uri: URIRef) -> "WorksheetFeatureSet":
+    def from_fields(
+        cls, *, title: str, uri: URIRef, abstract: Union[str, Text, None] = None
+    ) -> "WorksheetFeatureSet":
         return cls(
             ResourceBuilder(uri)
             .add(RDF.type, WORKSHEET.FeatureSet)
+            .add(DCTERMS.abstract, abstract)
             .add(DCTERMS.title, title)
             .build()
         )
