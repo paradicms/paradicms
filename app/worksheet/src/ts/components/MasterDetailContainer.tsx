4ddcafdb2fe2cc7b68f1aa93517edc26367bfe7b
Minor G <minorg@server.fake>
2022-05-02 21:24:48 -0400

    start on master-detail view

---
+++ app/worksheet/src/ts/components/MasterDetailContainer.tsx
@@ -0,0 +1,126 @@
+import * as React from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Container,
+  Row,
+} from "reactstrap";
+import {Image, Text} from "@paradicms/models";
+import {useState} from "react";
+import {thumbnailTargetDimensions} from "@paradicms/bootstrap";
+import classnames from "classnames";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faInfoCircle} from "@fortawesome/free-solid-svg-icons";
+
+interface Item {
+  description: string | Text | null;
+  onToggleSelected: () => void | null;
+  selected: boolean | null;
+  thumbnail: Image | null;
+  title: string;
+}
+
+export const MasterDetailContainer: React.FunctionComponent<{
+  items: readonly Item[];
+}> = ({items}) => {
+  const [detailItem, setDetailItem] = useState<Item | null>(null);
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col xs={detailItem ? 8 : 12}>
+          <ItemsGallery items={items} setDetailItem={setDetailItem} />
+        </Col>
+        {detailItem ? (
+          <Col xs={4}>
+            <ItemDetail item={detailItem} />
+          </Col>
+        ) : null}
+      </Row>
+    </Container>
+  );
+};
+
+const ItemDetail: React.FunctionComponent<{item: Item}> = ({item}) => {
+  return <div>{item.title}</div>;
+};
+
+// {thumbnail && thumbnail.rights ? (
+//   <figcaption
+//     className="figure-caption"
+//     style={{fontSize: "xx-small"}}
+//   >
+//     <RightsParagraph
+//       material="Image"
+//       rights={thumbnail.rights}
+//     />
+//   </figcaption>
+// ) : null}
+
+const ItemsGallery: React.FunctionComponent<{
+  items: readonly Item[];
+  setDetailItem: (item: Item) => void;
+}> = ({items, setDetailItem}) => (
+  <Container fluid>
+    <Row>
+      {items.map((item, itemI) => {
+        const {onToggleSelected, selected, thumbnail, title} = item;
+
+        let thumbnailSrc: string;
+        if (thumbnail) {
+          thumbnailSrc = thumbnail.src ?? thumbnail.uri;
+        } else {
+          thumbnailSrc = Image.placeholderSrc(thumbnailTargetDimensions);
+        }
+
+        return (
+          <Card
+            className={classnames({
+              "border-info": selected,
+              "mb-4": true,
+              "me-4": true,
+            })}
+            key={itemI}
+            style={{borderWidth: "8px", width: "240px"}}
+          >
+            <CardHeader className="px-0 mt-2 text-center w-100">
+              <Button
+                color="primary"
+                onClick={onToggleSelected}
+                style={{cursor: "pointer", textDecoration: "none"}}
+              >
+                {title}
+              </Button>
+            </CardHeader>
+            <CardBody
+              className="pb-0 px-0 text-center w-100"
+              style={{cursor: "pointer"}}
+            >
+              <a onClick={onToggleSelected}>
+                <img
+                  className="figure-img rounded"
+                  src={thumbnailSrc}
+                  style={{
+                    height: thumbnailTargetDimensions.height,
+                    width: thumbnailTargetDimensions.width,
+                  }}
+                />
+              </a>
+              <div className="mb-2">
+                <a onClick={() => setDetailItem(item)}>
+                  <FontAwesomeIcon
+                    icon={faInfoCircle}
+                    style={{height: "16px", width: "16px"}}
+                  />
+                </a>
+              </div>
+            </CardBody>
+          </Card>
+        );
+      })}
+    </Row>
+  </Container>
+);
--- app/worksheet/src/ts/pages/WorksheetFeatureEditPage.tsx
+++ app/worksheet/src/ts/pages/WorksheetFeatureEditPage.tsx
@@ -2,45 +2,17 @@ import * as React from "react";
 import {useWorksheet} from "~/hooks/useWorksheet";
 import {Spinner} from "~/components/Spinner";
 import {WorksheetNavigationFrame} from "~/components/WorksheetNavigationFrame";
-import {
-  Accordion,
-  AccordionBody,
-  AccordionHeader,
-  Button,
-  Card,
-  CardBody,
-  CardHeader,
-  Container,
-  Row,
-} from "reactstrap";
-import classnames from "classnames";
-import {RightsParagraph, thumbnailTargetDimensions} from "@paradicms/bootstrap";
-import {Image} from "@paradicms/models";
-import {WorksheetDescriptionFragment} from "~/components/WorksheetDescriptionFragment";
-import {useCallback, useState} from "react";
+import {thumbnailTargetDimensions} from "@paradicms/bootstrap";
+import {NamedValue} from "@paradicms/models";
+import {useState} from "react";
+import {MasterDetailContainer} from "~/components/MasterDetailContainer";
 
 export const WorksheetFeatureEditPage: React.FunctionComponent = () => {
   const [worksheet, dispatchWorksheet] = useWorksheet();
-  const [
-    openFeatureValueDescriptionAccordionIds,
-    setOpenFeatureValueDescriptionAccordionIds,
-  ] = useState<string[]>([]);
 
-  const toggleFeatureValueDescriptionAccordion = useCallback(
-    (featureValueDescriptionAccordionId: string) => {
-      const accordionIdsCopy = openFeatureValueDescriptionAccordionIds.concat();
-      const accordionIdIndex = openFeatureValueDescriptionAccordionIds.indexOf(
-        featureValueDescriptionAccordionId
-      );
-      if (accordionIdIndex !== -1) {
-        accordionIdsCopy.splice(accordionIdIndex, 1);
-      } else {
-        accordionIdsCopy.push(featureValueDescriptionAccordionId);
-      }
-      setOpenFeatureValueDescriptionAccordionIds(accordionIdsCopy);
-    },
-    [openFeatureValueDescriptionAccordionIds]
-  );
+  // @ts-ignore
+  const [detailFeatureValue, setDetailFeatureValue] =
+    useState<NamedValue | null>(null);
 
   if (!worksheet) {
     return <Spinner />;
@@ -58,102 +30,32 @@ export const WorksheetFeatureEditPage: React.FunctionComponent = () => {
       previousButtonEnabled={true}
       worksheet={worksheet}
     >
-      <p>
-        Select one or more boxes, then click <b>Next</b> to move to the next
-        feature. Click on the arrow at the bottom right of an image to see the
-        definition and other information.
-      </p>
-      <Container fluid>
-        <Row>
-          {feature.values
-            .concat()
-            .sort((left, right) =>
-              left.definition.title!.localeCompare(right.definition.title!)
-            )
-            .map((featureValue, featureValueI) => {
-              const thumbnail = featureValue.definition.thumbnail({
-                targetDimensions: thumbnailTargetDimensions,
-              });
-              let thumbnailSrc: string;
-              if (thumbnail) {
-                thumbnailSrc = thumbnail.src ?? thumbnail.uri;
-              } else {
-                thumbnailSrc = Image.placeholderSrc(thumbnailTargetDimensions);
-              }
-
-              const onToggleSelected = () => {
-                featureValue.selected = !featureValue.selected;
-                dispatchWorksheet({payload: worksheet});
-              };
-
-              return (
-                <Card
-                  className={classnames({
-                    "border-info": featureValue.selected,
-                    "mb-4": true,
-                    "me-4": true,
-                  })}
-                  key={featureValueI}
-                  style={{borderWidth: "8px", width: "240px"}}
-                >
-                  <CardHeader className="px-0 mt-2 text-center w-100">
-                    <Button
-                      color="primary"
-                      onClick={onToggleSelected}
-                      style={{cursor: "pointer", textDecoration: "none"}}
-                    >
-                      {featureValue.definition.title}
-                    </Button>
-                  </CardHeader>
-                  <CardBody
-                    className="pb-0 px-0 w-100"
-                    style={{cursor: "pointer"}}
-                  >
-                    <figure className="figure">
-                      <a onClick={onToggleSelected}>
-                        <img
-                          className="figure-img rounded"
-                          src={thumbnailSrc}
-                          style={{
-                            height: thumbnailTargetDimensions.height,
-                            width: thumbnailTargetDimensions.width,
-                          }}
-                        />
-                      </a>
-                      {thumbnail && thumbnail.rights ? (
-                        <figcaption
-                          className="figure-caption"
-                          style={{fontSize: "xx-small"}}
-                        >
-                          <RightsParagraph
-                            material="Image"
-                            rights={thumbnail.rights}
-                          />
-                        </figcaption>
-                      ) : null}
-                    </figure>
-                    {featureValue.definition.abstract ? (
-                      <Accordion
-                        open={openFeatureValueDescriptionAccordionIds}
-                        /* @ts-ignore */
-                        toggle={toggleFeatureValueDescriptionAccordion}
-                      >
-                        <AccordionHeader targetId={featureValueI.toString()}>
-                          Description
-                        </AccordionHeader>
-                        <AccordionBody accordionId={featureValueI.toString()}>
-                          <WorksheetDescriptionFragment
-                            description={featureValue.definition.abstract}
-                          />
-                        </AccordionBody>
-                      </Accordion>
-                    ) : null}
-                  </CardBody>
-                </Card>
-              );
-            })}
-        </Row>
-      </Container>
+      <div className="text-center w-100">
+        <p>
+          Select one or more boxes, then click <b>Next</b> to move to the next
+          feature. Click on the arrow at the bottom right of an image to see the
+          definition and other information.
+        </p>
+      </div>
+      <MasterDetailContainer
+        items={feature.values
+          .concat()
+          .sort((left, right) =>
+            left.definition.title!.localeCompare(right.definition.title!)
+          )
+          .map((featureValue) => ({
+            description: featureValue.definition.abstract,
+            onToggleSelected: () => {
+              featureValue.selected = !featureValue.selected;
+              dispatchWorksheet({payload: worksheet});
+            },
+            selected: featureValue.selected,
+            thumbnail: featureValue.definition.thumbnail({
+              targetDimensions: thumbnailTargetDimensions,
+            }),
+            title: featureValue.definition.title!,
+          }))}
+      />
     </WorksheetNavigationFrame>
   );
 };
