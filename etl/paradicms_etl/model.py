199a7a17689571657b36c38a2633a82aff669a67
Minor Gordon <minorg@server.fake>
2022-09-21 18:07:10 -0400

    all models must have an rdf:type

--- etl/paradicms_etl/model.py
+++ etl/paradicms_etl/model.py
@@ -1,11 +1,14 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 
-from rdflib import Graph
+from rdflib import Graph, RDF
 from rdflib.resource import Resource
 
 
 class Model(ABC):
+    def __init__(self):
+        self._required_uri_value(RDF.type)
+
     @property
     def label(self) -> Optional[str]:
         pass
