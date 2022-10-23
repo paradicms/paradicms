a858d5ca2a548dea2865e3bedcd90f00ee91afa5
Minor Gordon <minorg@server.fake>
2022-10-23 12:50:06 -0400

    change default Bootstrap stylesheet href to 5.1.3

--- lib/ts/bootstrap/src/defaultBootstrapStylesheetHref.ts
+++ lib/ts/bootstrap/src/defaultBootstrapStylesheetHref.ts
@@ -1,2 +1,2 @@
 export const defaultBootstrapStylesheetHref =
-  "https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css";
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css";
