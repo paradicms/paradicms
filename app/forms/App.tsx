cde2202f079628e614ba00fbb8edfb52ce49774a
Minor Gordon <minorg@server.fake>
2022-10-29 19:36:26 -0400

    move hook out

--- app/forms/App.tsx
+++ app/forms/App.tsx
@@ -1,9 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {StatusBar} from "expo-status-bar";
+import {SafeAreaProvider} from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import Navigation from "./navigation";
+import {useColorScheme} from "@paradicms/react-native";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
