db4850b530ab596af925a185fb9f31f7b481df7c
Minor Gordon <minorg@server.fake>
2022-10-30 18:28:56 -0400

    vocabularies: add missing prefixes

--- lib/ts/vocabularies/src/prefixes.ts
+++ lib/ts/vocabularies/src/prefixes.ts
@@ -1,3 +1,5 @@
+import {CMS} from "./CMS";
+import {CONFIGURATION} from "./CONFIGURATION";
 import {FOAF} from "./FOAF";
 import {RDF} from "./RDF";
 import {DC} from "./DC";
@@ -9,18 +11,24 @@ import {VRA} from "./VRA";
 import {SKOS} from "./SKOS";
 import {TIME} from "./TIME";
 import {WGS} from "./WGS";
-import {CMS} from "./CMS";
+import {RDFS} from "./RDFS";
+import {CONTACT} from "./CONTACT";
+import {SH} from "./SH";
 
 export const prefixes: Prefixes = {
   cms: CMS.NS(""),
+  configuration: CONFIGURATION.NS(""),
+  contact: CONTACT.NS(""),
   dc: DC.NS(""),
   dcterms: DCTERMS.NS(""),
   exif: EXIF.NS(""),
   foaf: FOAF.NS(""),
   rdf: RDF.NS(""),
+  rdfs: RDFS.NS(""),
+  sh: SH.NS(""),
   skos: SKOS.NS(""),
   time: TIME.NS(""),
   vra: VRA.NS(""),
-  xsd: XSD.NS(""),
   wgs: WGS.NS(""),
+  xsd: XSD.NS(""),
 };
