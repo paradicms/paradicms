e13f4b100ca75d859d8d11c9f767da638d7e8bfe
Minor Gordon <minorg@server.fake>
2022-10-30 18:29:09 -0400

    vocabularies: add RDFS for rdfs:label

---
+++ lib/ts/vocabularies/src/RDFS.ts
@@ -0,0 +1,8 @@
+import {Namespace} from "./Namespace";
+
+export class RDFS {
+  static readonly NS = Namespace("http://www.w3.org/2000/01/rdf-schema#");
+
+  // Properties
+  static readonly label = RDFS.NS("label");
+}
