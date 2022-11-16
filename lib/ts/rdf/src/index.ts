4b1320c28d8dc5b80c8c418d38aa969d47291ba4
Minor Gordon <minorg@server.fake>
2022-11-16 18:06:42 -0500

    readRdfList function

--- lib/ts/rdf/src/index.ts
+++ lib/ts/rdf/src/index.ts
@@ -2,6 +2,7 @@ export * from "./createDataset";
 export * from "./datasetCoreToDataset";
 export * from "./datasetToString";
 export * from "./parseIntoDataset";
+export * from "./readRdfList";
 export * from "./Resource";
 export * from "./TermMap";
 export * from "./TermSet";
