699788e6b49c543305d531a9d5e5e9e342fc16b9
Minor Gordon <minorg@server.fake>
2022-11-19 16:32:51 -0500

    clear out old FormNodeViewer.tsx experiment

--- lib/ts/react-native-shacl-forms/test/viewers/FormNodeViewer.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as renderer from "react-test-renderer";
-import {FormNodeViewer} from "../../src/viewers/FormNodeViewer";
-import {expect} from "@jest/globals";
-
-test("renders correctly", () => {
-  const tree = renderer.create(<FormNodeViewer />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
