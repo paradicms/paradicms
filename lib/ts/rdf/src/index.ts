da4aa65cd352483977da3fa16976a87bc4978f7f
Minor Gordon <minorg@server.fake>
2022-11-27 16:57:32 -0500

    @paradicms/rdf: getRdfInstances

--- lib/ts/rdf/src/index.ts
+++ lib/ts/rdf/src/index.ts
@@ -2,6 +2,7 @@ export * from "./createDataset";
 export {DataFactory, Store} from "n3";
 export * from "./datasetCoreToDataset";
 export * from "./datasetToString";
+export * from "./getRdfInstances";
 export * from "./hasRdfSuperClass";
 export * from "./parseIntoDataset";
 export * from "./readRdfList";
