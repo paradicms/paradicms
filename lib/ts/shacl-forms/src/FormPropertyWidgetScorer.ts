5895d900a3e0455d4e1bd1c45c842899bfa5e031
Minor Gordon <minorg@server.fake>
2022-12-13 14:59:02 -0500

    turn FormPropertyWidgetScorer into a function signature

--- lib/ts/shacl-forms/src/FormPropertyWidgetScorer.ts
+++ lib/ts/shacl-forms/src/FormPropertyWidgetScorer.ts
@@ -2,19 +2,18 @@ import {FormProperty} from "./FormProperty";
 import {NamedNode} from "@rdfjs/types";
 import {FormPropertyValue} from "./FormPropertyValue";
 
-export interface FormPropertyWidgetScorer {
-  /**
-   * Score the suitability of this widget to the given FormProperty.
-   *
-   * The scores use the system described at https://datashapes.org/forms.html#widgets
-   *   0: the widget is not applicable at all
-   *   between 1 and 100, the higher the better
-   *   null if the widget may or may not be suitable, yet can be selected manually
-   * @param formProperty
-   */
-  score(
-    formProperty: FormProperty,
-    formPropertyValues: readonly FormPropertyValue[]
-  ): number | null;
-  readonly widgetName: NamedNode;
-}
+/**
+ * Function that scores the suitability of one or more widgets vis a vis the given FormProperty.
+ *
+ * The scores use the system described at https://datashapes.org/forms.html#widgets
+ *   0: the widget is not applicable at all
+ *   between 1 and 100, the higher the better
+ *   null if the widget may or may not be suitable, yet can be selected manually
+ */
+export type FormPropertyWidgetScorer = (kwds: {
+  formProperty: FormProperty;
+  formPropertyValues: readonly FormPropertyValue[];
+}) => {
+  score: number | null;
+  widget: NamedNode;
+};
