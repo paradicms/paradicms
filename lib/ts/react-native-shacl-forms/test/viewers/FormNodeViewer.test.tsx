8e372e8c3845ce5eb000ac3400197af6c177e0f2
Minor Gordon <minorg@server.fake>
2022-12-14 10:33:00 -0500

    get react-native-shacl-forms tests working again

--- lib/ts/react-native-shacl-forms/test/viewers/FormNodeViewer.test.tsx
+++ lib/ts/react-native-shacl-forms/test/viewers/FormNodeViewer.test.tsx
@@ -3,15 +3,22 @@ import * as renderer from "react-test-renderer";
 import {testForm} from "../testForm";
 import {FormNodeViewer} from "../../src/viewers/FormNodeViewer";
 import {createFormPropertyViewer} from "../../src/viewers/createFormPropertyViewer";
+import {Text, ThemeProvider} from "@rneui/themed";
+import {defaultTheme} from "@rneui/base";
+
+jest.useFakeTimers();
 
 test("renders correctly", () => {
   const formNode = testForm().nodeTypes[0].nodes[0];
   const tree = renderer
     .create(
-      <FormNodeViewer
-        formNode={formNode}
-        formPropertyViewerFactory={createFormPropertyViewer}
-      />
+      <ThemeProvider theme={defaultTheme}>
+        <FormNodeViewer
+          formNode={formNode}
+          formPropertyViewerFactory={createFormPropertyViewer}
+          iconFactory={({name}) => <Text>{name}</Text>}
+        />
+      </ThemeProvider>
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
