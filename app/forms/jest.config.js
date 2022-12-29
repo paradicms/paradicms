953e028afef58e1ab317119bc7c39eebfb65d504
Minor Gordon <minorg@server.fake>
2022-12-29 18:40:29 -0500

    do mocks in a jest setup file

--- app/forms/jest.config.js
+++ app/forms/jest.config.js
@@ -3,13 +3,14 @@ const {defaults: tsjPreset} = require("ts-jest/presets");
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "jest-expo",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFiles: ["./tests/setUp.js"],
   transform: {
     "^.+\\.jsx?$": "babel-jest",
     "^.+\\.tsx?$": "ts-jest",
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   // https://github.com/react-native-elements/react-native-elements/discussions/3290
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@rneui/*)",
   ],
 };
