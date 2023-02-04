cd7c192cd28ee601e02cd2b3d1a8719f24168e91
Minor Gordon <minorg@server.fake>
2023-02-04 10:46:17 -0500

    fix relative paths

--- lib/py/etl/test/paradicms_etl_test/conftest.py
+++ lib/py/etl/test/paradicms_etl_test/conftest.py
@@ -2,14 +2,14 @@ from pathlib import Path
 from typing import Tuple
 
 import pytest
-from paradicms_etl.model import Model
 
+from paradicms_etl.model import Model
 from paradicms_etl.pipelines.test_data_pipeline import TestDataPipeline
 
 
 @pytest.fixture
 def data_dir_path() -> Path:
-    path_ = Path(__file__).absolute().parent.parent.parent.parent / "data"
+    path_ = Path(__file__).absolute().parent.parent.parent.parent.parent.parent / "data"
     assert path_.is_dir()
     return path_
 
