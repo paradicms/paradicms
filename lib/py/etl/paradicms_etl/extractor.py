13b5375f9f2199414bfb80f8b4a8ff41fc1f0eb7
Minor Gordon <minorg@server.fake>
2023-02-12 16:28:12 -0500

    convert pipeline phases to function signatures

--- lib/py/etl/paradicms_etl/extractor.py
+++ lib/py/etl/paradicms_etl/extractor.py
@@ -1,81 +1,9 @@
-from abc import abstractmethod
 from pathlib import Path
-from typing import Dict, Optional
-from urllib.request import urlopen
-
-from pathvalidate import sanitize_filename
-
-from paradicms_etl.pipeline_phase import PipelinePhase
+from typing import Dict, Optional, Protocol, Any
 
 
-class Extractor(PipelinePhase):
-    def __init__(
-        self,
-        *,
-        data_dir_path: Optional[Path] = None,
-        extracted_data_dir_path: Optional[Path] = None,
-        **kwds
-    ):
-        """
-        Construct an extractor.
-
-        If extracted_data_dir_path is specified, use it for the extractor's outputs.
-        If data_dir_path is specified, use data_dir_path / pipeline_id / "extracted".
-        If neither is specified, use the default data_dir_path in the repository / pipeline_id / "extracted".
-        """
-
-        PipelinePhase.__init__(self, **kwds)
-        self.__data_dir_path = data_dir_path
-        self.__extracted_data_dir_path = extracted_data_dir_path
-
-    def _download(self, from_url: str, force: bool) -> Path:
-        """
-        Utility method to download a file from a URL to a local file path.
-        """
-        file_path = self._extracted_data_dir_path / sanitize_filename(from_url)
-        if not force and file_path.exists():
-            self._logger.info(
-                "%s already downloaded to %s and force not specified, skipping download",
-                from_url,
-                file_path,
-            )
-            return file_path
-
-        self._logger.info("downloading %s to %s", from_url, file_path)
-        try:
-            url_ = urlopen(from_url)
-            url_contents = url_.read()
-        finally:
-            url_.close()
-        with open(file_path, "w+b") as file_:
-            file_.write(url_contents)
-        self._logger.info("downloaded %s", from_url)
-        return file_path
-
-    @abstractmethod
-    def extract(self, *, force: bool) -> Optional[Dict[str, object]]:
-        """
-        Extract data from a source.
-        :param force: force extraction, ignoring any cached data
-        :return a **kwds dictionary to merge with kwds to pass to transformer
-        """
-
-    @property
-    def _extracted_data_dir_path(self) -> Path:
-        """
-        Directory to use to store extracted data.
-        The directory is created on demand when this method is called.
-        Paths into this directory can be passed to the transformer via the kwds return from extract.
-        """
-
-        if self.__extracted_data_dir_path is not None:
-            extracted_data_dir_path = self.__extracted_data_dir_path
-        elif self.__data_dir_path is not None:
-            extracted_data_dir_path = (
-                self.__data_dir_path / self._pipeline_id / "extracted"
-            )
-        else:
-            raise ValueError("must specify extracted_data_dir_path or data_dir_path")
-        extracted_data_dir_path = extracted_data_dir_path.absolute()
-        extracted_data_dir_path.mkdir(parents=True, exist_ok=True)
-        return extracted_data_dir_path
+class Extractor(Protocol):
+    def __call__(
+        self, *, extracted_data_dir_path: Path, force: bool, pipeline_id: str
+    ) -> Optional[Dict[str, Any]]:
+        pass
--- lib/py/etl/paradicms_etl/loader.py
+++ lib/py/etl/paradicms_etl/loader.py
@@ -1,57 +1,11 @@
-from abc import abstractmethod
 from pathlib import Path
-from typing import Optional, Iterable
+from typing import Iterable, Protocol, Any
 
 from paradicms_etl.model import Model
-from paradicms_etl.pipeline_phase import PipelinePhase
 
 
-class Loader(PipelinePhase):
-    def __init__(
-        self,
-        *,
-        data_dir_path: Optional[Path] = None,
-        loaded_data_dir_path: Optional[Path] = None,
-        **kwds
-    ):
-        """
-        Construct a loader.
-
-        If loaded_data_dir_path is specified, use it for the loader's outputs.
-        If data_dir_path is specified, use data_dir_path / pipeline_id / "loaded".
-        If neither is specified, use the default data_dir_path in the repository / pipeline_id / "loaded".
-        """
-
-        PipelinePhase.__init__(self, **kwds)
-        self.__data_dir_path = data_dir_path
-        self.__loaded_data_dir_path = loaded_data_dir_path
-
-    def flush(self):
-        """
-        Flush any buffered data.
-        """
-
-    @abstractmethod
-    def load(self, *, models: Iterable[Model]):
-        """
-        Load models from the given generator.
-        :param models: generator of models to load, normally the result of the transformer
-        """
-
-    @property
-    def _loaded_data_dir_path(self) -> Path:
-        """
-        Directory to use to store loaded data.
-        The directory is created on demand when this method is called.
-        A loader does not have to use this directory. It can load data into an external database, for example.
-        """
-
-        if self.__loaded_data_dir_path is not None:
-            loaded_data_dir_path = self.__loaded_data_dir_path
-        elif self.__data_dir_path is not None:
-            loaded_data_dir_path = self.__data_dir_path / self._pipeline_id / "loaded"
-        else:
-            raise ValueError("must specify loaded_data_dir_path or data_dir_path")
-        loaded_data_dir_path = loaded_data_dir_path.absolute()
-        loaded_data_dir_path.mkdir(parents=True, exist_ok=True)
-        return loaded_data_dir_path
+class Loader(Protocol):
+    def __call__(
+        self, *, flush: bool, loaded_data_dir_path: Path, models: Iterable[Model]
+    ) -> Any:
+        pass
