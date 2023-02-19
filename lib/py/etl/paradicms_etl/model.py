b85f8723dc5db055d8c07a1132aac07d26af9100
Minor Gordon <minorg@server.fake>
2023-02-18 19:39:32 -0500

    Model: exclude some namespaces

--- lib/py/etl/paradicms_etl/model.py
+++ lib/py/etl/paradicms_etl/model.py
@@ -8,6 +8,7 @@ from rdflib.resource import Resource
 import paradicms_etl
 from paradicms_etl.utils.module_namespaces import module_namespaces
 
+_EXCLUDE_NAMESPACE_PREFIXES_FROM_JSON_LD_CONTEXT = {"brick", "csvw", "odrl2", "prof", "qb", "sosa", "ssn", "vann", "void", "xmlns"}
 
 class Model(ABC):
     @classmethod
@@ -20,6 +21,8 @@ class Model(ABC):
         for namespace_prefix, namespace in module_namespaces(
             rdflib.namespace, paradicms_etl.namespaces
         ).items():
+            if namespace_prefix in _EXCLUDE_NAMESPACE_PREFIXES_FROM_JSON_LD_CONTEXT:
+                continue
             context[namespace_prefix] = str(namespace)
         return context
 
