fbefe184b9b333f68e489faed1e6063e862d2dc3
Minor Gordon <minorg@server.fake>
2023-02-20 12:28:38 -0500

    no longer need ExistingFileExtractor, only dressdiscover_etl was using it

--- lib/py/etl/test/paradicms_etl_test/extractors/existing_file_extractor_test.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from paradicms_etl.extractors.existing_file_extractor import ExistingFileExtractor
-
-
-def test_extract_file_path():
-    assert ExistingFileExtractor(
-        file_path=Path(__file__),
-    )() == {"file_path": Path(__file__)}
