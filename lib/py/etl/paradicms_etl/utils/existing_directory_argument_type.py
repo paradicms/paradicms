79a9222023383b155f6f9df4b7686f57c289555f
Minor Gordon <minorg@server.fake>
2023-02-20 15:36:15 -0500

    absolute data directory path

--- lib/py/etl/paradicms_etl/utils/existing_directory_argument_type.py
+++ lib/py/etl/paradicms_etl/utils/existing_directory_argument_type.py
@@ -5,4 +5,4 @@ def existing_directory_argument_type(value: str) -> Path:
     dir_path = Path(value)
     if not dir_path.is_dir():
         raise ValueError(f"{value} is not a directory")
-    return dir_path
+    return dir_path.absolute()
