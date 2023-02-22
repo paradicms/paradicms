a2c57107eac05dee09aea2a568297217130b7dae
Minor Gordon <minorg@server.fake>
2023-02-22 14:21:24 -0500

    do namespace binding in the same way the JSON-LD namespace prefixes are bound

--- lib/py/etl/paradicms_etl/namespaces/bind_namespaces.py
+++ lib/py/etl/paradicms_etl/namespaces/bind_namespaces.py
@@ -1,37 +1,33 @@
 from typing import TypeVar, Union
 
-from rdflib import DC, DCTERMS, FOAF, OWL, SKOS, Graph, SDO
+import rdflib
+from rdflib import Graph
 from rdflib.namespace import NamespaceManager
+
+import paradicms_etl
+from paradicms_etl.utils.module_namespaces import module_namespaces
 
 _NamespaceManagerT = TypeVar("_NamespaceManagerT", bound=Union[Graph, NamespaceManager])
 
 
-def bind_namespaces(namespace_manager: _NamespaceManagerT) -> _NamespaceManagerT:
-    from paradicms_etl.namespaces import (
-        CMS,
-        CONTACT,
-        EXIF,
-        PROV,
-        TIME,
-        VRA,
-        WGS,
-        WORKSHEET,
-    )
+EXCLUDE_RDFLIB_NAMESPACE_PREFIXES = {
+    "brick",
+    "csvw",
+    "odrl2",
+    "prof",
+    "qb",
+    "sosa",
+    "ssn",
+    "vann",
+    "void",
+    "xmlns",
+}
 
-    namespace_manager.bind("cms", CMS)
-    namespace_manager.bind("contact", CONTACT)
-    namespace_manager.bind("cc", "http://creativecommons.org/ns#")
-    namespace_manager.bind("dc", DC)
-    # namespace_manager.bind("dcmitype", DCMITYPE)
-    namespace_manager.bind("dcterms", DCTERMS)
-    namespace_manager.bind("exif", EXIF)
-    namespace_manager.bind("foaf", FOAF)
-    namespace_manager.bind("owl", OWL)
-    namespace_manager.bind("prov", PROV)
-    namespace_manager.bind("sdo", SDO)
-    namespace_manager.bind("skos", SKOS)
-    namespace_manager.bind("time", TIME)
-    namespace_manager.bind("vra", VRA)
-    namespace_manager.bind("wgs", WGS)
-    namespace_manager.bind("worksheet", WORKSHEET)
-    return namespace_manager
+
+def bind_namespaces(namespace_manager: _NamespaceManagerT) -> _NamespaceManagerT:
+    for namespace_prefix, namespace in module_namespaces(
+        rdflib.namespace, paradicms_etl.namespaces
+    ).items():
+        if namespace_prefix in EXCLUDE_RDFLIB_NAMESPACE_PREFIXES:
+            continue
+        namespace_manager.bind(namespace_prefix, namespace)
