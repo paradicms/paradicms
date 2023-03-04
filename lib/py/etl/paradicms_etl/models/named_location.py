70cc95047ca365bc7f5eb2989fc6776145ae44d7
Minor Gordon <minorg@server.fake>
2023-03-04 11:01:13 -0500

    etl: fix add of rdf:type cms:Location

--- lib/py/etl/paradicms_etl/models/named_location.py
+++ lib/py/etl/paradicms_etl/models/named_location.py
@@ -1,12 +1,17 @@
-from rdflib import URIRef
+from rdflib import URIRef, RDF
 
 from paradicms_etl.models.location import Location
 from paradicms_etl.models.resource_backed_named_model import ResourceBackedNamedModel
+from paradicms_etl.namespaces import CMS
 from paradicms_etl.utils.resource_builder import ResourceBuilder
 from paradicms_etl.utils.safe_dict_update import safe_dict_update
 
 
 class NamedLocation(ResourceBackedNamedModel, Location):
+    def __init__(self, *args, **kwds):
+        ResourceBackedNamedModel.__init__(self, *args, **kwds)
+        self._resource.add(RDF.type, CMS.Location)
+
     @classmethod
     def from_fields(cls, *, uri: URIRef, **kwds) -> "NamedLocation":
         return Location._from_fields(cls, resource_builder=ResourceBuilder(uri), **kwds)
