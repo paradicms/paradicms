4deabdb595a3324eab9222e0c431e586e3952cdc
Minor Gordon <minorg@server.fake>
2023-03-10 14:30:53 -0500

    FileCache.put_file method

--- lib/py/ssg/paradicms_ssg/file_cache.py
+++ lib/py/ssg/paradicms_ssg/file_cache.py
@@ -29,6 +29,36 @@ class FileCache:
         self.__logger = logging.getLogger(self.__class__.__name__)
         self.__sleep_s_after_download = sleep_s_after_download
 
+    def __cached_file_ext(self, *, file_url: str, file_mime_type: Optional[str]) -> str:
+        if file_mime_type is not None:
+            cached_file_ext = mimetypes.guess_extension(file_mime_type, strict=False)
+            if cached_file_ext is not None:
+                self.__logger.debug(
+                    "guessed file extension %s from Content-Type %s",
+                    cached_file_ext,
+                    file_mime_type,
+                )
+                return cached_file_ext
+
+        guessed_mime_type, _ = mimetypes.guess_type(file_url, strict=False)
+        if guessed_mime_type is not None:
+            self.__logger.debug(
+                "guessed MIME type %s from file URL %s", guessed_mime_type, file_url
+            )
+            cached_file_ext = mimetypes.guess_extension(guessed_mime_type, strict=False)
+            if cached_file_ext is not None:
+                self.__logger.debug(
+                    "guessed file extension %s from guessed MIME type %s",
+                    cached_file_ext,
+                    guessed_mime_type,
+                )
+                return cached_file_ext
+
+        raise ValueError(f"unable to guess file extension for {file_url}")
+
+    def __file_cache_dir_path(self, *, file_url: str) -> Path:
+        return self.__cache_dir_path / sanitize_filename(str(file_url))
+
     def get_file(self, file_url: URIRef) -> Path:
         """
         Get file from the cache, downloading if necessary.
@@ -41,7 +71,7 @@ class FileCache:
 
         assert not str(file_url).startswith("file:")
 
-        file_cache_dir_path = self.__cache_dir_path / sanitize_filename(str(file_url))
+        file_cache_dir_path = self.__file_cache_dir_path(file_url=file_url)
 
         if not self.__force_download and file_cache_dir_path.is_dir():
             for file_name in os.listdir(file_cache_dir_path):
@@ -72,41 +102,8 @@ class FileCache:
             )
             sleep(self.__sleep_s_after_download)
 
-        content_type = headers_dict.get("Content-Type")
-        cached_file_ext = None
-        if content_type:
-            cached_file_ext = mimetypes.guess_extension(content_type, strict=False)
-            self.__logger.debug(
-                "guessed file extension %s from Content-Type %s",
-                cached_file_ext,
-                content_type,
-            )
-        else:
-            self.__logger.debug("%s returned an empty Content-Type", file_url)
-
-        if cached_file_ext is None:
-            guessed_mime_type, _ = mimetypes.guess_type(file_url, strict=False)
-            if guessed_mime_type is not None:
-                self.__logger.debug(
-                    "guessed MIME type %s from file URL %s", guessed_mime_type, file_url
-                )
-                cached_file_ext = mimetypes.guess_extension(
-                    guessed_mime_type, strict=False
-                )
-                self.__logger.debug(
-                    "guessed file extension %s from guessed MIME type %s",
-                    cached_file_ext,
-                    guessed_mime_type,
-                )
-
-        if cached_file_ext is None:
-            raise ValueError(f"unable to guess file extension for {file_url}")
-
-        self.__logger.debug(
-            "%s content type = %s, file extension = %s",
-            file_url,
-            content_type,
-            cached_file_ext,
+        cached_file_ext = self.__cached_file_ext(
+            file_mime_type=headers_dict.get("Content-Type"), file_url=file_url
         )
 
         cached_file_path = file_cache_dir_path / ("file" + cached_file_ext)
@@ -122,4 +119,17 @@ class FileCache:
             self.__logger.debug(
                 "wrote %s headers to %s", file_url, headers_json_file_path
             )
+        return cached_file_path
+
+    def put_file(
+        self, *, file_data: bytes, file_mime_type: str, file_url: URIRef
+    ) -> Path:
+        file_cache_dir_path = self.__file_cache_dir_path(file_url=file_url)
+        cached_file_ext = self.__cached_file_ext(
+            file_mime_type=file_mime_type, file_url=file_url
+        )
+        cached_file_path = file_cache_dir_path / ("file" + cached_file_ext)
+        file_cache_dir_path.mkdir(exist_ok=True)
+        with open(cached_file_path, "w+b") as cached_file:
+            cached_file.write(file_data)
         return cached_file_path
--- lib/py/ssg/test/file_cache_test.py
+++ lib/py/ssg/test/file_cache_test.py
@@ -17,12 +17,25 @@ def test_get_file_404(tmp_path):
 
 def test_get_file_200(tmp_path):
     sut = FileCache(cache_dir_path=tmp_path)
-    URL = URIRef("https://place-hold.it/100x100")
+    url = URIRef("https://place-hold.it/100x100")
 
     for i in range(2):  # Initial miss, then hit
-        cached_file_path = sut.get_file(URL)
+        cached_file_path = sut.get_file(url)
         assert cached_file_path.is_file(), cached_file_path
         assert (
             cached_file_path.parent != tmp_path
         ), cached_file_path  # Every file is put in its own directory
         assert (cached_file_path.parent / "headers.json").is_file()
+
+
+def test_put_file(tmp_path):
+    sut = FileCache(cache_dir_path=tmp_path)
+    with open(__file__, "rb") as file_:
+        file_data = file_.read()
+    cached_file_path = sut.put_file(
+        file_data=file_data,
+        file_mime_type="text/plain",
+        file_url=URIRef("https://place-hold.it/100x100"),
+    )
+    assert str(cached_file_path).endswith(".txt")
+    assert cached_file_path.is_file()
