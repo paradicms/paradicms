11d52078b893807ea41c3d0ff4be77e85bbafcf4
Minor Gordon <minorg@server.fake>
2023-03-12 19:17:37 -0400

    RootModel typedef to ResourceBackedModel

--- lib/py/etl/paradicms_etl/models/root_model.py
+++ lib/py/etl/paradicms_etl/models/root_model.py
@@ -1,6 +1,6 @@
-from paradicms_etl.models.resource_backed_named_model import ResourceBackedNamedModel
+from paradicms_etl.models.resource_backed_model import ResourceBackedModel
 
 """
 Typedef for root model classes such as Collection, Image, and Work.
 """
-RootModel = ResourceBackedNamedModel
+RootModel = ResourceBackedModel
