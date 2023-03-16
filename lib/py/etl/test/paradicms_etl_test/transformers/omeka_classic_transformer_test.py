a6c59b3824b3e79cee57471378c19628f0d74b33
Minor Gordon <minorg@server.fake>
2023-03-15 21:06:29 -0400

    OmekaClassicTransformerTest: transformer doesn't yield License or RightsStatement any more

--- lib/py/etl/test/paradicms_etl_test/transformers/omeka_classic_transformer_test.py
+++ lib/py/etl/test/paradicms_etl_test/transformers/omeka_classic_transformer_test.py
@@ -3,8 +3,6 @@ from pathlib import Path
 from paradicms_etl.extractors.omeka_classic_extractor import OmekaClassicExtractor
 from paradicms_etl.models.collection import Collection
 from paradicms_etl.models.image import Image
-from paradicms_etl.models.license import License
-from paradicms_etl.models.rights_statement import RightsStatement
 from paradicms_etl.models.work import Work
 from paradicms_etl.transformers.omeka_classic_transformer import OmekaClassicTransformer
 
@@ -27,9 +25,7 @@ def test_transform(data_dir_path: Path):
     models = tuple(transformer(**extract_result))
     assert models
     model_types = set(model.__class__ for model in models)
-    assert len(model_types) == 5
+    assert len(model_types) == 3
     assert Collection in model_types
     assert Image in model_types
-    assert License in model_types
-    assert RightsStatement in model_types
     assert Work in model_types
