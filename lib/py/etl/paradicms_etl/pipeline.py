ee08bd86947592160db8e27b6ba84bf39c392947
Minor Gordon <minorg@server.fake>
2023-03-15 20:54:39 -0400

    AmbientReferenceTransformer

--- lib/py/etl/paradicms_etl/pipeline.py
+++ lib/py/etl/paradicms_etl/pipeline.py
@@ -9,6 +9,9 @@ from paradicms_etl.extractor import Extractor
 from paradicms_etl.loader import Loader
 from paradicms_etl.model import Model
 from paradicms_etl.transformer import Transformer
+from paradicms_etl.transformers.ambient_reference_transformer import (
+    ambient_reference_transformer,
+)
 from paradicms_etl.transformers.validation_transformer import validation_transformer
 from paradicms_etl.utils.existing_directory_argument_type import (
     existing_directory_argument_type,
@@ -144,8 +147,10 @@ class Pipeline(ABC):
     def __transform(self, extract_kwds: Optional[Dict[str, Any]]) -> Iterable[Model]:
         if extract_kwds is None:
             extract_kwds = {}
-        transform_result = self.transformer(**extract_kwds)
-        return validation_transformer(transform_result)
+        models = self.transformer(**extract_kwds)
+        models_with_ambient_referees = ambient_reference_transformer(models)
+        validated_models = validation_transformer(models_with_ambient_referees)
+        return validated_models
 
     @property
     def transformer(self):
