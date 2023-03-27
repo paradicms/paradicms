8b5618ddc4b1435a1cf879772c73f8f1f95ce727
Minor Gordon <minorg@server.fake>
2023-03-27 16:54:45 -0400

    merge readConfigurationFile into readModelSetFile

--- lib/ts/next/src/readModelSetFile.ts
+++ lib/ts/next/src/readModelSetFile.ts
@@ -1,9 +1,14 @@
 import {ModelSet} from "@paradicms/models";
-import {anyRdfStringToDataset, createDataset} from "@paradicms/rdf";
+import {
+  anyRdfStringToDataset,
+  createDataset,
+  DataFactory,
+  Store,
+} from "@paradicms/rdf";
 
 let _modelSet: ModelSet | undefined;
 
-export const readModelSetFile = (
+export const readModelSetFile = async (
   // There are issues importing "fs" from a library, so pass in the function we need here
   // https://github.com/vercel/next.js/issues/7755
   readFile: (filePath: string) => Promise<string>
@@ -12,18 +17,50 @@ export const readModelSetFile = (
     return Promise.resolve(_modelSet);
   }
 
+  const filePaths: string[] = [];
+
+  const configurationFilePath: string | undefined =
+    process.env.CONFIGURATION_FILE_PATH;
+  if (configurationFilePath) {
+    filePaths.push(configurationFilePath);
+  }
+
   const dataFilePath: string | undefined = process.env.DATA_FILE_PATH;
-  if (!dataFilePath) {
+  if (dataFilePath) {
+    filePaths.push(dataFilePath);
+  }
+
+  if (filePaths.length === 0) {
     _modelSet = ModelSet.fromDataset(createDataset());
     return Promise.resolve(_modelSet!);
   }
 
-  return readFile(dataFilePath).then(dataString =>
-    anyRdfStringToDataset(dataString, {
-      path: dataFilePath,
-    }).then(dataset => {
-      _modelSet = ModelSet.fromDataset(dataset);
-      return _modelSet!;
-    })
-  );
+  const combinedStore = new Store();
+  for (const filePath of filePaths) {
+    const fileContents = await readFile(filePath);
+    const fileDataset = await anyRdfStringToDataset(fileContents, {
+      path: filePath,
+    });
+
+    // Rename the default graph from each file
+    const renamedDefaultGraph = DataFactory.blankNode();
+    const defaultGraph = DataFactory.defaultGraph();
+    for (const quad of fileDataset) {
+      if (quad.graph.equals(defaultGraph)) {
+        combinedStore.add(
+          DataFactory.quad(
+            quad.subject,
+            quad.predicate,
+            quad.object,
+            renamedDefaultGraph
+          )
+        );
+      } else {
+        combinedStore.add(quad);
+      }
+    }
+  }
+
+  _modelSet = ModelSet.fromDatasetCore(combinedStore);
+  return _modelSet!;
 };
