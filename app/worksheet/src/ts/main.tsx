94e1f93820478c14e8c216209d33b77d502127b8
Minor Gordon <minorg@server.fake>
2023-03-29 19:18:33 -0400

    app/worksheet: main.tsx: use ReactDOMClient.createRoot, new style

--- app/worksheet/src/ts/main.tsx
+++ app/worksheet/src/ts/main.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
+import * as ReactDOMClient from "react-dom/client";
 
 // const whyDidYouRender = require("@welldone-software/why-did-you-render");
 // whyDidYouRender(React, {
 //   trackAllPureComponents: true,
 //   trackHooks: true,
 // });
-
-import * as ReactDOM from "react-dom";
 // import Hammer from "hammerjs";
 import {Application} from "~/Application";
 
 // delete Hammer.defaults.cssProps.userSelect;
 
-ReactDOM.render(<Application />, document.getElementById("root"));
+const root = ReactDOMClient.createRoot(document.getElementById("root")!);
+root.render(<Application />);
