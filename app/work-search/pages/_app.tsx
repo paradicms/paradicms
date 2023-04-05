3faa9c1024001e2302050d39a641850519876aae
Minor Gordon <minorg@server.fake>
2023-04-05 10:29:06 -0400

    enableBatching in use-query-params

--- app/work-search/pages/_app.tsx
+++ app/work-search/pages/_app.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import React from "react";
 import "react-vertical-timeline-component/style.min.css";
 import "leaflet/dist/leaflet.css";
-import {NextAdapter} from 'next-query-params';
+import {NextAdapter} from "next-query-params";
 import {QueryParamProvider} from "use-query-params";
 
 const App: React.FunctionComponent<AppProps> = ({Component, pageProps}) => {
@@ -15,7 +15,10 @@ const App: React.FunctionComponent<AppProps> = ({Component, pageProps}) => {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      <QueryParamProvider adapter={NextAdapter}>
+      <QueryParamProvider
+        adapter={NextAdapter}
+        options={{enableBatching: true}}
+      >
         <Component {...pageProps} />
       </QueryParamProvider>
     </>
