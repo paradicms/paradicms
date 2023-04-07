3e2de91490db98d4df112e1a97971e26253ae4b1
Minor Gordon <minorg@server.fake>
2023-04-07 16:42:35 -0400

    updating spreadsheet transformer test

--- lib/py/etl/test/paradicms_etl_test/transformers/spreadsheet_transformer_test.py
+++ lib/py/etl/test/paradicms_etl_test/transformers/spreadsheet_transformer_test.py
@@ -1,12 +1,14 @@
 import os
-from datetime import date
+from datetime import datetime
 from pathlib import Path
+from typing import Tuple
 
 import pytest
 from rdflib import Graph, DCTERMS
 
 from paradicms_etl.extractors.excel_2010_extractor import Excel2010Extractor
 from paradicms_etl.extractors.google_sheets_extractor import GoogleSheetsExtractor
+from paradicms_etl.model import Model
 from paradicms_etl.models.image import Image
 from paradicms_etl.models.image_data import ImageData
 from paradicms_etl.models.person import Person
@@ -15,14 +17,7 @@ from paradicms_etl.models.work_creation import WorkCreation
 from paradicms_etl.transformers.spreadsheet_transformer import SpreadsheetTransformer
 
 
-def test_transform_excel_2010(excel_2010_test_data_file_path: Path):
-    models = tuple(
-        SpreadsheetTransformer(pipeline_id="test")(
-            **Excel2010Extractor(xlsx_file_path=excel_2010_test_data_file_path)(
-                force=False
-            )
-        )
-    )
+def __check_test_data_models(models: Tuple[Model, ...]):
     assert len(models) == 4
     person = next(person for person in models if isinstance(person, Person))
     image = next(image for image in models if isinstance(image, Image))
@@ -38,24 +33,33 @@ def test_transform_excel_2010(excel_2010_test_data_file_path: Path):
     work_creation_date = (
         work_creation.to_rdf(graph=Graph()).value(DCTERMS.date).toPython()
     )
-    assert isinstance(work_creation_date, date)
+    assert isinstance(work_creation_date, datetime)
+
+
+def test_transform_excel_2010(excel_2010_test_data_file_path: Path):
+    __check_test_data_models(
+        tuple(
+            SpreadsheetTransformer(pipeline_id="test")(
+                **Excel2010Extractor(xlsx_file_path=excel_2010_test_data_file_path)(
+                    force=False
+                )
+            )
+        )
+    )
 
 
 @pytest.mark.skipif("CI" in os.environ, reason="don't connect to Google Sheets in CI")
 def test_transform_google_sheets(google_sheets_spreadsheet_id: str, tmp_path: Path):
-    models = tuple(
-        SpreadsheetTransformer(pipeline_id="test")(
-            **GoogleSheetsExtractor(
-                extracted_data_dir_path=tmp_path,
-                spreadsheet_id=google_sheets_spreadsheet_id,
-            )(force=False)
+    __check_test_data_models(
+        tuple(
+            SpreadsheetTransformer(pipeline_id="test")(
+                **GoogleSheetsExtractor(
+                    extracted_data_dir_path=tmp_path,
+                    spreadsheet_id=google_sheets_spreadsheet_id,
+                )(force=False)
+            )
         )
     )
-    assert len(models) == 2
-    person = next(person for person in models if isinstance(person, Person))
-    image = next(image for image in models if isinstance(image, Image))
-    assert person.name == "Minor Gordon"
-    assert isinstance(image.src, ImageData)
 
 
 @pytest.mark.skipif("CI" in os.environ, reason="don't connect to Google Sheets in CI")
