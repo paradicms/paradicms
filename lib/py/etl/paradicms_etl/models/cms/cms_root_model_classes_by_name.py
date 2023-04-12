e1d889325a9337dd696e05978286a6dccbbd08af
Minor Gordon <minorg@server.fake>
2023-04-12 14:47:24 -0400

    cms_root_model_classes_by_name

--- lib/py/etl/paradicms_etl/models/cms/cms_root_model_classes_by_name.py
+++ lib/py/etl/paradicms_etl/models/cms/cms_root_model_classes_by_name.py
@@ -1,9 +1,21 @@
 from typing import Type, Dict, Tuple
 
-from paradicms_etl.models.named_location import NamedLocation
-from paradicms_etl.models.root_model import RootModel
+from paradicms_etl.models.cms.cms_collection import CmsCollection
+from paradicms_etl.models.cms.cms_concept import CmsConcept
+from paradicms_etl.models.cms.cms_image import CmsImage
+from paradicms_etl.models.cms.cms_license import CmsLicense
+from paradicms_etl.models.cms.cms_model import CmsModel
+from paradicms_etl.models.cms.cms_named_location import CmsNamedLocation
+from paradicms_etl.models.cms.cms_named_model import CmsNamedModel
+from paradicms_etl.models.cms.cms_organization import CmsOrganization
+from paradicms_etl.models.cms.cms_person import CmsPerson
+from paradicms_etl.models.cms.cms_rights_statement import CmsRightsStatement
+from paradicms_etl.models.cms.cms_work import CmsWork
+from paradicms_etl.models.cms.cms_work_closing import CmsWorkClosing
+from paradicms_etl.models.cms.cms_work_creation import CmsWorkCreation
+from paradicms_etl.models.cms.cms_work_opening import CmsWorkOpening
 
-__ROOT_MODEL_CLASSES: Tuple[Type[RootModel], ...] = (
+__CMS_ROOT_MODEL_CLASSES: Tuple[Type[CmsNamedModel], ...] = (
     CmsCollection,
     CmsConcept,
     CmsImage,
@@ -18,8 +30,10 @@ __ROOT_MODEL_CLASSES: Tuple[Type[RootModel], ...] = (
     CmsWorkOpening,
 )
 
-ROOT_MODEL_CLASSES_BY_NAME: Dict[str, Type[RootModel]] = {
-    __class.__name__: __class for __class in __ROOT_MODEL_CLASSES
-}
+CMS_ROOT_MODEL_CLASSES_BY_NAME: Dict[str, Type[CmsModel]] = {}
+for __class in __CMS_ROOT_MODEL_CLASSES:
+    CMS_ROOT_MODEL_CLASSES_BY_NAME[__class.__name__] = __class
+    CMS_ROOT_MODEL_CLASSES_BY_NAME[__class.__name__.lstrip("Cms")] = __class
+
 # Aliases
-ROOT_MODEL_CLASSES_BY_NAME["Location"] = NamedLocation
+CMS_ROOT_MODEL_CLASSES_BY_NAME["Location"] = CmsNamedLocation
--- lib/py/ssg/paradicms_ssg/models/root_model_classes_by_name.py
+++ lib/py/ssg/paradicms_ssg/models/root_model_classes_by_name.py
@@ -1,10 +1,12 @@
 from typing import Type, Dict
 
-import paradicms_etl.models.root_model_classes_by_name
-from paradicms_etl.models.root_model import RootModel
+import paradicms_etl
+from paradicms_etl.model import Model
 from paradicms_ssg.models.app_configuration import AppConfiguration
 
 ROOT_MODEL_CLASSES_BY_NAME: Dict[
-    str, Type[RootModel]
-] = paradicms_etl.models.root_model_classes_by_name.ROOT_MODEL_CLASSES_BY_NAME.copy()
+    str, Type[Model]
+] = (
+    paradicms_etl.models.cms.cms_root_model_classes_by_name.CMS_ROOT_MODEL_CLASSES_BY_NAME.copy()
+)
 ROOT_MODEL_CLASSES_BY_NAME["AppConfiguration"] = AppConfiguration
