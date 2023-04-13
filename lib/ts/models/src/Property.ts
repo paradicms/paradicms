cab3dcbfadadcd540f92c60c967e8c7595444728
Minor Gordon <minorg@server.fake>
2023-04-13 14:48:55 -0400

    work through Property

--- lib/ts/models/src/Property.ts
+++ lib/ts/models/src/Property.ts
@@ -1,64 +1,15 @@
 import {NamedModel} from "./NamedModel";
-import {Mixin} from "ts-mixer";
 import {NamedNode} from "@rdfjs/types";
-import {cms, rdfs, sh} from "@paradicms/vocabularies";
 import {PropertyValue} from "./PropertyValue";
-import {Memoize} from "typescript-memoize";
-import {getRdfInstanceQuads} from "@paradicms/rdf";
-import {createPropertyValuesFromQuadSubjects} from "./createPropertyValuesFromQuadSubjects";
-import {CmsCommentMixin} from "./CmsCommentMixin";
-import {CmsImagesMixin} from "./CmsImagesMixin";
-import {CmsLabelMixin} from "./CmsLabelMixin";
+import {ImagesMixin} from "./ImagesMixin";
 
-export class Property extends Mixin(
-  NamedModel,
-  CmsCommentMixin,
-  CmsImagesMixin,
-  CmsLabelMixin
-) {
-  get filterable(): boolean {
-    return (
-      this.findAndMapObject(cms.propertyFilterable, this.mapBooleanObject) ??
-      false
-    );
-  }
-
-  get groupUris(): readonly string[] {
-    return this.filterAndMapObjects(cms.propertyGroup, term =>
-      term.termType === "NamedNode" ? term.value : null
-    );
-  }
-
-  get order(): number {
-    return this.findAndMapObject(sh.order, this.mapIntObject) ?? 0;
-  }
-
-  @Memoize()
-  get range(): NamedNode | null {
-    return this.findAndMapObject(rdfs.range, term =>
-      term.termType === "NamedNode" ? term : null
-    );
-  }
-
-  @Memoize()
-  get rangeValues(): readonly PropertyValue[] {
-    const range = this.range;
-    if (range === null) {
-      return [];
-    }
-
-    return createPropertyValuesFromQuadSubjects(this.modelSet, [
-      ...getRdfInstanceQuads({
-        class_: range,
-        dataset: this.dataset,
-      }),
-    ]);
-  }
-
-  get searchable(): boolean {
-    return (
-      this.findAndMapObject(cms.propertySearchable, this.mapBooleanObject) ??
-      false
-    );
-  }
+export interface Property extends NamedModel, ImagesMixin {
+  readonly comment: string | null;
+  readonly filterable: boolean;
+  readonly label: string;
+  readonly groupUris: readonly string[];
+  readonly order: number;
+  readonly range: NamedNode | null;
+  readonly rangeValues: readonly PropertyValue[];
+  readonly searchable: boolean;
 }
