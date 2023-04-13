23be01a512d04e228a8b0e9609b4f756c79de788
Minor Gordon <minorg@server.fake>
2023-04-13 14:53:08 -0400

    work through RightsStatement

---
+++ lib/ts/models/src/CmsRightsStatement.ts
@@ -0,0 +1,31 @@
+import {dcterms, skos} from "@paradicms/vocabularies";
+import {requireNonNull} from "@paradicms/utilities";
+import {RightsStatement} from "./RightsStatement";
+import {ResourceBackedNamedModel} from "./ResourceBackedNamedModel";
+
+export class CmsRightsStatement extends ResourceBackedNamedModel
+  implements RightsStatement {
+  get definition(): string | null {
+    return this.findAndMapObject(skos.definition, this.mapStringObject);
+  }
+
+  get description(): string | null {
+    return this.findAndMapObject(dcterms.description, this.mapStringObject);
+  }
+
+  get identifier(): string {
+    return requireNonNull(
+      this.findAndMapObject(dcterms.identifier, this.mapStringObject)
+    );
+  }
+
+  get prefLabel(): string {
+    return requireNonNull(
+      this.findAndMapObject(skos.prefLabel, this.mapStringObject)
+    );
+  }
+
+  override toString() {
+    return this.prefLabel;
+  }
+}
