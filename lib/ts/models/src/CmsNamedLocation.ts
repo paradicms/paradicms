cd8d4e4620adbcc008b55354d53d6b624664660a
Minor Gordon <minorg@server.fake>
2023-04-14 20:04:52 -0400

    NamedLocation, CmsNamedLocation

--- lib/ts/models/src/CmsNamedLocation.ts
+++ lib/ts/models/src/CmsNamedLocation.ts
@@ -3,7 +3,7 @@ import {requireNonNull} from "@paradicms/utilities";
 import {ResourceBackedNamedModel} from "ResourceBackedNamedModel";
 import {NamedLocation} from "NamedLocation";
 
-export class CmsLocation extends ResourceBackedNamedModel
+export class CmsNamedLocation extends ResourceBackedNamedModel
   implements NamedLocation {
   get lat(): number {
     return requireNonNull(this.findAndMapObject(wgs.lat, this.mapFloatObject));
