52439421bb5358b3a569e81d169aaa8fd6e9fdd4
Minor Gordon <minorg@server.fake>
2023-04-16 10:18:33 -0400

    remove commented-out interface fields

--- lib/ts/models/src/RightsStatement.ts
+++ lib/ts/models/src/RightsStatement.ts
@@ -1,9 +1,5 @@
 import {NamedModel} from "./NamedModel";
 
 export interface RightsStatement extends NamedModel {
-  // readonly definition: string | null;
-  // readonly description: string | null;
-  // readonly identifier: string;
-  // readonly prefLabel: string;
   readonly label: string;
 }
