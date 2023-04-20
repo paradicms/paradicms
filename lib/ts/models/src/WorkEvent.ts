74823c7f5c6ad1772dc581f3422efce977528f4c
Minor Gordon <minorg@server.fake>
2023-04-20 11:43:59 -0400

    WorkEvent: no visitor

--- lib/ts/models/src/WorkEvent.ts
+++ lib/ts/models/src/WorkEvent.ts
@@ -1,10 +1,8 @@
 import {Event} from "./Event";
 import {Work} from "./Work";
-import {WorkEventVisitor} from "./WorkEventVisitor";
 import {WorkLocation} from "./WorkLocation";
 
 export interface WorkEvent extends Event {
-  accept<T>(visitor: WorkEventVisitor<T>): T;
   readonly work: Work;
   readonly workLocation: WorkLocation | null;
   readonly workUri: string;
