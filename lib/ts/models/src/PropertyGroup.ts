c8f9237cb1a06e649de236d3f1e28f9efc7ef97c
Minor Gordon <minorg@server.fake>
2023-04-21 18:10:34 -0400

    Property, PropertyGroup: make comment a Text | null instead of string | null

--- lib/ts/models/src/PropertyGroup.ts
+++ lib/ts/models/src/PropertyGroup.ts
@@ -1,9 +1,10 @@
 import {ImagesMixin} from "./ImagesMixin";
 import {NamedModel} from "./NamedModel";
 import {Property} from "./Property";
+import {Text} from "./Text";
 
 export interface PropertyGroup extends NamedModel, ImagesMixin {
-  readonly comment: string | null;
+  readonly comment: Text | null;
   readonly label: string;
   readonly properties: readonly Property[];
 }
