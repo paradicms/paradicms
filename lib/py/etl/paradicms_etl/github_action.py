7473862b9869ab69d643f3138a3f40b2339e8e2c
Minor Gordon <minorg@server.fake>
2023-04-23 20:11:48 -0400

    move GitHubAction ABC to etl project and make it amenable to the new ssg-action

--- lib/py/etl/paradicms_etl/github_action.py
+++ lib/py/etl/paradicms_etl/github_action.py
@@ -5,99 +5,77 @@ import sys
 from abc import ABC, abstractmethod
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import TypeVar, Generic, Type
+from typing import Dict, Any, Type, Optional
 
 import yaml
 from configargparse import ArgParser
 
-from paradicms_etl.github_action_inputs import GitHubActionInputs
 
-InputsT = TypeVar("InputsT", bound=GitHubActionInputs)
-
-
-class GitHubAction(ABC, Generic[InputsT]):
+class GitHubAction(ABC):
     """
     Abstract base class for GitHub Actions.
     """
 
-    def __init__(
-        self, *, data_dir_path: Path, dev: bool, force_extract: bool, inputs: InputsT
-    ):
-        self.__data_dir_path = data_dir_path
-        self.__dev = dev
-        self._force_extract = force_extract
-        self._inputs = inputs
-        self.__logger = logging.getLogger(__name__)
+    @dataclasses.dataclass(frozen=True)
+    class Inputs:
+        class _RequiredType(str):
+            pass
 
-    @property
-    def _extracted_data_dir_path(self) -> Path:
-        return self.__data_dir_path / "extracted"
+        REQUIRED = _RequiredType()
 
-    @classmethod
-    def __generate_action_yml(cls):
-        action_yaml = {
-            "author": "Minor Gordon",
-            "branding": {
-                "icon": "loader",
+        debug: str = dataclasses.field(
+            default="", metadata={"description": "Debug the action"}
+        )
+
+        pipeline_id: str = dataclasses.field(
+            default="",
+            metadata={
+                "description": "Pipeline id. Defaults to the name of the repository."
             },
-            "description": cls.__doc__.strip(),
-            "inputs": cls._inputs_class.fields_yaml,
-            "name": cls.__doc__.strip(),
-            "runs": {"image": "Dockerfile", "using": "docker"},
-        }
+        )
 
-        action_yml_file_path = (
-            Path(sys.modules[cls.__module__].__file__).parent / "action.yml"
-        ).absolute()
-        with open(action_yml_file_path, "w+") as action_yml_file:
-            yaml.dump(
-                {key: action_yaml[key] for key in sorted(action_yaml.keys())},
-                action_yml_file,
-            )
+        @classmethod
+        @property
+        def fields_yaml(cls) -> Dict[str, Dict[str, Any]]:
+            fields_yaml: Dict[str, Dict[str, Any]] = {}
+            for field in dataclasses.fields(cls):
+                if not issubclass(field.type, str):
+                    continue
+                field_yaml: Dict[str, Any] = {}
+                if field.default:
+                    field_yaml["default"] = str(field.default)
+                if field.metadata:
+                    for key in ("description",):
+                        value = field.metadata.get(key)
+                        if value:
+                            field_yaml[key] = value.strip()
+                field_yaml["required"] = id(field.default) == id(
+                    GitHubAction.Inputs.REQUIRED
+                )
+                fields_yaml[field.name] = {
+                    key: field_yaml[key] for key in sorted(field_yaml.keys())
+                }
+            return fields_yaml
 
-    @classmethod
-    def main(cls):
-        if len(sys.argv) > 1:
-            # ArgParse/ArgumentParser has no notion of a "default subparser"
-            # https://stackoverflow.com/questions/46963172/how-do-you-get-argparse-to-choose-a-default-subparser
-            # Hack it here instead of forcing actions to require a subparser command like "run".
-            command = sys.argv[1].lower()
-            if command == "generate-action-yml":
-                cls.__generate_action_yml()
-                return
+        def __post_init__(self):
+            for field in dataclasses.fields(self):
+                if id(field.default) != id(GitHubAction.Inputs.REQUIRED):
+                    continue
+                value = getattr(self, field.name)
+                if not value.strip():
+                    raise ValueError("empty/blank " + field.name)
 
-        args = cls.__parse_args()
-
-        if args.debug:
-            logging.basicConfig(level=logging.DEBUG)
-        else:
-            logging.basicConfig(level=logging.INFO)
-
-        inputs = cls.__parse_inputs(args)
-
-        # Instantiate this class and call its _run()
-        cls_kwds = {
-            "dev": bool(args.dev),
-            "force_extract": bool(args.force_extract),
-            "inputs": inputs,
-        }
-        if args.data_dir_path:
-            cls_kwds["data_dir_path"] = args.data_dir_path
-            cls(**cls_kwds)._run()
-        else:
-            with TemporaryDirectory() as temp_dir:
-                cls_kwds["data_dir_path"] = Path(temp_dir)
-                cls(**cls_kwds)._run()
+    def __init__(
+        self, *, data_dir_path: Path, debug: bool, force_extract: bool, pipeline_id: str
+    ):
+        self._data_dir_path = data_dir_path
+        self._debug = debug
+        self._force_extract = force_extract
+        self.__logger = logging.getLogger(__name__)
+        self._pipeline_id = pipeline_id
 
     @classmethod
-    @property
-    def _inputs_class(cls) -> Type[InputsT]:
-        raise NotImplementedError
-
-    @classmethod
-    def __parse_args(cls):
-        arg_parser = ArgParser()
-
+    def _add_arguments(cls, arg_parser: ArgParser, *, inputs_class: Type[Inputs]):
         arg_parser.add_argument(
             "-c", is_config_file=True, help="path to a file to read arguments from"
         )
@@ -107,47 +85,108 @@ class GitHubAction(ABC, Generic[InputsT]):
             type=Path,
         )
         arg_parser.add_argument(
-            "--dev",
-            action="store_true",
-            help="start the app in dev mode rather than building it",
-        )
-        arg_parser.add_argument(
             "--force-extract",
             action="store_true",
             help="force extraction, ignoring any cached files",
         )
 
-        for field in dataclasses.fields(cls._inputs_class):
+        for field in dataclasses.fields(inputs_class):
             arg_name = "--" + field.name.replace("_", "-")
             if not issubclass(field.type, str):
                 raise TypeError(field.type)
             arg_parser.add_argument(
                 arg_name,
+                default=field.default
+                if id(field.default) != id(GitHubAction.Inputs.REQUIRED)
+                else None,
                 env_var="INPUT_" + field.name.upper(),
-                required=id(field.default) == id(GitHubActionInputs.REQUIRED),
+                required=id(field.default) == id(GitHubAction.Inputs.REQUIRED),
             )
 
+    @property
+    def _extracted_data_dir_path(self) -> Path:
+        return self._data_dir_path / "extracted"
+
+    @classmethod
+    def __generate_action_yml(cls, inputs_class: Type[Inputs]):
+        assert cls.__doc__ is not None
+        action_yaml = {
+            "author": "Minor Gordon",
+            "branding": {
+                "icon": "loader",
+            },
+            "description": cls.__doc__.strip(),
+            "inputs": inputs_class.fields_yaml,
+            "name": cls.__doc__.strip(),
+            "runs": {"image": "Dockerfile", "using": "docker"},
+        }
+
+        action_yml_file_path = (
+            Path(sys.modules[cls.__module__].__file__).parent / "action.yml"  # type: ignore
+        ).absolute()
+        with open(action_yml_file_path, "w+") as action_yml_file:
+            yaml.dump(
+                {key: action_yaml[key] for key in sorted(action_yaml.keys())},
+                action_yml_file,
+            )
+
+    @classmethod
+    def main(cls, *, inputs_class: Optional[Type[Inputs]] = None):
+        if inputs_class is None:
+            inputs_class = cls.Inputs
+
+        if len(sys.argv) > 1:
+            # ArgParse/ArgumentParser has no notion of a "default subparser"
+            # https://stackoverflow.com/questions/46963172/how-do-you-get-argparse-to-choose-a-default-subparser
+            # Hack it here instead of forcing actions to require a subparser command like "run".
+            command = sys.argv[1].lower()
+            if command == "generate-action-yml":
+                cls.__generate_action_yml(inputs_class=inputs_class)
+                return
+
+        args = cls.__parse_args(inputs_class=inputs_class)
+        inputs = cls.__parse_inputs(args, inputs_class=inputs_class)
+
+        if inputs.get("debug"):
+            logging.basicConfig(level=logging.DEBUG)
+        else:
+            logging.basicConfig(level=logging.INFO)
+
+        # Instantiate this class with args and inputs
+        cls_kwds = {
+            key: value
+            for key, value in vars(args).items()
+            if key not in {"c"} and value is not None
+        }
+        cls_kwds.update(inputs)
+
+        # Call its run method
+        if args.data_dir_path:
+            cls(**cls_kwds)._run()
+        else:
+            with TemporaryDirectory() as temp_dir:
+                cls_kwds["data_dir_path"] = Path(temp_dir)
+                cls(**cls_kwds)._run()
+
+    @classmethod
+    def __parse_args(cls, *, inputs_class: Type[Inputs]):
+        arg_parser = ArgParser()
+        cls._add_arguments(arg_parser, inputs_class=inputs_class)
         return arg_parser.parse_args()
 
     @classmethod
-    def __parse_inputs(cls, args) -> InputsT:
-        inputs_kwds = {}
-        for key, value in vars(args).items():
+    def __parse_inputs(cls, args, *, inputs_class: Type[Inputs]) -> Dict[str, str]:
+        inputs: Dict[str, str] = {}
+        for field in dataclasses.fields(inputs_class):
+            value = getattr(args, field.name)
             if value is None:
                 continue
-            elif key in {"c", "data_dir_path", "dev", "force_extract"}:
-                continue
-            elif isinstance(value, str):
-                stripped_value = value.strip()
-                if stripped_value:
-                    inputs_kwds[key] = stripped_value
-            else:
-                raise TypeError(type(value))
-        if "pipeline_id" not in inputs_kwds:
-            inputs_kwds["pipeline_id"] = os.environ["GITHUB_REPOSITORY"].rsplit("/", 1)[
-                -1
-            ]
-        return cls._inputs_class(**inputs_kwds)  # type: ignore
+            stripped_value = value.strip()
+            if stripped_value:
+                inputs[field.name] = stripped_value
+        if "pipeline_id" not in inputs:
+            inputs["pipeline_id"] = os.environ["GITHUB_REPOSITORY"].rsplit("/", 1)[-1]
+        return inputs
 
     @abstractmethod
     def _run(self):
