11fb765743cc3532c8cf6ab2e9535c37a2dd6f1f
Minor Gordon <minorg@server.fake>
2023-05-07 17:59:06 -0400

    Revert "remove Image.replace from signature"
    This reverts commit 095eea69779766a04047bf800d00ab95a59a8a46.

--- lib/py/etl/paradicms_etl/models/image.py
+++ lib/py/etl/paradicms_etl/models/image.py
@@ -24,6 +24,12 @@ class Image(NamedModel, RightsMixin):
     def original_image_uri(self) -> Optional[URIRef]:
         raise NotImplementedError
 
+    @abstractmethod
+    def replace(
+        self, *, copyable: Optional[bool] = None, src: Optional[str] = None
+    ) -> "Image":
+        raise NotImplementedError
+
     @property
     @abstractmethod
     def src(self) -> Union[ImageData, str, None]:
