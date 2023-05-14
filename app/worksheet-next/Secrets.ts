4678f90dadca42da298ccc120e83f22a10706051
Minor Gordon <minorg@server.fake>
2023-05-14 14:52:26 -0400

    copy in Secrets and loadGapiClient

---
+++ app/worksheet-next/Secrets.ts
@@ -0,0 +1,4 @@
+export class Secrets {
+    static readonly GOOGLE_API_KEY = "AIzaSyB4PwYxXfjO8bFwueteV7myDA5vxSK3nzc";
+    static readonly GOOGLE_CLIENT_ID = "768033890505-b9u6s293d52k4f1v4bav9fif7feoe4b4.apps.googleusercontent.com";
+}
