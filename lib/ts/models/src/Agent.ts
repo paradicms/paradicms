432d97727f34270bbd89b5805325e04f7a781b2e
Minor Gordon <minorg@server.fake>
2023-05-17 15:32:37 -0400

    Agent.works accessor

--- lib/ts/models/src/Agent.ts
+++ lib/ts/models/src/Agent.ts
@@ -1,7 +1,9 @@
 import {ImagesMixin} from "./ImagesMixin";
 import {Model} from "./Model";
 import {RelationsMixin} from "./RelationsMixin";
+import {Work} from "./Work";
 
 export interface Agent extends Model, ImagesMixin, RelationsMixin {
   readonly label: string;
+  readonly works: readonly Work[];
 }
--- lib/ts/models/src/literal/LiteralAgent.ts
+++ lib/ts/models/src/literal/LiteralAgent.ts
@@ -2,6 +2,7 @@ import {Image} from "../Image";
 import {OtherAgent} from "../OtherAgent";
 import {ThumbnailSelector} from "../ThumbnailSelector";
 import {LiteralModel} from "./LiteralModel";
+import {Work} from "../Work";
 
 export class LiteralAgent extends LiteralModel implements OtherAgent {
   images: readonly Image[] = [];
@@ -13,4 +14,5 @@ export class LiteralAgent extends LiteralModel implements OtherAgent {
   type: "OtherAgent" = "OtherAgent";
   wikipediaUrl: string | null = null;
   wikidataConceptUri: string | null = null;
+  readonly works: readonly Work[] = [];
 }
