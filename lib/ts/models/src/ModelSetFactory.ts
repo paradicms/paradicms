76590633edf7e2da0c1c802cabf89850356a0f0f
Minor Gordon <minorg@server.fake>
2023-05-29 18:13:42 -0400

    start of SameAsModelReader which will join (some) models on sameAs

--- lib/ts/models/src/ModelSetFactory.ts
+++ lib/ts/models/src/ModelSetFactory.ts
@@ -1,11 +1,12 @@
-import { datasetCoreToDataset, fastRdfStringToDataset } from "@paradicms/rdf";
-import { Dataset, DatasetCore } from "@rdfjs/types";
-import { CachingModelSet } from "./CachingModelSet";
-import { CmsModelReader } from "./cms/CmsModelReader";
+import {datasetCoreToDataset, fastRdfStringToDataset} from "@paradicms/rdf";
+import {Dataset, DatasetCore} from "@rdfjs/types";
+import {CachingModelSet} from "./CachingModelSet";
+import {CmsModelReader} from "./cms/CmsModelReader";
+import {SameAsModelReader} from "./same-as/SameAsModelReader";
 
 class DatasetBackedModelSet extends CachingModelSet {
   constructor(private readonly dataset: Dataset) {
-    super([new CmsModelReader(dataset)]);
+    super(new SameAsModelReader([new CmsModelReader(dataset)]));
   }
 
   override toRdf(): Dataset {
---
+++ lib/ts/models/src/same-as/SameAsModelReader.ts
@@ -0,0 +1,6 @@
+import {ConcatenatingModelReader} from "../ConcatenatingModelReader";
+
+/**
+ * Model reader that connects sameAs models from other ModelReader's.
+ */
+export class SameAsModelReader extends ConcatenatingModelReader {}
