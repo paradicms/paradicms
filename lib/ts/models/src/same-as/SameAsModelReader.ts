35031c95dd45ff1494480e0d65d7b61ca660742d
Minor Gordon <minorg@server.fake>
2023-05-30 21:29:03 -0400

    SameAsModelReader: implement grouping algorithm with test

--- lib/ts/models/src/same-as/SameAsModelReader.ts
+++ lib/ts/models/src/same-as/SameAsModelReader.ts
@@ -1,6 +1,66 @@
 import {ConcatenatingModelReader} from "../ConcatenatingModelReader";
+import {ModelSet} from "../ModelSet";
+import {Work} from "../Work";
+import {Model} from "../Model";
+import {SameAsWork} from "./SameAsWork";
+import {BlankNode, NamedNode} from "@rdfjs/types";
+import TermMap from "@rdfjs/term-map";
+import {DataFactory} from "@paradicms/rdf";
+import invariant from "ts-invariant";
 
 /**
  * Model reader that connects sameAs models from other ModelReader's.
  */
-export class SameAsModelReader extends ConcatenatingModelReader {}
+export class SameAsModelReader extends ConcatenatingModelReader {
+    override readWorks(kwds: { modelSet: ModelSet }): readonly Work[] {
+        return this.readSameAsModels(super.readWorks(kwds), SameAsWork);
+    }
+
+    private readSameAsModels<ModelT extends Model>(models: readonly ModelT[], sameAsModelFactory: {
+        new(models: readonly ModelT[]): ModelT
+    }): readonly ModelT[] {
+        const modelGroupsByModelIdentifier: TermMap<BlankNode | NamedNode, ModelT[]> = new TermMap();
+
+        // Simple agglomerative algorithm for merging models that are sameAs each other
+
+        // Put each model into its own group
+        for (const model of models) {
+            modelGroupsByModelIdentifier.set(model.identifier, [model]);
+        }
+
+        // For each (this, sameAs, that), "pull" that's model group into "this's" model group and adjust the pointers
+        for (const model of models) {
+            const modelGroup = modelGroupsByModelIdentifier.get(model.identifier);
+            invariant(typeof modelGroup !== "undefined");
+            for (const sameAsUri of model.sameAsUris) {
+                const sameAsNamedNode = DataFactory.namedNode(sameAsUri);
+                const sameAsModelGroup = modelGroupsByModelIdentifier.get(sameAsNamedNode);
+                if (typeof sameAsModelGroup === "undefined") {
+                    console.debug(model.identifier.value, "sameAs", sameAsUri, "that is not in the model set");
+                    continue;
+                }
+                for (const sameAsModel of sameAsModelGroup) {
+                    // If the sameAsModel is not already in modelGroup, pull it in
+                    if (!modelGroup.some(modelGroupModel => modelGroupModel.identifier.equals(sameAsModel.identifier))) {
+                        modelGroup.push(sameAsModel);
+                    }
+                    // Point sameAsModel to the model group it was pulled into
+                    modelGroupsByModelIdentifier.set(sameAsModel.identifier, modelGroup);
+                }
+            }
+        }
+
+        const sameAsModels: ModelT[] = [];
+        // Return unique model groups.
+        for (const uniqueModelGroup of new Set(modelGroupsByModelIdentifier.values())) {
+            invariant(uniqueModelGroup.length > 0);
+            if (uniqueModelGroup.length === 1) {
+                // Model was not sameAs anything and nothing was sameAs it, pass it through as-is.
+                sameAsModels.push(uniqueModelGroup[0]);
+            } else {
+                sameAsModels.push(new sameAsModelFactory(uniqueModelGroup));
+            }
+        }
+        return sameAsModels;
+    }
+}
---
+++ lib/ts/models/test/same-as/SameAsModelReader.test.ts
@@ -0,0 +1,32 @@
+import {datasetCoreToDataset} from "@paradicms/rdf";
+import {syntheticData} from "@paradicms/test";
+import {SameAsModelReader} from "../../src/same-as/SameAsModelReader";
+import {describe} from "mocha";
+import {CmsModelReader} from "../../src/cms/CmsModelReader";
+import {WikidataModelReader} from "../../src/wikidata/WikidataModelReader";
+import {ConcatenatingModelReader} from "../../src/ConcatenatingModelReader";
+import {dummyModelSet} from "../dummyModelSet";
+import {expect} from "chai";
+import {SameAsWork} from "../../src/same-as/SameAsWork";
+
+describe("SameAsModelReader", () => {
+  const dataset = datasetCoreToDataset(syntheticData);
+  const modelReaders = [
+    new CmsModelReader(dataset),
+    new WikidataModelReader(dataset),
+  ];
+  const concatenatingModelReader = new ConcatenatingModelReader(modelReaders);
+  const sut = new SameAsModelReader(modelReaders);
+
+  it("should group CmsWorks with Wikidata works", () => {
+    const allWorks = concatenatingModelReader.readWorks({
+      modelSet: dummyModelSet,
+    });
+    expect(allWorks).to.have.length(13);
+    const groupedWorks = sut.readWorks({modelSet: dummyModelSet});
+    expect(groupedWorks).to.have.length(12);
+    expect(
+      groupedWorks.filter(work => work instanceof SameAsWork)
+    ).to.have.length(1);
+  });
+});
