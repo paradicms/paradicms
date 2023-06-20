cf73d30fe9bfb816ad3a8662bf34ac60a59a7cb5
Minor Gordon <minorg@server.fake>
2023-06-20 16:16:13 -0400

    copy schema.org vocabulary from rdf-ns-builders and change to https://

--- lib/ts/vocabularies/src/index.ts
+++ lib/ts/vocabularies/src/index.ts
@@ -1,8 +1,9 @@
-export * from "@tpluscode/rdf-ns-builders";
 export {strict as cms} from "./cms";
 export {strict as configuration} from "./configuration";
 export {strict as contact} from "./contact";
+export {strict as schema} from "./schema"; // Must be before export * from rdf-ns-builders
 export {strict as vra} from "./vra";
 export {loose as wd} from "./wd";
 export {loose as wdt} from "./wdt";
 export {strict as wikibase} from "./wikibase";
+export * from "@tpluscode/rdf-ns-builders";
