04808336957549fb776aa50b2eee0464b6909d3b
Minor Gordon <minorg@server.fake>
2023-06-21 18:08:29 -0400

    CmsImagesMixin: use foaf:depiction to get images

--- lib/ts/models/src/cms/CmsImagesMixin.ts
+++ lib/ts/models/src/cms/CmsImagesMixin.ts
@@ -3,15 +3,15 @@ import {ImagesMixin} from "../ImagesMixin";
 import {ResourceBackedNamedModelMixin} from "../ResourceBackedNamedModelMixin";
 import {ThumbnailSelector} from "../ThumbnailSelector";
 import {selectThumbnail} from "../selectThumbnail";
+import {foaf} from "@paradicms/vocabularies";
+import {mapImageObject} from "../mapImageObject";
 
 export abstract class CmsImagesMixin extends ResourceBackedNamedModelMixin
   implements ImagesMixin {
   get images(): readonly Image[] {
-    return this.modelSet.imagesByDepictsIri(this.iri);
-  }
-
-  get originalImages(): readonly Image[] {
-    return this.images.filter(image => image.isOriginal);
+    return this.filterAndMapObjects(foaf.depiction, term =>
+      mapImageObject(this, term)
+    );
   }
 
   thumbnail(selector: ThumbnailSelector): Image | null {
