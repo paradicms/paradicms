4cd20ddfb4b41299ba801bc90c1c29ffc21fb2cc
Minor Gordon <minorg@server.fake>
2023-06-21 16:17:02 -0400

    fill out SchemaCreativeWorkMixin

--- lib/ts/models/src/schema/SchemaCreativeWorkMixin.ts
+++ lib/ts/models/src/schema/SchemaCreativeWorkMixin.ts
@@ -1,4 +1,50 @@
 import {Mixin} from "ts-mixer";
 import {SchemaThingMixin} from "./SchemaThingMixin";
+import {RightsMixin} from "../RightsMixin";
+import {AgentUnion} from "../AgentUnion";
+import {Memoize} from "typescript-memoize";
+import {schema} from "@paradicms/vocabularies";
+import {mapAgentObject} from "../mapAgentObject";
+import {License} from "../License";
+import {mapLicenseObject} from "../mapLicenseObject";
+import {RightsStatement} from "../RightsStatement";
+import {mapRightsStatementObject} from "../mapRightsStatementObject";
 
-export abstract class SchemaCreativeWorkMixin extends Mixin(SchemaThingMixin) {}
+export abstract class SchemaCreativeWorkMixin extends Mixin(SchemaThingMixin)
+  implements RightsMixin {
+  @Memoize()
+  get contributors(): readonly AgentUnion[] {
+    return this.filterAndMapObjects(schema.contributor, term =>
+      mapAgentObject(this, term)
+    );
+  }
+
+  @Memoize()
+  get creators(): readonly AgentUnion[] {
+    return this.filterAndMapObjects(schema.creator, term =>
+      mapAgentObject(this, term)
+    );
+  }
+
+  get license(): License | null {
+    return this.findAndMapObject(schema.license, term =>
+      mapLicenseObject(this, term)
+    );
+  }
+
+  get requiresAttribution(): boolean {
+    return this.license?.requiresAttribution ?? true;
+  }
+
+  get rightsHolders(): readonly AgentUnion[] {
+    return this.filterAndMapObjects(schema.copyrightHolder, term =>
+      mapAgentObject(this, term)
+    );
+  }
+
+  get rightsStatement(): RightsStatement | null {
+    return this.findAndMapObject(schema.usageInfo, term =>
+      mapRightsStatementObject(this, term)
+    );
+  }
+}
--- lib/ts/models/src/schema/SchemaThingMixin.ts
+++ lib/ts/models/src/schema/SchemaThingMixin.ts
@@ -5,35 +5,45 @@ import {selectThumbnail} from "../selectThumbnail";
 import {ResourceBackedModelMixin} from "../ResourceBackedModelMixin";
 import {schema} from "@paradicms/vocabularies";
 import {Text} from "../Text";
+import {mapImageObject} from "../mapImageObject";
+import {mapTextObject} from "../mapTextObject";
+import {Memoize} from "typescript-memoize";
 
 export abstract class SchemaThingMixin extends ResourceBackedModelMixin
   implements ImagesMixin {
+  @Memoize()
   get alternateNames(): readonly string[] {
     return this.filterAndMapObjects(schema.alternateName, this.mapStringObject);
   }
 
+  @Memoize()
   get description(): Text | null {
-    // return this.findAndMapObject(schema.description, mapText)
-    return null;
+    return this.findAndMapObject(schema.description, term =>
+      mapTextObject(this, term)
+    );
   }
 
+  @Memoize()
   get images(): readonly Image[] {
-    return [];
-    // return this.modelSet.imagesByDepictsIri(this.iri);
+    return this.filterAndMapObjects(schema.image, term =>
+      mapImageObject(this, term)
+    );
   }
 
+  get label(): string | null {
+    return this.name;
+  }
+
+  @Memoize()
   get name(): string | null {
     return this.findAndMapObject(schema.name, this.mapStringObject);
-  }
-
-  get originalImages(): readonly Image[] {
-    return this.images.filter(image => image.isOriginal);
   }
 
   thumbnail(selector: ThumbnailSelector): Image | null {
     return selectThumbnail(this.images, selector);
   }
 
+  @Memoize()
   get url(): string | null {
     return this.findAndMapObject(schema.url, this.mapIriObject);
   }
