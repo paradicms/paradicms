2193d6664c23547b39fd9e4cbd331987802ae66f
Minor Gordon <minorg@server.fake>
2023-06-22 10:57:55 -0400

    SchemaCollection.test.ts

--- lib/ts/models/test/schema/SchemaCollection.test.ts
+++ lib/ts/models/test/schema/SchemaCollection.test.ts
@@ -1,13 +1,8 @@
 import {expect} from "chai";
 import {Text} from "../../src";
-import {ThumbnailSelector} from "../../src/ThumbnailSelector";
 import {testModelSet} from "../testModelSet";
 import {describe} from "mocha";
 import {SchemaCollection} from "../../src/schema/SchemaCollection";
-
-const THUMBNAIL_SELECTOR: ThumbnailSelector = {
-  targetDimensions: {height: 200, width: 200},
-};
 
 describe("SchemaCollection", () => {
   const sut: SchemaCollection = testModelSet.collections.find(
@@ -21,20 +16,11 @@ describe("SchemaCollection", () => {
     expect(description.value).to.not.be.empty;
   });
 
-  it("should get the collection's label", () => {
-    expect(sut.label).to.eq(sut.label);
+  it("should get the collection's name", () => {
+    expect(sut.name).to.eq(sut.name);
   });
 
-  it("should get the collection's thumbnail", () => {
-    const thumbnail = sut.thumbnail(THUMBNAIL_SELECTOR);
-    expect(thumbnail).to.not.be.null;
-    expect(
-      sut.iris.some(collectionIri => collectionIri === thumbnail!.depictsIri)
-    ).to.be.false;
-    expect(
-      sut.works.some(work =>
-        work.iris.some(workIri => workIri === thumbnail!.depictsIri)
-      )
-    ).to.be.true;
+  it("should get the collection's works", () => {
+    expect(sut.works).not.to.be.empty;
   });
 });
