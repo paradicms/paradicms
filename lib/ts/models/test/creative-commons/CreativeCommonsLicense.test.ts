81a0b4c44a96bf2fce4f1fb6aa9583f23eae7836
Minor Gordon <minorg@server.fake>
2023-06-22 11:13:29 -0400

    CreativeCommonsLicense.test.ts

---
+++ lib/ts/models/test/creative-commons/CreativeCommonsLicense.test.ts
@@ -0,0 +1,26 @@
+import {expect} from "chai";
+import {testModelSet} from "../testModelSet";
+import {describe} from "mocha";
+import {CreativeCommonsLicense} from "../../src/creative-commons/CreativeCommonsLicense";
+
+describe("CreativeCommonsLicense", () => {
+  const nc_1_0: CreativeCommonsLicense = testModelSet.licenseByIri(
+    "http://creativecommons.org/licenses/nc/1.0/"
+  ) as CreativeCommonsLicense;
+  const pd: CreativeCommonsLicense = testModelSet.licenseByIri(
+    "http://creativecommons.org/publicdomain/mark/1.0/"
+  ) as CreativeCommonsLicense;
+
+  // it("should get the license's identifier", () => {
+  //   expect(sut.identifier).to.not.be.empty;
+  // });
+
+  it("should get requires attribution", () => {
+    expect(nc_1_0.requiresAttribution).to.be.true;
+    expect(pd.requiresAttribution).to.be.false;
+  });
+
+  it("should get the license's title", () => {
+    expect(nc_1_0.title).to.not.be.empty;
+  });
+});
