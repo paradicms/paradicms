6c4e1d3de43e06dbb286f549b602acf3ded33818
Minor Gordon <minorg@server.fake>
2023-06-24 19:53:39 -0400

    more behavesLikeModel

--- lib/ts/models/test/behavesLikeModel.ts
+++ lib/ts/models/test/behavesLikeModel.ts
@@ -1,6 +1,7 @@
 import {Model} from "../src";
 import {it} from "mocha";
 import {expect} from "chai";
+import {Store} from "@paradicms/rdf";
 
 export const behavesLikeModel = (model: Model) => {
   it("should have at least one identifier", () => {
@@ -9,5 +10,11 @@ export const behavesLikeModel = (model: Model) => {
 
   it("should have a non-empty key", () => {
     expect(model.key).not.to.be.empty;
+  });
+
+  it("should convert to RDF", () => {
+    const store = new Store();
+    model.toRdf(store);
+    expect(store.size).to.be.gt(0);
   });
 };
