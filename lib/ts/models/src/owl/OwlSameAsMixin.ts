8823af4cc5869a2592be95f7c5582142463e7bca
Minor Gordon <minorg@server.fake>
2023-06-25 18:57:22 -0400

    @Memoize OwlSameAsMixin

--- lib/ts/models/src/owl/OwlSameAsMixin.ts
+++ lib/ts/models/src/owl/OwlSameAsMixin.ts
@@ -3,9 +3,11 @@ import {ResourceBackedModelMixin} from "../ResourceBackedModelMixin";
 import {SameAsMixin} from "../SameAsMixin";
 import {ModelIdentifier} from "../ModelIdentifier";
 import {owl} from "@paradicms/vocabularies";
+import {Memoize} from "typescript-memoize";
 
 export abstract class OwlSameAsMixin extends Mixin(ResourceBackedModelMixin)
   implements SameAsMixin {
+  @Memoize()
   get sameAsIdentifiers(): readonly ModelIdentifier[] {
     return this.filterAndMapObjects(owl.sameAs, term => {
       switch (term.termType) {
