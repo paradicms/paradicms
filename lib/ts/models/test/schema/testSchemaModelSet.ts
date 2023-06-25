da67470dc505ce1160eb72fc5cc938b969dec43f
Minor Gordon <minorg@server.fake>
2023-06-25 11:24:08 -0400

    register resource-backed model factories here

--- lib/ts/models/test/schema/testSchemaModelSet.ts
+++ lib/ts/models/test/schema/testSchemaModelSet.ts
@@ -1,15 +1,15 @@
 import {DatasetBackedModelSet} from "../../src/DatasetBackedModelSet";
 import {syntheticData} from "@paradicms/test";
-import {CmsModelReader} from "../../src/cms/CmsModelReader";
 import {ConcatenatingModelReader} from "../../src/ConcatenatingModelReader";
 import {CreativeCommonsModelReader} from "../../src/creative-commons/CreativeCommonsModelReader";
 import {RightsStatementsDotOrgModelReader} from "../../src/rights-statements-dot-org/RightsStatementsDotOrgModelReader";
+import {SchemaModelReader} from "../../src/schema/SchemaModelReader";
 import {registerResourceBackedModelFactories} from "../../src/registerResourceBackedModelFactories";
 
-export const testCmsModelSet = new DatasetBackedModelSet(
+export const testSchemaModelSet = new DatasetBackedModelSet(
   syntheticData,
   new ConcatenatingModelReader([
-    new CmsModelReader(syntheticData),
+    new SchemaModelReader(syntheticData),
     new CreativeCommonsModelReader(syntheticData),
     new RightsStatementsDotOrgModelReader(syntheticData),
   ])
