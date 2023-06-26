c432bb9c5a07586cde90130b6bc113729b0865d4
Minor Gordon <minorg@server.fake>
2023-06-26 16:27:34 -0400

    don't need schema_agent.py

--- lib/py/etl/paradicms_etl/models/schema/schema_organization.py
+++ lib/py/etl/paradicms_etl/models/schema/schema_organization.py
@@ -1,19 +1,24 @@
 from typing import Optional
 
 from rdflib import URIRef, Graph, BNode, SDO
+from rdflib.resource import Resource
 
 from paradicms_etl.models.organization import Organization
-from paradicms_etl.models.schema.schema_agent import SchemaAgent
+from paradicms_etl.models.schema.schema_model import SchemaModel
 
 
-class SchemaOrganization(SchemaAgent, Organization):
+class SchemaOrganization(SchemaModel, Organization):
     """
     Schema.org implementation of the Organization interface using schema:Organization properties.
     """
 
-    class Builder(SchemaAgent.Builder):
+    class Builder(SchemaModel.Builder):
         def build(self):
             return SchemaOrganization(self._resource)
+
+    def __init__(self, resource: Resource):
+        SchemaModel.__init__(self, resource)
+        self.name
 
     @classmethod
     def builder(cls, *, name: str, uri: Optional[URIRef] = None) -> Builder:
@@ -24,3 +29,7 @@ class SchemaOrganization(SchemaAgent, Organization):
     @property
     def label(self) -> str:
         return self.name
+
+    @property
+    def name(self) -> str:
+        return self._required_name
