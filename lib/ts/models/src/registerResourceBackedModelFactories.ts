528ab0653aff07b22fb1099a06d135e3c36a0293
Minor Gordon <minorg@server.fake>
2023-06-30 18:52:28 -0400

    nuke CmsLocation

--- lib/ts/models/src/registerResourceBackedModelFactories.ts
+++ lib/ts/models/src/registerResourceBackedModelFactories.ts
@@ -5,22 +5,18 @@ import {imageFactories} from "./imageFactories";
 import {CmsImage} from "./cms/CmsImage";
 import {licenseFactories} from "./licenseFactories";
 import {locationFactories} from "./locationFactories";
-import {CmsLocation} from "./cms/CmsLocation";
 import {agentFactories} from "./agentFactories";
 import {rightsStatementFactories} from "./rightsStatementFactories";
 import {CreativeCommonsLicense} from "./creative-commons/CreativeCommonsLicense";
 import {dateTimeDescriptionFactories} from "./dateTimeDescriptionFactories";
 import {OwlTimeDateTimeDescription} from "./owl-time/OwlTimeDateTimeDescription";
 import {SchemaImageObject} from "./schema/SchemaImageObject";
-import {workEventFactories} from "./workEventFactories";
-import {CmsWorkClosing} from "./cms/CmsWorkClosing";
-import {CmsWorkCreation} from "./cms/CmsWorkCreation";
-import {CmsWorkOpening} from "./cms/CmsWorkOpening";
 import {SchemaTextObject} from "./schema/SchemaTextObject";
 import {FoafOrganization} from "./foaf/FoafOrganization";
 import {FoafPerson} from "./foaf/FoafPerson";
 import {DcRightsStatement} from "./dc/DcRightsStatement";
 import {DcLicenseDocument} from "./dc/DcLicenseDocument";
+import {SchemaPlace} from "./schema/SchemaPlace";
 
 export const registerResourceBackedModelFactories = () => {
   agentFactories.register(foaf.Organization, FoafOrganization);
@@ -37,14 +33,10 @@ export const registerResourceBackedModelFactories = () => {
   licenseFactories.register(dcterms.LicenseDocument, DcLicenseDocument);
   licenseFactories.register(cc.License, CreativeCommonsLicense);
 
-  locationFactories.register(cms.Location, CmsLocation);
+  locationFactories.register(schema.Place, SchemaPlace);
 
   rightsStatementFactories.register(dcterms.RightsStatement, DcRightsStatement);
 
   textFactories.register(cms.Text, CmsText);
   textFactories.register(schema.TextObject, SchemaTextObject);
-
-  workEventFactories.register(cms.WorkClosing, CmsWorkClosing);
-  workEventFactories.register(cms.WorkCreation, CmsWorkCreation);
-  workEventFactories.register(cms.WorkOpening, CmsWorkOpening);
 };
--- lib/ts/models/src/workEventFactories.ts
+++
@@ -1,6 +0,0 @@
-import {ResourceBackedModelFactories} from "./ResourceBackedModelFactories";
-import {WorkEventUnion} from "./WorkEventUnion";
-
-export const workEventFactories = new ResourceBackedModelFactories<
-  WorkEventUnion
->();
