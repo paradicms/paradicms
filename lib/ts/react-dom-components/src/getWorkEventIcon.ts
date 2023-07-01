c5fd315329cc32152ea20c45f1827390f396c260
Minor Gordon <minorg@server.fake>
2023-07-01 13:06:19 -0400

    fix react-dom-components

--- lib/ts/react-dom-components/src/getWorkEventIcon.ts
+++ lib/ts/react-dom-components/src/getWorkEventIcon.ts
@@ -1,18 +1,14 @@
 import {IconDefinition} from "@fortawesome/fontawesome-svg-core";
-import {
-  faDoorClosed,
-  faDoorOpen,
-  faLightbulb,
-} from "@fortawesome/free-solid-svg-icons";
+import {faLightbulb} from "@fortawesome/free-solid-svg-icons";
 import {WorkEventUnion} from "@paradicms/models";
 
 export const getWorkEventIcon = (workEvent: WorkEventUnion): IconDefinition => {
   switch (workEvent.type) {
-    case "WorkClosing":
-      return faDoorClosed;
+    // case "WorkClosing":
+    //   return faDoorClosed;
     case "WorkCreation":
       return faLightbulb;
-    case "WorkOpening":
-      return faDoorOpen;
+    // case "WorkOpening":
+    //   return faDoorOpen;
   }
 };
