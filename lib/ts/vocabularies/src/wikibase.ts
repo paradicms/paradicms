d2362231e91bbc3ae34fc4dd1c331964ca5d58a2
Minor Gordon <minorg@server.fake>
2023-07-04 17:48:24 -0400

    parse a wikibase:TimeValue as a DateTimeDescription

--- lib/ts/vocabularies/src/wikibase.ts
+++ lib/ts/vocabularies/src/wikibase.ts
@@ -4,8 +4,13 @@ import {NamedNode} from "@rdfjs/types";
 interface Wikibase {
   "": NamedNode<"http://wikiba.se/ontology#">;
 
+  // Properties
+  timePrecision: NamedNode<"http://wikiba.se/ontology#timePrecision">;
+  timeValue: NamedNode<"http://wikiba.se/ontology#timeValue">;
+
   // Resources
   Item: NamedNode<"http://wikiba.se/ontology#Item">;
+  TimeValue: NamedNode<"http://wikiba.se/ontology#TimeValue">;
 }
 
 const builder = namespace("http://wikiba.se/ontology#") as any;
