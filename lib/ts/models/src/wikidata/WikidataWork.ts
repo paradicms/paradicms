5f90ee914fea4b65af20cf38383b110275c2dd9d
Minor Gordon <minorg@server.fake>
2023-07-04 17:18:36 -0400

    WikidataWork: inception property, events

--- lib/ts/models/src/wikidata/WikidataWork.ts
+++ lib/ts/models/src/wikidata/WikidataWork.ts
@@ -10,6 +10,10 @@ import {Memoize} from "typescript-memoize";
 import {mapTermToText} from "../mapTermToText";
 import {DataFactory} from "@paradicms/rdf";
 import {getWorkDisplayDate} from "../getWorkDisplayDate";
+import {DateTimeDescription} from "../DateTimeDescription";
+import {wdt} from "@paradicms/vocabularies";
+import {mapTermToDateTimeDescription} from "../mapTermToDateTimeDescription";
+import {SyntheticWorkCreationEvent} from "../synthetic/SyntheticWorkCreationEvent";
 
 export class WikidataWork extends WikidataModel implements Work {
   @Memoize()
@@ -19,6 +23,7 @@ export class WikidataWork extends WikidataModel implements Work {
 
   readonly collections: readonly Collection[] = [];
 
+  @Memoize()
   get description(): Text | null {
     return this.findAndMapObject(
       DataFactory.namedNode("http://schema.org/description"),
@@ -30,6 +35,28 @@ export class WikidataWork extends WikidataModel implements Work {
     return getWorkDisplayDate(this);
   }
 
-  readonly events: readonly WorkEventUnion[] = [];
+  @Memoize()
+  get inception(): DateTimeDescription | null {
+    return this.findAndMapStatementValue(wdt["P571"], term =>
+      mapTermToDateTimeDescription(this, term)
+    );
+  }
+
+  @Memoize()
+  get events(): readonly WorkEventUnion[] {
+    const events: WorkEventUnion[] = [];
+
+    if (this.inception) {
+      events.push(
+        SyntheticWorkCreationEvent.fromWork({
+          date: this.inception,
+          work: this,
+        })
+      );
+    }
+
+    return events;
+  }
+
   readonly location: WorkLocation | null = null;
 }
