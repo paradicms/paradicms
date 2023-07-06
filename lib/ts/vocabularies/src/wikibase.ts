767768205ec8e4db5bdd96e3a600b9758dcfa11f
Minor Gordon <minorg@server.fake>
2023-07-05 20:25:59 -0400

    WikidataLocation working

--- lib/ts/vocabularies/src/wikibase.ts
+++ lib/ts/vocabularies/src/wikibase.ts
@@ -5,10 +5,15 @@ interface Wikibase {
   "": NamedNode<"http://wikiba.se/ontology#">;
 
   // Properties
+  geoLatitude: NamedNode<"http://wikiba.se/ontology#geoLatitude">;
+  geoLongitude: NamedNode<"http://wikiba.se/ontology#geoLongitude">;
   timePrecision: NamedNode<"http://wikiba.se/ontology#timePrecision">;
   timeValue: NamedNode<"http://wikiba.se/ontology#timeValue">;
 
   // Resources
+  GlobecoordinateValue: NamedNode<
+    "http://wikiba.se/ontology#Globecoordinatevalue"
+  >;
   Item: NamedNode<"http://wikiba.se/ontology#Item">;
   TimeValue: NamedNode<"http://wikiba.se/ontology#TimeValue">;
 }
