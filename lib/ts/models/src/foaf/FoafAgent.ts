72dd7555aa9a3e59fad3fe13fe4611f13ed29a25
Minor Gordon <minorg@server.fake>
2023-07-07 19:17:30 -0400

    don't need separate FoafPageMixin

--- lib/ts/models/src/foaf/FoafAgent.ts
+++ lib/ts/models/src/foaf/FoafAgent.ts
@@ -7,14 +7,14 @@ import {mapTermToString} from "@paradicms/rdf";
 import {OwlSameAsMixin} from "../owl/OwlSameAsMixin";
 import {FoafImagesMixin} from "../foaf/FoafImagesMixin";
 import {AgentType} from "../AgentType";
-import {FoafPageMixin} from "./FoafPageMixin";
 import {SomeImageThumbnailMixin} from "../SomeImageThumbnailMixin";
+import {isWikipediaUrl} from "../isWikipediaUrl";
+import {Memoize} from "typescript-memoize";
 
 export abstract class FoafAgent
   extends Mixin(
     FoafModel,
     FoafImagesMixin,
-    FoafPageMixin,
     OwlSameAsMixin,
     SomeImageThumbnailMixin
   )
@@ -23,8 +23,30 @@ export abstract class FoafAgent
     return this.name;
   }
 
+  @Memoize()
+  get homepage(): string | null {
+    return this.findAndMapObject(foaf.homepage, term => {
+      switch (term.termType) {
+        case "Literal":
+        case "NamedNode":
+          return term.value;
+        default:
+          return null;
+      }
+    });
+  }
+
+  @Memoize()
   get name(): string {
     return requireNonNull(this.findAndMapObject(foaf.name, mapTermToString));
+  }
+
+  get wikipediaUrl(): string | null {
+    return this.findAndMapObject(foaf.page, term =>
+      term.termType === "NamedNode" && isWikipediaUrl(term.value)
+        ? term.value
+        : null
+    );
   }
 
   abstract readonly type: AgentType;
