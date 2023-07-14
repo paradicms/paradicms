5771a6e84e6e3b363aab3412fff42767688464ed
Minor Gordon <minorg@server.fake>
2023-07-13 20:32:28 -0400

    Work: no longer need type: ignore

--- lib/py/etl/paradicms_etl/models/work.py
+++ lib/py/etl/paradicms_etl/models/work.py
@@ -5,7 +5,7 @@ from paradicms_etl.models.images_mixin import ImagesMixin
 from paradicms_etl.models.rights_mixin import RightsMixin
 
 
-class Work(Model, ImagesMixin, RightsMixin):  # type: ignore
+class Work(Model, ImagesMixin, RightsMixin):
     """
     Model of a work such as a painting or a garment.
 
