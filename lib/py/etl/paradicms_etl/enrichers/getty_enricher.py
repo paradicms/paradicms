c174a27b1531fa1cc394e76b787239b9ce0486d5
Minor Gordon <minorg@server.fake>
2023-07-14 18:17:33 -0400

    next pass at Getty enricher, no image metadata yet

--- lib/py/etl/paradicms_etl/enrichers/getty_enricher.py
+++ lib/py/etl/paradicms_etl/enrichers/getty_enricher.py
@@ -9,11 +9,18 @@ from rdflib import URIRef, Graph, RDF
 from rdflib.resource import Resource
 
 from paradicms_etl.model import Model
+from paradicms_etl.models.creative_commons.creative_commons_licenses import (
+    CreativeCommonsLicenses,
+)
 from paradicms_etl.models.linked_art.linked_art_human_made_object import (
     LinkedArtHumanMadeObject,
 )
+from paradicms_etl.models.linked_art.linked_art_images_mixin import LinkedArtImagesMixin
+from paradicms_etl.models.linked_art.linked_art_rights_mixin import LinkedArtRightsMixin
+from paradicms_etl.models.rights_statements_dot_org.rights_statements_dot_org_rights_statements import (
+    RightsStatementsDotOrgRightsStatements,
+)
 from paradicms_etl.models.stub.stub_model import StubModel
-from paradicms_etl.models.work import Work
 from paradicms_etl.utils.file_cache import FileCache
 from paradicms_etl.utils.resolve_json_ld_contexts import resolve_json_ld_contexts
 
@@ -26,6 +33,10 @@ class GettyEnricher:
             return self.value
 
     def __init__(self, *, cache_dir_path: Path):
+        self.__available_licenses_by_uri = CreativeCommonsLicenses.by_uri()
+        self.__available_rights_statements_by_uri = (
+            RightsStatementsDotOrgRightsStatements.by_uri()
+        )
         self.__file_cache = FileCache(cache_dir_path=cache_dir_path)
         self.__logger = logging.getLogger(__name__)
 
@@ -40,9 +51,21 @@ class GettyEnricher:
                     continue
 
                 for referenced_getty_entity_uri in referenced_getty_entity_uris:
-                    yield from getattr(self, f"_get_getty_{getty_entity_type}_entity")(
-                        referenced_getty_entity_uri
-                    )
+                    for referenced_getty_entity in getattr(
+                        self, f"_get_getty_{getty_entity_type}_entity"
+                    )(referenced_getty_entity_uri):
+                        yield referenced_getty_entity
+                        if isinstance(referenced_getty_entity, LinkedArtImagesMixin):
+                            yield from referenced_getty_entity.representations
+                        if isinstance(referenced_getty_entity, LinkedArtRightsMixin):
+                            for license_uri in referenced_getty_entity.licenses:
+                                yield self.__available_licenses_by_uri[license_uri]
+                            for (
+                                rights_statement_uri
+                            ) in referenced_getty_entity.rights_statements:
+                                yield self.__available_rights_statements_by_uri[
+                                    rights_statement_uri
+                                ]
 
             if not isinstance(model, StubModel):
                 # A StubModel is "replaced" by the Wikidata entity model
@@ -67,12 +90,10 @@ class GettyEnricher:
     def _get_getty_object_entity(self, getty_entity_uri: URIRef) -> Iterable[Model]:
         resource = self.__get_getty_entity_resource(getty_entity_uri)
         rdf_type = resource.value(RDF.type)
-        work: Work
         if rdf_type.identifier == LinkedArtHumanMadeObject.rdf_type_uri():
-            work = LinkedArtHumanMadeObject.from_rdf(resource)
+            yield LinkedArtHumanMadeObject.from_rdf(resource)
         else:
             raise NotImplementedError(rdf_type)
-        yield work
 
     @staticmethod
     def __get_model_getty_entity_references(
