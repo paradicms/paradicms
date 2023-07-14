13f028a6a538024dc916369479e13dd2608f7332
Minor Gordon <minorg@server.fake>
2023-07-14 17:38:11 -0400

    remove yielded_ deduplicate at enricher level

--- lib/py/etl/paradicms_etl/enrichers/getty_enricher.py
+++ lib/py/etl/paradicms_etl/enrichers/getty_enricher.py
@@ -2,7 +2,7 @@ import json
 import logging
 from enum import Enum
 from pathlib import Path
-from typing import Iterable, Set, Tuple
+from typing import Iterable, Tuple
 from urllib.parse import urlparse
 
 from rdflib import URIRef, Graph, RDF
@@ -30,7 +30,6 @@ class GettyEnricher:
         self.__logger = logging.getLogger(__name__)
 
     def __call__(self, models: Iterable[Model]) -> Iterable[Model]:
-        yielded_getty_entity_uris: Set[URIRef] = set()
         for model in models:
             for getty_entity_type in self._GettyEntityType:
                 referenced_getty_entity_uris = self.__get_model_getty_entity_references(
@@ -41,16 +40,9 @@ class GettyEnricher:
                     continue
 
                 for referenced_getty_entity_uri in referenced_getty_entity_uris:
-                    if referenced_getty_entity_uri in yielded_getty_entity_uris:
-                        continue
-
-                    for getty_entity in getattr(
-                        self, f"_get_getty_{getty_entity_type}_entity"
-                    )(referenced_getty_entity_uri):
-                        if getty_entity.uri in yielded_getty_entity_uris:
-                            continue
-                        yield getty_entity
-                        yielded_getty_entity_uris.add(getty_entity)
+                    yield from getattr(self, f"_get_getty_{getty_entity_type}_entity")(
+                        referenced_getty_entity_uri
+                    )
 
             if not isinstance(model, StubModel):
                 # A StubModel is "replaced" by the Wikidata entity model
@@ -114,180 +106,3 @@ class GettyEnricher:
                 for same_as_uri in model.same_as_uris
                 if is_getty_entity_uri(same_as_uri)
             )
-
-    # def __get_getty_entity(self, getty_entity_uri: URIRef) -> WikibaseItem:
-    #     """
-    #     Get a single Wikidata entity by its id.
-    #     """
-    #
-    #     cached_getty_entity = self.__cached_getty_entities_without_related_by_uri.get(
-    #         getty_entity_uri
-    #     )
-    #     if cached_getty_entity is not None:
-    #         return cached_getty_entity
-    #
-    #     graph = WikidataEntityExtractor(
-    #         cache_dir_path=self.__cache_dir_path,
-    #         entity_uris=(getty_entity_uri,),
-    #     )(force=False)["graph"]
-    #     extracted_getty_items = WikibaseItems.from_rdf(
-    #         graph=graph,
-    #         uris=(getty_entity_uri,),
-    #     )
-    #     assert len(extracted_getty_items) == 1
-    #     extracted_getty_item = extracted_getty_items[0]
-    #     self.__cached_getty_entities_without_related_by_uri[
-    #         getty_entity_uri
-    #     ] = extracted_getty_item
-    #     return extracted_getty_item
-    #
-    # @staticmethod
-    # def __get_getty_entity_images(getty_entity: WikibaseItem) -> Iterable[Image]:
-    #     yielded_image_uris: Set[URIRef] = set()
-    #     for statement in getty_entity.statements_by_property_label.get("image", []):
-    #         assert isinstance(statement.value, URIRef)
-    #         image_uri = statement.value
-    #         if image_uri in yielded_image_uris:
-    #             continue
-    #         yield SchemaImageObject.builder(uri=image_uri).build()
-    #         yielded_image_uris.add(image_uri)
-    #
-    # def __get_getty_entity_creators(
-    #     self, getty_entity: WikibaseItem
-    # ) -> Iterable[Model]:
-    #     for creator_getty_entity in self.__get_connected_getty_entities(
-    #         connected_by_property_label="creator", getty_entity=getty_entity
-    #     ):
-    #         yield creator_getty_entity
-    #         yield from self.__get_getty_entity_images(creator_getty_entity)
-    #
-    # def __get_getty_entity_locations(
-    #     self,
-    #     getty_entity: WikibaseItem,
-    # ) -> Iterable[WikibaseItem]:
-    #     for location_getty_entity in self.__get_connected_getty_entities(
-    #         connected_by_property_label="location", getty_entity=getty_entity
-    #     ):
-    #         yield location_getty_entity
-    #         if not location_getty_entity.statements_by_property_label.get(
-    #             "coordinate location"
-    #         ):
-    #             # Keep chasing "location" until we get one with coordinates
-    #             yield from self.__get_getty_entity_locations(location_getty_entity)
-    #
-    # def __get_getty_entity_subjects(
-    #     self, getty_entity: WikibaseItem
-    # ) -> Iterable[Model]:
-    #     yield from self.__get_connected_getty_entities(
-    #         connected_by_property_label="main subject", getty_entity=getty_entity
-    #     )
-    #
-    # def __get_getty_entity_with_superclass_tree(
-    #     self, root_getty_entity_uri: URIRef
-    # ) -> WikibaseItem:
-    #     """
-    #     Get a Wikidata entity as well as all of its superclass tree (in the form of P279 "subclass of" statements) in
-    #     one WikibaseItem Graph.
-    #
-    #     :param root_getty_entity_uri:
-    #     :return: WikibaseItem corresponding to the given entity id, backed by a Graph with its superclass tree statements
-    #     """
-    #
-    #     getty_entities_by_uri: Dict[URIRef, WikibaseItem] = {}
-    #
-    #     def get_type_getty_entities(
-    #         resolving_getty_entity_uris: Set[URIRef],
-    #         type_property_labels: Tuple[str, ...],
-    #         getty_entity: WikibaseItem,
-    #     ) -> None:
-    #         """
-    #         Recursive function to resolve Wikidata entities in the superclass tree of the given getty_entity.
-    #
-    #         :param resolving_getty_entity_uris: set of Wikidata entity id's already involved in this resolution, to avoid infinite recursion
-    #         :param type_property_labels: property labels such as "subclass of" to consider part of the superclass tree
-    #         :param getty_entity: resolve Wikidata entities related to this entity
-    #         :return:
-    #         """
-    #
-    #         if getty_entity.uri in resolving_getty_entity_uris:
-    #             # It's already somewhere in the stack
-    #             return
-    #         resolving_getty_entity_uris.add(getty_entity.uri)
-    #         self.__logger.debug(
-    #             "resolving Wikidata entities related to %s", getty_entity.uri
-    #         )
-    #
-    #         for type_property_label in type_property_labels:
-    #             type_getty_entity_uris: Sequence[URIRef]
-    #             if type_property_label == "same as":
-    #                 type_getty_entity_uris = getty_entity.same_as_uris
-    #             else:
-    #                 type_getty_entity_uris = []
-    #                 for statement in getty_entity.statements_by_property_label.get(
-    #                     type_property_label, []
-    #                 ):
-    #                     assert isinstance(statement.value, URIRef)
-    #                     type_getty_entity_uris.append(statement.value)
-    #
-    #             if len(type_getty_entity_uris) == 0:
-    #                 continue
-    #
-    #             for type_getty_entity_uri in type_getty_entity_uris:
-    #                 self.__logger.debug(
-    #                     "%s %s %s",
-    #                     getty_entity.uri,
-    #                     type_property_label,
-    #                     type_getty_entity_uri,
-    #                 )
-    #                 type_getty_entity = getty_entities_by_uri.get(
-    #                     type_getty_entity_uri, None
-    #                 )
-    #                 if type_getty_entity is not None:
-    #                     self.__logger.debug("related hit", type_getty_entity_uri)
-    #                 else:
-    #                     self.__logger.debug("related miss", type_getty_entity_uri)
-    #                     getty_entities_by_uri[
-    #                         type_getty_entity_uri
-    #                     ] = type_getty_entity = self.__get_getty_entity(
-    #                         getty_entity_uri=type_getty_entity_uri
-    #                     )
-    #
-    #                 get_type_getty_entities(
-    #                     type_property_labels=("same as", "subclass of"),
-    #                     resolving_getty_entity_uris=resolving_getty_entity_uris,
-    #                     getty_entity=type_getty_entity,
-    #                 )
-    #             # Only consider one type of related entities per resolve
-    #             # same as
-    #             # OR
-    #             # subclass of
-    #             # OR
-    #             # instance of
-    #             # Only one will be relevant. We don't care what a class is an instance of, for example.
-    #             return
-    #
-    #     root_getty_entity = self.__get_getty_entity(root_getty_entity_uri)
-    #     getty_entities_by_uri[root_getty_entity_uri] = root_getty_entity
-    #
-    #     get_type_getty_entities(
-    #         resolving_getty_entity_uris=set(),
-    #         type_property_labels=("same as", "subclass of", "instance of"),
-    #         getty_entity=root_getty_entity,
-    #     )
-    #
-    #     # Combine the root Wikidata entity and all of its superclass tree statements in one Graph
-    #     combined_graph = Graph()
-    #     for getty_entity in getty_entities_by_uri.values():
-    #         if getty_entity.uri == root_getty_entity.uri:
-    #             getty_entity.to_concise_rdf(graph=combined_graph)
-    #         else:
-    #             getty_entity.to_type_rdf(
-    #                 graph=combined_graph,
-    #                 subclass_of_property_uri=WDT["P279"],
-    #             )
-    #     combined_getty_items = WikibaseItems.from_rdf(graph=combined_graph)
-    #     assert (
-    #         len(combined_getty_items) == 1
-    #     )  # The rdf:type wikibase:Item statements should have been elided from everything but the root Wikidata entity
-    #     assert combined_getty_items[0].uri == root_getty_entity_uri
-    #     return combined_getty_items[0]
--- lib/py/etl/test/paradicms_etl_test/enrichers/getty_enricher_test.py
+++ lib/py/etl/test/paradicms_etl_test/enrichers/getty_enricher_test.py
@@ -3,9 +3,11 @@ from pathlib import Path
 from rdflib import URIRef
 
 from paradicms_etl.enrichers.getty_enricher import GettyEnricher
+from paradicms_etl.models.image import Image
 from paradicms_etl.models.license import License
 from paradicms_etl.models.rights_statement import RightsStatement
 from paradicms_etl.models.stub.stub_work import StubWork
+from paradicms_etl.models.work import Work
 
 
 def test_enrich_stub_work(data_dir_path: Path):
@@ -20,17 +22,26 @@ def test_enrich_stub_work(data_dir_path: Path):
         )
     )
     assert len(enriched_models) == 4  # Enricher should eat the stub
-    assert any(isinstance(model, License) for model in enriched_models)
-    assert any(isinstance(model, RightsStatement) for model in enriched_models)
-    # assert any(
-    #     isinstance(model, WikibaseItem) and model.uri == wikidata_entity_uri
-    #     for model in enriched_models
-    # )
-    # assert any(
-    #     isinstance(model, Image)
-    #     and model.uri
-    #     == URIRef(
-    #         "http://commons.wikimedia.org/wiki/Special:FilePath/Alan%20Turing%20Aged%2016.jpg"
-    #     )
-    #     for model in enriched_models
-    # )
+    assert any(
+        isinstance(model, Image)
+        for model in enriched_models
+        if model.uri
+        == URIRef(
+            "https://media.getty.edu/iiif/image/fcbc34dd-cd8a-4a5b-8fd6-cd769588b9a8/full/full/0/default.jpg"
+        )
+    )
+    assert any(
+        isinstance(model, License)
+        for model in enriched_models
+        if model.uri == URIRef("http://creativecommons.org/licenses/by/4.0/")
+    )
+    assert any(
+        isinstance(model, RightsStatement)
+        for model in enriched_models
+        if model.uri == URIRef("https://rightsstatements.org/vocab/NoC-US/1.0/")
+    )
+    assert any(
+        isinstance(model, Work)
+        for model in enriched_models
+        if model.uri == stub_work.uri
+    )
