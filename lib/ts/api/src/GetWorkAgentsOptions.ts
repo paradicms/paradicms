09ff5f1bef6f9c78baa6ec335aa67fd72bbcffbd
Minor Gordon <minorg@server.fake>
2023-08-06 12:37:33 -0400

    rename misnamed options type

--- lib/ts/api/src/GetWorkAgentsOptions.ts
+++ lib/ts/api/src/GetWorkAgentsOptions.ts
@@ -1,7 +1,7 @@
 import {AgentJoinSelector} from "@paradicms/models";
 import {WorkAgentsSort} from "./WorkAgentsSort";
 
-export interface GetNamedWorkAgentsOptions {
+export interface GetWorkAgentsOptions {
   readonly agentJoinSelector?: AgentJoinSelector;
   readonly limit: number;
   readonly offset: number;
