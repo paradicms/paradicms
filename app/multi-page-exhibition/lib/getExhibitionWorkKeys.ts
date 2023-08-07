8f9682007b1b7c6f5df3869096b301f8573f7b12
Minor Gordon <minorg@server.fake>
2023-08-07 10:52:47 -0400

    multi-page-exhibition converted

---
+++ app/multi-page-exhibition/lib/getExhibitionWorkKeys.ts
@@ -0,0 +1,42 @@
+import {Api} from "@paradicms/api";
+import {Collection, ModelSet} from "@paradicms/models";
+
+export const getExhibitionWorkKeys = async (
+  api: Api
+): Promise<{
+  readonly collection: Collection | null;
+  readonly collectionModelSet: ModelSet;
+  readonly workKeys: readonly string[];
+}> => {
+  const {modelSet: collectionModelSet} = await api.getCollections({
+    limit: 1,
+    offset: 0,
+    requireWorks: true,
+  });
+
+  const collection =
+    collectionModelSet.collections.length > 0
+      ? collectionModelSet.collections[0]
+      : null;
+
+  const workKeys = (
+    await api.getWorkKeys(
+      {
+        limit: Number.MAX_SAFE_INTEGER,
+        offset: 0,
+      },
+      {
+        filters: collection
+          ? [
+              {
+                includeValues: [collection.key],
+                type: "CollectionValue",
+              },
+            ]
+          : [],
+      }
+    )
+  ).workKeys;
+
+  return {collection, collectionModelSet, workKeys};
+};
