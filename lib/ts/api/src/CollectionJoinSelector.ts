40fa36287075b4ddd008535230b87d6302040857
Minor Gordon <minorg@server.fake>
2023-08-12 14:46:17 -0400

    convert remaining join selectors

--- lib/ts/api/src/CollectionJoinSelector.ts
+++ lib/ts/api/src/CollectionJoinSelector.ts
@@ -1,15 +1,13 @@
 import {ThumbnailSelector} from "@paradicms/models";
 import {WorkJoinSelector} from "./WorkJoinSelector";
 import {ImageJoinSelector} from "./ImageJoinSelector";
-import {RightsJoinSelector} from "./RightsJoinSelector";
+import {TextJoinSelector} from "./TextJoinSelector";
+import {Intersect, Optional, Record, Static} from "runtypes";
 
-/**
- * See note in ModelSetBuilder re: the use of this interface.
- */
-export interface CollectionJoinSelector {
-  description?: RightsJoinSelector;
-  // Return a single thumbnail Image for this Collection.
-  thumbnail?: ImageJoinSelector & ThumbnailSelector;
-  // Return all works that have a collectionIri referring to this Collection.
-  works?: WorkJoinSelector;
-}
+export const CollectionJoinSelector = Record({
+  description: Optional(TextJoinSelector),
+  thumbnail: Optional(Intersect(ImageJoinSelector, ThumbnailSelector)),
+  works: Optional(WorkJoinSelector),
+}).asReadonly();
+
+export type CollectionJoinSelector = Static<typeof CollectionJoinSelector>;
--- lib/ts/api/src/PropertyGroupJoinSelector.ts
+++ lib/ts/api/src/PropertyGroupJoinSelector.ts
@@ -1,9 +1,14 @@
 import {ThumbnailSelector} from "@paradicms/models";
 import {PropertyJoinSelector} from "./PropertyJoinSelector";
 import {ImageJoinSelector} from "./ImageJoinSelector";
+import {Dictionary, Intersect, Optional, Record, Static} from "runtypes";
 
-export interface PropertyGroupJoinSelector {
-  properties?: PropertyJoinSelector;
-  propertiesByKey?: {[index: string]: PropertyJoinSelector};
-  thumbnail?: ImageJoinSelector & ThumbnailSelector;
-}
+export const PropertyGroupJoinSelector = Record({
+  properties: Optional(PropertyJoinSelector),
+  propertiesByKey: Optional(Dictionary(PropertyJoinSelector)),
+  thumbnail: Optional(Intersect(ImageJoinSelector, ThumbnailSelector)),
+}).asReadonly();
+
+export type PropertyGroupJoinSelector = Static<
+  typeof PropertyGroupJoinSelector
+>;
