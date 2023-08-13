7c7cf7ceb53603047fff5cd530a91310c7f811dd
Minor Gordon <minorg@server.fake>
2023-08-13 15:57:31 -0400

    integrate RestApiClient

--- lib/ts/next/src/getClientApiConfiguration.ts
+++ lib/ts/next/src/getClientApiConfiguration.ts
@@ -1,5 +1,14 @@
 import {ApiConfiguration} from "./ApiConfiguration";
 import {getStaticApiConfiguration} from "./getStaticApiConfiguration";
+import log from "loglevel";
 
-export const getClientApiConfiguration = async (): Promise<ApiConfiguration> =>
-  getStaticApiConfiguration();
+export const getClientApiConfiguration = async (): Promise<ApiConfiguration> => {
+  if (process.env.PARADICMS_CLIENT_API?.toLowerCase() === "rest") {
+    log.info("using REST client API");
+    return {
+      type: "rest",
+    };
+  }
+
+  return getStaticApiConfiguration();
+};
