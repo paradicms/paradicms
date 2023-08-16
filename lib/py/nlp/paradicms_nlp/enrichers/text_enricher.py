79bc819c0b65211cea7dc988f586bab8b9a52af0
Minor Gordon <minorg@server.fake>
2023-08-16 09:53:29 -0400

    developing text enricher

--- lib/py/nlp/paradicms_nlp/enrichers/text_enricher.py
+++ lib/py/nlp/paradicms_nlp/enrichers/text_enricher.py
@@ -1,34 +1,35 @@
 import logging
-from pathlib import Path
-from typing import Dict, Iterable, List, Optional, Tuple
+from typing import Dict, Iterable, List, Set, Tuple
+from urllib.parse import quote, unquote
 
-from bertopic import BERTopic
+import spacy
 from paradicms_etl.model import Model
 from paradicms_etl.models.concept import Concept
 from paradicms_etl.models.schema.schema_creative_work import SchemaCreativeWork
+from paradicms_etl.models.schema.schema_organization import SchemaOrganization
+from paradicms_etl.models.schema.schema_person import SchemaPerson
 from paradicms_etl.models.text import Text
 from rdflib import URIRef
 
-logging.getLogger("pdfminer.cmapdb").setLevel(logging.WARNING)
-logging.getLogger("pdfminer.encodingdb").setLevel(logging.WARNING)
-logging.getLogger("pdfminer.pdfdocument").setLevel(logging.WARNING)
-logging.getLogger("pdfminer.pdfinterp").setLevel(logging.WARNING)
-logging.getLogger("pdfminer.pdfpage").setLevel(logging.WARNING)
-logging.getLogger("pdfminer.pdfparser").setLevel(logging.WARNING)
-logging.getLogger("pdfminer.psparser").setLevel(logging.WARNING)
+__import__("spacy_wordnet.wordnet_annotator")
 
 
 class TextEnricher:
     def __init__(self):
         self.__logger = logging.getLogger(__name__)
-        self.__topic_model: Optional[BERTopic] = None
+        self.__nlp = spacy.load("en_core_web_md")
+        self.__nlp.add_pipe("spacy_wordnet", after="tagger")
 
     def __call__(self, models: Iterable[Model]) -> Iterable[Model]:
-        concept_uris_by_label: Dict[str, URIRef] = {}
         text_works: List[Tuple[SchemaCreativeWork, str]] = []
+        wordnet_concept_uris_by_synset: Dict[str, URIRef] = {}
+
         for model in models:
             if isinstance(model, Concept):
-                concept_uris_by_label[model.label] = model.uri
+                if model.uri.startswith("urn:wordnet:"):
+                    wordnet_concept_uris_by_synset[
+                        unquote(str(model.uri)[len("urn:wordnet:") :])
+                    ] = model.uri
             elif isinstance(model, SchemaCreativeWork) and model.text:
                 if isinstance(model.text, Text):
                     text = model.text.value
@@ -39,23 +40,67 @@ class TextEnricher:
                 continue
             yield model
 
-        if not text_works:
-            return
+        ignored_ent_labels: Set[str] = set()
+        ignored_wordnet_synsets: Set[str] = set()
 
-        texts = [text_work[1] for text_work in text_works]
+        for work, text in text_works:
+            doc = self.__nlp(text)
 
-        while len(texts) <= 64:
-            texts = texts * 2
+            work_replacer = work.replacer()
+            modified_work = False
 
-        self._topic_model.fit(texts)
-        topic_distribution, _ = self._topic_model.approximate_distribution(texts)
+            # Reference concepts corresponding to WordNet synsets
+            for token in doc:
+                if token.pos_ != "NOUN":
+                    continue
+                for wordnet_synset in token._.wordnet.synsets():
+                    concept_uri = wordnet_concept_uris_by_synset.get(
+                        str(wordnet_synset)
+                    )
+                    if concept_uri is not None:
+                        work_replacer.add_about(concept_uri)
+                        modified_work = True
+                    elif str(wordnet_synset) not in ignored_wordnet_synsets:
+                        self.__logger.info(
+                            "%s: no concept for noun %s synset %s",
+                            work.uri,
+                            token,
+                            wordnet_synset,
+                        )
+                        ignored_wordnet_synsets.add(str(wordnet_synset))
 
-        self._topic_model.visualize_distribution(topic_distribution[1])
+            # Reference named entities
+            ent_uris: Dict[str, Dict[str, URIRef]] = {}
+            for ent in doc.ents:
+                ent_uri = ent_uris.get(ent.label_, {}).get(ent.text.lower())
+                if ent_uri is not None:
+                    work_replacer.add_about(ent_uri)
+                    modified_work = True
+                    continue
 
-        return
+                ent_uri = URIRef(f"urn:ner:{quote(ent.text)}")
+                if ent.label_ == "ORG":
+                    model = SchemaOrganization.builder(
+                        name=ent.text, uri=ent_uri
+                    ).build()
+                elif ent.label_ == "PERSON":
+                    model = SchemaPerson.builder(name=ent.text, uri=ent_uri).build()
+                else:
+                    if ent.label_ not in ignored_ent_labels:
+                        self.__logger.info(
+                            "%s: unrecognized named entity label %s: %s",
+                            work.uri,
+                            ent.label_,
+                            ent.text,
+                        )
+                        ignored_ent_labels.add(ent.label_.lower())
+                    continue
 
-    @property
-    def _topic_model(self) -> BERTopic:
-        if self.__topic_model is None:
-            self.__topic_model = BERTopic()
-        return self.__topic_model
+                yield model
+
+                work_replacer.add_about(model.uri)
+                modified_work = True
+
+                ent_uris.setdefault(ent.label_, {})[ent.text.lower()] = ent_uri
+
+            yield work_replacer.build() if modified_work else work
--- lib/py/nlp/tests/enrichers/text_enricher_test.py
+++ lib/py/nlp/tests/enrichers/text_enricher_test.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 from typing import Tuple
 
+from paradicms_etl.models.organization import Organization
+from paradicms_etl.models.person import Person
 from paradicms_etl.models.schema.schema_creative_work import SchemaCreativeWork
+from paradicms_etl.models.work import Work
 
 from paradicms_nlp.enrichers.pdf_enricher import PdfEnricher
 from paradicms_nlp.enrichers.text_enricher import TextEnricher
@@ -18,4 +21,21 @@ def test_enrich(
     )
 
     enriched_models = tuple(TextEnricher()(pdf_works))
-    assert len(enriched_models) == len(pdf_works)
+    enriched_works = tuple(
+        enriched_model
+        for enriched_model in enriched_models
+        if isinstance(enriched_model, Work)
+    )
+    assert len(enriched_works) == len(pdf_works)
+    organizations = tuple(
+        enriched_model
+        for enriched_model in enriched_models
+        if isinstance(enriched_model, Organization)
+    )
+    assert organizations
+    people = tuple(
+        enriched_model
+        for enriched_model in enriched_models
+        if isinstance(enriched_model, Person)
+    )
+    assert people
