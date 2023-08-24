c5405d976add78906dcb35a39c19a7eeb05522ab
Minor Gordon <minorg@server.fake>
2023-08-24 12:55:24 -0400

    don't really need AgentType

--- lib/ts/models/src/Agent.ts
+++ lib/ts/models/src/Agent.ts
@@ -1,13 +1,11 @@
 import {ImagesMixin} from "./ImagesMixin";
 import {Model} from "./Model";
-import {Work} from "./Work";
-import {AgentType} from "./AgentType";
 import {ThumbnailMixin} from "./ThumbnailMixin";
+import {Work} from "./Work";
 
 export interface Agent extends Model, ImagesMixin, ThumbnailMixin {
   readonly homepage: string | null;
   readonly label: string;
-  readonly type: AgentType;
   readonly wikipediaUrl: string | null;
   readonly wikidataConceptIri: string | null;
   readonly works: readonly Work[];
