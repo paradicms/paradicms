670d45380207e282bd6757970fbb80fefcd45d65
Minor Gordon <minorg@server.fake>
2023-08-25 17:57:35 -0400

    mapTermToConcept

---
+++ lib/ts/models/src/conceptFactories.ts
@@ -0,0 +1,4 @@
+import {Concept} from "./Concept";
+import {ResourceBackedModelFactories} from "./ResourceBackedModelFactories";
+
+export const conceptFactories = new ResourceBackedModelFactories<Concept>();
