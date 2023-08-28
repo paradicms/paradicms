1cc7e302a25b574247556b14fa52579970e81766
Minor Gordon <minorg@server.fake>
2023-08-28 13:56:56 -0400

    starting on ranges

--- lib/ts/mem-api/src/facetizeWorks.ts
+++ lib/ts/mem-api/src/facetizeWorks.ts
@@ -1,98 +1,109 @@
+import { DateRangeEndpoint, WorksFacet, WorksFilter } from "@paradicms/api";
 import {
-  StringPropertyValueFacet,
-  WorkSubjectValueFacet,
-  WorksFacet,
-  WorksFilter,
-} from "@paradicms/api";
-import {ThumbnailSelector, Work} from "@paradicms/models";
-import {deleteUndefined} from "@paradicms/utilities";
-import {MutableValueFacetValue} from "./MutableValueFacetValue";
-import {imageToValueFacetValueThumbnail} from "./imageToValueFacetThumbnail";
+  DateTimeDescription,
+  Image,
+  ThumbnailSelector,
+  Work,
+  WorkEvent,
+} from "@paradicms/models";
+import { deleteUndefined } from "@paradicms/utilities";
+import { MutableValueFacetValue } from "./MutableValueFacetValue";
+import { imageToValueFacetValueThumbnail } from "./imageToValueFacetThumbnail";
 
-const facetizeWorksByStringPropertyValue = (kwds: {
-  propertyIri: string;
-  valueFacetValueThumbnailSelector?: ThumbnailSelector;
-  works: readonly Work[];
-}): StringPropertyValueFacet => {
-  const {propertyIri, valueFacetValueThumbnailSelector, works} = kwds;
-  let unknownCount: number = 0;
-  const facetValues: {
-    [index: string]: MutableValueFacetValue<string>;
-  } = {};
-  for (const work of works) {
-    let workHasProperty = false;
-    for (const propertyValue of work.propertyValuesByPropertyIri(propertyIri)) {
-      const propertyValueString: string = propertyValue.value;
-      let facetValue = facetValues[propertyValueString];
-      if (facetValue) {
-        facetValue.count++;
-      } else {
-        facetValue = {
-          count: 1,
-          label: propertyValue.label,
-          value: propertyValueString,
-          thumbnail: valueFacetValueThumbnailSelector
-            ? imageToValueFacetValueThumbnail(
-                propertyValue.thumbnail(valueFacetValueThumbnailSelector)
-              )
-            : undefined,
-        };
-        facetValues[propertyValueString] = deleteUndefined(facetValue);
-      }
-      workHasProperty = true;
-    }
-    if (!workHasProperty) {
-      unknownCount++;
-    }
+const compareDateRangeEndpoints = (left: DateRangeEndpoint, right: DateRangeEndpoint): number {
+  const yearDiff = thisSortDate.year - otherSortDate.year;
+  if (yearDiff !== 0) {
+    return yearDiff;
   }
-  return {
-    propertyIri,
-    type: "StringPropertyValue",
-    unknownCount,
-    values: Object.values(facetValues),
-  };
+
+  // Years are the same.
+  // Fill in the month and/or day with 1 for the purposes of comparison.
+
+  const monthDiff =
+    (thisSortDate.month !== null ? thisSortDate.month : 1) -
+    (otherSortDate.month !== null ? otherSortDate.month : 1);
+  if (monthDiff !== 0) {
+    return monthDiff;
+  }
+  return (
+    (thisSortDate.day !== null ? thisSortDate.day : 1) -
+    (otherSortDate.day !== null ? otherSortDate.day : 1)
+  );
+}
+
+const facetizeWorksByDateRange = (kwds: {
+  workDate: (work: Work) => DateTimeDescription | null;
+  works: readonly Work[];
+}): DateRangeFacet | null => {
+  for (const work of works) {
+    
+  }
 };
 
-const facetizeWorksByWorkSubjectValue = (kwds: {
+const facetizeWorksByValue = <
+  ValueT extends number | string,
+  WorkValueT extends {
+    label: string;
+    thumbnail: (selector: ThumbnailSelector) => Image | null;
+    value: ValueT;
+  }
+>(kwds: {
   valueFacetValueThumbnailSelector?: ThumbnailSelector;
   works: readonly Work[];
-}): WorkSubjectValueFacet => {
-  const {valueFacetValueThumbnailSelector, works} = kwds;
+  workValues: (work: Work) => readonly WorkValueT[];
+}): {
+  unknownCount: number;
+  values: MutableValueFacetValue<ValueT>[];
+} => {
+  const {valueFacetValueThumbnailSelector, works, workValues} = kwds;
+
   let unknownCount: number = 0;
-  const facetValues: {
-    [index: string]: MutableValueFacetValue<string>;
-  } = {};
+  const facetValues: MutableValueFacetValue<ValueT>[] = [];
   for (const work of works) {
-    if (work.subjects.length === 0) {
+    const values = workValues(work);
+    if (values.length === 0) {
       unknownCount++;
       continue;
     }
 
-    for (const workSubject of work.subjects) {
-      let facetValue = facetValues[workSubject.value];
+    for (const value of values) {
+      let facetValue = facetValues.find(
+        facetValue => facetValue.value === value.value
+      );
       if (facetValue) {
         facetValue.count++;
       } else {
         facetValue = {
           count: 1,
-          label: workSubject.label,
-          value: workSubject.value,
+          label: value.label,
+          value: value.value,
           thumbnail: valueFacetValueThumbnailSelector
             ? imageToValueFacetValueThumbnail(
-                workSubject.thumbnail(valueFacetValueThumbnailSelector)
+                value.thumbnail(valueFacetValueThumbnailSelector)
               )
             : undefined,
         };
-        facetValues[workSubject.value] = deleteUndefined(facetValue);
+        facetValues.push(deleteUndefined(facetValue));
       }
     }
   }
   return {
-    type: "WorkSubjectValue",
     unknownCount,
-    values: Object.values(facetValues),
+    values: facetValues,
   };
 };
+
+const getWorkEventDate = (workEvent: WorkEvent): DateTimeDescription | null {
+  if (workEvent.date) {
+    return workEvent.date;
+  } else if (workEvent.startDate) {
+    return workEvent.startDate;
+  } else if (workEvent.endDate) {
+    return workEvent.endDate;
+  } else {
+    return null;
+  }
+}
 
 export const facetizeWorks = (kwds: {
   filters: readonly WorksFilter[];
@@ -104,21 +115,45 @@ export const facetizeWorks = (kwds: {
   for (const filter of filters) {
     switch (filter.type) {
       case "StringPropertyValue":
-        facets.push(
-          facetizeWorksByStringPropertyValue({
-            propertyIri: filter.propertyIri,
+        facets.push({
+          ...facetizeWorksByValue({
             valueFacetValueThumbnailSelector,
             works,
-          })
-        );
+            workValues: work =>
+              work.propertyValuesByPropertyIri(filter.propertyIri),
+          }),
+          propertyIri: filter.propertyIri,
+          type: "StringPropertyValue",
+        });
         break;
+      case "WorkCreationDateRange": {
+        const facet = facetizeWorksByDateRange({
+          workDate: (work) => {
+            for (const workEvent of work.events) {
+              if (workEvent.type === "WorkCreation") {
+                return getWorkEventDate(workEvent);
+              }
+            }
+            return null;
+          },
+          works
+        });
+        if (facet) {
+          facets.push({
+            ...facet,
+            type: "WorkCreationDateRange"
+          });
+        }
+        break;
+      }
       case "WorkSubjectValue":
-        facets.push(
-          facetizeWorksByWorkSubjectValue({
-            valueFacetValueThumbnailSelector,
-            works,
-          })
-        );
+        facets.push({
+          ...facetizeWorksByValue({
+            ...kwds,
+            workValues: work => work.subjects,
+          }),
+          type: "WorkSubjectValue",
+        });
         break;
     }
   }
