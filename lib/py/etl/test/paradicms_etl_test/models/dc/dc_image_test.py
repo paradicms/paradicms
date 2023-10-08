6baf938af6b3f5310c1ad3f9a89d692fce4b8af2
Minor Gordon <minorg@server.fake>
2023-10-08 16:57:14 -0400

    undo conftest.py changes

--- lib/py/etl/test/paradicms_etl_test/models/dc/dc_image_test.py
+++ lib/py/etl/test/paradicms_etl_test/models/dc/dc_image_test.py
@@ -1,6 +1,35 @@
-from rdflib import Graph
+from datetime import datetime
 
+import pytest
+from rdflib import Graph, URIRef
+
+from paradicms_etl.models.creative_commons.creative_commons_licenses import (
+    CreativeCommonsLicenses,
+)
 from paradicms_etl.models.dc.dc_image import DcImage
+from paradicms_etl.models.image_dimensions import ImageDimensions
+from paradicms_etl.models.rights_statements_dot_org.rights_statements_dot_org_rights_statements import (
+    RightsStatementsDotOrgRightsStatements,
+)
+
+
+@pytest.fixture(scope="session")
+def dc_image() -> DcImage:
+    return (
+        DcImage.builder(
+            uri=URIRef("http://example.com/image"),
+        )
+        .set_created(datetime.now())
+        .set_exact_dimensions(ImageDimensions(height=300, width=300))
+        .set_format("image/gif")
+        .set_modified(datetime.now())
+        .add_creator("Test creator")
+        .add_rights_holder("Test holder")
+        .add_license(CreativeCommonsLicenses.BY_1_0.uri)
+        .add_rights_statement(RightsStatementsDotOrgRightsStatements.InC_EDU.uri)
+        .set_src("http://example.com/imagesrc")
+        .build()
+    )
 
 
 def test_replace_copyable(dc_image: DcImage):
--- lib/py/etl/test/paradicms_etl_test/models/linked_art/conftest.py
+++ lib/py/etl/test/paradicms_etl_test/models/linked_art/conftest.py
@@ -1,42 +1,12 @@
-from datetime import datetime
 from pathlib import Path
 
 import pytest
 from rdflib import Graph, URIRef
 
-from paradicms_etl.models.creative_commons.creative_commons_licenses import (
-    CreativeCommonsLicenses,
-)
-from paradicms_etl.models.dc.dc_image import DcImage
-from paradicms_etl.models.dc.dc_license_document import DcLicenseDocument
-from paradicms_etl.models.dc.dc_rights_statement import DcRightsStatement
-from paradicms_etl.models.image_dimensions import ImageDimensions
 from paradicms_etl.models.linked_art.linked_art_human_made_object import (
     LinkedArtHumanMadeObject,
 )
-from paradicms_etl.models.rights_statements_dot_org.rights_statements_dot_org_rights_statements import (
-    RightsStatementsDotOrgRightsStatements,
-)
 from paradicms_etl.utils.skolemize import skolemize
-
-
-@pytest.fixture(scope="session")
-def dc_image() -> DcImage:
-    return (
-        DcImage.builder(
-            uri=URIRef("http://example.com/image"),
-        )
-        .set_created(datetime.now())
-        .set_exact_dimensions(ImageDimensions(height=300, width=300))
-        .set_format("image/gif")
-        .set_modified(datetime.now())
-        .add_creator("Test creator")
-        .add_rights_holder("Test holder")
-        .add_license(CreativeCommonsLicenses.BY_1_0.uri)
-        .add_rights_statement(RightsStatementsDotOrgRightsStatements.InC_EDU.uri)
-        .set_src("http://example.com/imagesrc")
-        .build()
-    )
 
 
 @pytest.fixture(scope="session")
