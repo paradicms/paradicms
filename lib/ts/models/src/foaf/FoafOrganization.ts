b7755187fdf1fb4cfcb6a80fcd2c7c306420b856
Minor Gordon <minorg@server.fake>
2023-10-16 14:44:17 -0400

    delete redundant foaf models

--- lib/ts/models/src/foaf/FoafOrganization.ts
+++
@@ -1,7 +0,0 @@
-import {Mixin} from "ts-mixer";
-import {Organization} from "../Organization";
-import {FoafAgent} from "./FoafAgent";
-
-export class FoafOrganization extends Mixin(FoafAgent) implements Organization {
-  readonly type: "Organization" = "Organization";
-}
