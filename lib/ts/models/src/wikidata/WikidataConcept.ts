442b84a7d05b67e23de0354ec6951406d7d679aa
Minor Gordon <minorg@server.fake>
2023-10-16 14:45:47 -0400

    delete redundant Wikibase and Wikidata models

--- lib/ts/models/src/wikidata/WikidataConcept.ts
+++
@@ -1,9 +0,0 @@
-import {NamedNode} from "rdf-js";
-import {Concept} from "../Concept";
-import {WikidataModel} from "./WikidataModel";
-
-export class WikidataConcept extends WikidataModel implements Concept {
-  get value(): NamedNode {
-    return this._identifier;
-  }
-}
