0394adbeb51a6a6c21cfe917c026c9164e58e681
Minor Gordon <minorg@server.fake>
2023-10-16 21:48:58 -0400

    fix StubWork

--- lib/py/etl/paradicms_etl/models/stub/stub_work.py
+++ lib/py/etl/paradicms_etl/models/stub/stub_work.py
@@ -1,3 +1,6 @@
+from rdflib import URIRef
+
+from paradicms_etl.models.date_time_union import DateTimeUnion
 from paradicms_etl.models.stub.stub_images_mixin import StubImagesMixin
 from paradicms_etl.models.stub.stub_model import StubModel
 from paradicms_etl.models.work import Work
@@ -8,12 +11,36 @@ class StubWork(StubModel, StubImagesMixin, Work):
         pass
 
     @property
-    def description(self):
+    def created(self) -> None:
+        return None
+
+    @property
+    def contributors(self) -> tuple[str | URIRef, ...]:
+        return ()
+
+    @property
+    def creators(self) -> tuple[str | URIRef, ...]:
+        return ()
+
+    @property
+    def description(self) -> None:
         return None
 
     @property
     def label(self) -> str:
         raise NotImplementedError(self.__class__.__name__)
 
-    def replacer(self):
+    @property
+    def modified(self) -> None:
+        return None
+
+    def replacer(self) -> Builder:
         raise NotImplementedError(self.__class__.__name__)
+
+    @property
+    def spatial(self) -> None:
+        return None
+
+    @property
+    def subjects(self) -> tuple[str | URIRef, ...]:
+        return ()
